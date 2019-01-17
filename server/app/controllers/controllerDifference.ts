18fd23affe7dde88e64a5dc6ab530fc13d976005
Martin Pouliot <martinp507@gmail.com>
2019-01-17 15:03:08 -0500

    (r) Removed useless dependencies (MP)

--- server/app/controllers/controllerDifference.ts
+++ server/app/controllers/controllerDifference.ts
@@ -1,4 +1,4 @@
-import { Request, Response, NextFunction } from "express";
+import { Request } from "express";
 import { Message } from "../../../common/communication/message";
 import "reflect-metadata";
 
