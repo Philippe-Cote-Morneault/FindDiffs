bbe259dab59f4dc6f421f05299c0bfbe039d5986
Martin Pouliot <martinp507@gmail.com>
2019-01-17 15:24:34 -0500

    (t) Added more tests for the diffenrence controller (MP)

--- server/app/controllers/controllerDifference.spec.ts
+++ server/app/controllers/controllerDifference.spec.ts
@@ -1,23 +1,42 @@
 import { ControllerDifference } from './controllerDifference';
-import { Message } from "../../../common/communication/message";
 import {expect} from 'chai';
 import { mockReq } from 'sinon-express-mock'
 
 
 describe('ControllerDifference', () => {
     let controllerDifference = new ControllerDifference();
+    
     it('If body is empty, should return an error', () => {
         const request = {
             body: {
             }
         };
         const response = controllerDifference.genDifference(mockReq(request));
-        
-        const message: Message = {
-            title: "Erreur",
-            body: "Le nom est manquant (name)"
+        const errorMessage = "Le nom est manquant (name)";
+        expect(response).to.equal(controllerDifference.printError(errorMessage));
+    });
+    
+    it("If body does not contain an original image return an error", () => {
+        const request = {
+            body: {
+                name:"bob"
+            }
+        };
+        const response = controllerDifference.genDifference(mockReq(request));
+        const errorMessage = "L'image originale est manquante (originalImage)";
+        expect(response).to.equal(controllerDifference.printError(errorMessage));
+    });
+
+    it("If body does not contain a modified image return an error", () =>{
+        const request = {
+            body:{
+                name:"bob",
+                originalImage: "image",
+            }
         }
-        expect(response).to.equal(JSON.stringify(message));
+        const response = controllerDifference.genDifference(mockReq(request));
+        const errorMessage = "L'image modifié est manquante (modifiedImage)";
+        expect(response).to.equal(controllerDifference.printError(errorMessage));
     });
     
 });
--- server/app/controllers/controllerDifference.ts
+++ server/app/controllers/controllerDifference.ts
@@ -4,7 +4,7 @@ import "reflect-metadata";
 
 export class ControllerDifference {
     
-    private printError(error: string): string{
+    public printError(error: string): string{
         const message: Message = {
             title: "Erreur",
             body: error
