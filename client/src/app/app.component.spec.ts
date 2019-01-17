77cd3986492d67e04f78f4191a6d78475dca812f
Laurent Barberis <laurent.barberis15@gmail.com>
2019-01-16 19:20:39 -0500

    (r) Comment unnecessary tests (correction to previous commit) (LB/JM)

--- client/src/app/app.component.spec.ts
+++ client/src/app/app.component.spec.ts
@@ -1,11 +1,11 @@
 // tslint:disable:no-any les attributs sont des types any
 // tslint:disable:no-floating-promises pour le before each
-/*import { TestBed, async, ComponentFixture } from "@angular/core/testing";
+import { TestBed, async, ComponentFixture } from "@angular/core/testing";
 import { AppComponent } from "./app.component";
 import { BasicService } from "./basic.service";
-import { HttpClientModule } from "@angular/common/http";*/
+import { HttpClientModule } from "@angular/common/http";
 describe("AppComponent", () => {
-  /*beforeEach(async(() => {
+  beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
         AppComponent
@@ -19,9 +19,9 @@ describe("AppComponent", () => {
     const app: any = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
-  /*it(`should have as title 'client'`, async(() => {
+  it(`should have as title 'client'`, async(() => {
     const fixture: ComponentFixture<AppComponent> = TestBed.createComponent(AppComponent);
     const app: any = fixture.debugElement.componentInstance;
     expect(app.title).toEqual("LOG2990");
-  }));*/
+  }));
 });
