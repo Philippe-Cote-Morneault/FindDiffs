7dc1b54434300bc058f0342d3454645c5428774b
Martin Pouliot <martinp507@gmail.com>
2019-01-18 15:19:33 -0500

    (r) Fixed a few tslint errors (MP)

--- server/app/controllers/differenceController.spec.ts
+++ server/app/controllers/differenceController.spec.ts
@@ -1,42 +1,41 @@
-import { DifferenceController } from './differenceController';
-import {expect} from 'chai';
-import { mockReq } from 'sinon-express-mock'
+import {expect} from "chai";
+import { mockReq } from "sinon-express-mock";
+import { DifferenceController } from "./differenceController";
 
-
-describe('DifferenceController', () => {
-    let differenceController = new DifferenceController();
-    
-    it('If body is empty, should return an error', () => {
+describe("DifferenceController", () => {
+    const differenceController: DifferenceController = new DifferenceController();
+    it("If body is empty, should return an error", () => {
+        // tslint:disable:typedef
         const request = {
             body: {
-            }
+            },
         };
-        const response = differenceController.genDifference(mockReq(request));
-        const errorMessage = "Le nom est manquant (name)";
+        const response: string = differenceController.genDifference(mockReq(request));
+        const errorMessage: string = "Le nom est manquant (name)";
         expect(response).to.equal(differenceController.printError(errorMessage));
     });
-    
+
     it("If body does not contain an original image return an error", () => {
         const request = {
             body: {
-                name:"bob"
-            }
+                name: "bob",
+            },
         };
-        const response = differenceController.genDifference(mockReq(request));
-        const errorMessage = "L'image originale est manquante (originalImage)";
+        const response: string = differenceController.genDifference(mockReq(request));
+        const errorMessage: string = "L'image originale est manquante (originalImage)";
         expect(response).to.equal(differenceController.printError(errorMessage));
     });
 
-    it("If body does not contain a modified image return an error", () =>{
+    it("If body does not contain a modified image return an error", () => {
         const request = {
-            body:{
-                name:"bob",
+            body: {
+                name: "bob",
                 originalImage: "image",
-            }
-        }
-        const response = differenceController.genDifference(mockReq(request));
-        const errorMessage = "L'image modifié est manquante (modifiedImage)";
+            },
+        };
+        const response: string = differenceController.genDifference(mockReq(request));
+        const errorMessage: string = "L'image modifié est manquante (modifiedImage)";
         expect(response).to.equal(differenceController.printError(errorMessage));
     });
-    
+
 });
