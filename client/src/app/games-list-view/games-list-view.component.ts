f699bde8c678d14925bc104ac0242e6101c37c5a
Unknown <felix-atnoine.bourbonnais@polymtl.ca>
2019-01-18 11:26:56 -0500

    (d) Service dependency added to GamesListView (FAB)

--- client/src/app/games-list-view/games-list-view.component.ts
+++ client/src/app/games-list-view/games-list-view.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { GamesListViewService } from '../games-list-view.service';
 
 @Component({
   selector: 'app-games-list-view',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GamesListViewComponent implements OnInit {
 
-  constructor() { }
+  constructor(public GamesListView : GamesListViewService) { }
 
   ngOnInit() {
   }
