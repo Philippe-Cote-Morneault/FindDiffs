d5079397e80ee3db35b0acbaddd05806dee057fa
Phil <philbond1997@hotmail.com>
2019-01-17 21:53:43 -0500

    (d) Added GamesCardView. (PC)

---
+++ client/src/app/games-card-view/games-card-view.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-games-card-view',
+  templateUrl: './games-card-view.component.html',
+  styleUrls: ['./games-card-view.component.css']
+})
+export class GamesCardViewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
