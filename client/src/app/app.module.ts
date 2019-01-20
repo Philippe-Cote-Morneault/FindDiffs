89baf5961998d61f5042099311a7414539d2ca6c
Laurent Barberis <laurent.barberis15@gmail.com>
2019-01-19 20:29:46 -0500

    Automatic update from creating AdminView component (LB)

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -5,15 +5,19 @@ import { AppComponent } from "./app.component";
 import { InitialViewService } from "./initial-view.service";
 import { HttpClientModule } from "@angular/common/http";
 import { InitialViewComponent } from './initial-view/initial-view.component';
+import { AdminViewComponent } from './admin-view/admin-view.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    InitialViewComponent
+    InitialViewComponent,
+    AdminViewComponent
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    AppRoutingModule
   ],
   providers: [InitialViewService],
   bootstrap: [AppComponent]
