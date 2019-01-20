38ca6bece838c9e97edefa7080dc4f72d5773488
Laurent Barberis <laurent.barberis15@gmail.com>
2019-01-19 20:27:09 -0500

    (d) Creation of the appRouting module in client (LB)

---
+++ client/src/app/app-routing.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AdminViewComponent } from './admin-view/admin-view.component';
+
+const routes: Routes = [{ path: 'admin', component: AdminViewComponent}];
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
