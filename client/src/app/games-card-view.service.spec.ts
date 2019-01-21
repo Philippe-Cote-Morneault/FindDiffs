1a89cd2660b2ea47d1dbcfa3ef61d97fbb2f72b4
Felix <felix-atnoine.bourbonnais@polymtl.ca>
2019-01-21 12:26:07 -0500

    (d) Removed tests in component and service clien t side (FAB)

--- client/src/app/games-card-view.service.spec.ts
+++ client/src/app/games-card-view.service.spec.ts
@@ -1,18 +1,1 @@
-import { TestBed } from '@angular/core/testing';
 
-import { GamesCardViewService } from './games-card-view.service';
-
-describe('GamesCardViewService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
-
-  it('should be created', () => {
-    const service: GamesCardViewService = TestBed.get(GamesCardViewService);
-    expect(service).toBeTruthy();
-  });
-
-  it('sorting [] should return []', () => {
-    let gameTest = new GamesCardViewService();
-    const result: number[]= gameTest.setBestTime([]);
-    expect(result).toEqual([]);
-  });
-});
