435f3418d2f83f19592e62767cb2a75c760f0496
Martin Pouliot <martinp507@gmail.com>
2019-01-22 15:54:07 -0500

    (t) Added test for brush enlarge (MP)

--- server/app/services/differenceGenerator/painter.spec.ts
+++ server/app/services/differenceGenerator/painter.spec.ts
@@ -2,7 +2,7 @@ import {expect} from "chai";
 import { COLOR, Pixel, Position } from "../../model/bitmap/pixel";
 import { Painter } from "./painter";
 
-/* tslint:disable no-shadowed-variable */
+/* tslint:disable no-shadowed-variable no-magic-numbers */
 describe("DifferenceGenerator - Painter", () => {
     describe("drawPixel()", () => {
         it("The pixel should be black on the canvas", () => {
@@ -68,6 +68,45 @@ describe("DifferenceGenerator - Painter", () => {
             painter.drawPixel(whitePixels, new Position(1, height));
 
             expect(whitePixels).to.eql(whitePixelsRef);
+        });
+    });
+    describe("enlargePixel()", () => {
+        it("Testing brush pattern 1", () => {
+            const width: number = 5;
+            const height: number = 5;
+            const pixels: Pixel[] = new Array<Pixel>(width * height);
+            pixels.fill(Pixel.fromColor(COLOR.WHITE));
+
+            const pixelsRef: Pixel[] = pixels.slice();
+            pixelsRef.fill(Pixel.fromColor(COLOR.BLACK));
+
+            const painter: Painter = new Painter(width, height);
+            painter.enlargePixel(pixels, new Position(2, 2).getIndex(width));
+
+            expect(pixels).to.eql(pixelsRef);
+        });
+
+        it("Testing brush pattern 2", () => {
+            const width: number = 7;
+            const height: number = 7;
+            const pixels: Pixel[] = new Array<Pixel>(width * height);
+            pixels.fill(Pixel.fromColor(COLOR.WHITE));
+
+            const pixelsRef: Pixel[] = pixels.slice();
+            pixelsRef.fill(Pixel.fromColor(COLOR.BLACK));
+
+            const painter: Painter = new Painter(width, height);
+            painter.enlargePixel(pixels, new Position(3, 3).getIndex(width));
+
+            const whitePixels: number[] = [0, 1, 5, 6, 7, 13, 35, 41, 42, 43, 47, 48];
+
+            for (let i: number = 0; i < pixelsRef.length; i++ ) {
+                if (whitePixels.indexOf(i) !== -1) {
+                    pixelsRef[i] = Pixel.fromColor(COLOR.WHITE);
+                }
+            }
+
+            expect(pixels).to.eql(pixelsRef);
         });
     });
 
