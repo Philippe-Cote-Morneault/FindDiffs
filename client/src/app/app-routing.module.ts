d857b130523c8481098ec2b29f0ac1b777456b79
Basil <bas98c@hotmail.com>
2019-01-22 23:01:08 -0500

    (D/B) Routing is now functional between initialView and adminView (B)

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
+import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { AdminViewComponent } from "./admin-view/admin-view.component";
+import { InitialViewComponent } from "./initial-view/initial-view.component";
 
-const routes: Routes = [{ path: "admin", component: AdminViewComponent}];
+const routes: Routes = [
+  { path: "", component: InitialViewComponent},
+  { path: "initilView", component: InitialViewComponent},
+  { path: "admin", component: AdminViewComponent},
+];
+
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
+
 })
 export class AppRoutingModule { }
