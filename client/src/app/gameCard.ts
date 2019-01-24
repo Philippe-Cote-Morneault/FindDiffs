c846e8f2fd66af36cf2781cfd66cffc9846d8ce3
Felix <felix-atnoine.bourbonnais@polymtl.ca>
2019-01-24 17:19:23 -0500

    (d) merge (FAB/BC)

--- client/src/app/gameCard.ts
+++ client/src/app/gameCard.ts
@@ -1,8 +1,6 @@
-
 export interface GameCard {
-  id: string;
-  title: string;
-  image: string;
-  bestTimeSolo: number[];
-  bestTimeOnline: number[];
+    title: string;
+    image: string;
+    bestTimeSolo: number[];
+    bestTimeOnline: number[];
 }
