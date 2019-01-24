dd3bf9c438a4703efc3449f2ca7cfbf93643e3e4
Felix <felix-atnoine.bourbonnais@polymtl.ca>
2019-01-24 12:49:42 -0500

    (d) Removed setBestTimeSolo function (FAB)

--- client/src/app/games-card-view.service.ts
+++ client/src/app/games-card-view.service.ts
@@ -1,15 +1,9 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
 
 @Injectable({
   providedIn: 'root'
 })
 export class GamesCardViewService {
-
-  public setBestTime(timeList: number[]): number[] {
-    return timeList.sort((n1, n2) => n1 - n2).slice(0, 3);
-  }
-
-
 
   constructor() { }
 }
