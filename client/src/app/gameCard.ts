0ebd00df5ff0ec56f84c26d0567d3de9ccef484f
Phil <philbond1997@hotmail.com>
2019-01-24 17:17:30 -0500

    (r) Changed gameCard to interface. (PC)

--- client/src/app/gameCard.ts
+++ client/src/app/gameCard.ts
@@ -1,17 +1,8 @@
-import { v4 as uuid } from "uuid";
 
-export class GameCard {
-  public id: string;
-  public title: string;
-  public image: string;
-  public bestTimeSolo: number[];
-  public bestTimeOnline: number[];
-
-  public constructor(id: string, title: string, image: string, bestTimeSolo: number[], bestTimeOnline: number[]) {
-      this.id = id;
-      this.title = title;
-      this.image = image;
-      this.bestTimeSolo = bestTimeSolo;
-      this.bestTimeOnline = bestTimeOnline;
-  }
+export interface GameCard {
+  id: string;
+  title: string;
+  image: string;
+  bestTimeSolo: number[];
+  bestTimeOnline: number[];
 }
