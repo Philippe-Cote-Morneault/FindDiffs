9dce2ee1513d017699586d641f8c37d470138320
Phil <philbond1997@hotmail.com>
2019-01-26 16:46:50 -0500

    (d) Added get request for gameCards. (PC)

--- client/src/app/games-list-view/games-list-view.component.ts
+++ client/src/app/games-list-view/games-list-view.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from "@angular/core";
-import { GameCard } from "../gameCard";
+import { GameCard, POVType } from "../gameCard";
 import { GameCardsService } from "../game-cards.service";
 
 @Component({
@@ -8,11 +8,21 @@ import { GameCardsService } from "../game-cards.service";
   styleUrls: ["./games-list-view.component.css"],
 })
 export class GamesListViewComponent implements OnInit {
-  public games: GameCard[];
+  public simplePOVgames: GameCard[];
+  public freePOVgames: GameCard[];
 
   public constructor(public gameCardsService: GameCardsService) { 
+    /*
     this.games = [{title: "Hello", image: "image1", bestTimeSolo: [1, 2, 3], bestTimeOnline: [4, 5, 6]},
              {title: "Hello2", image: "image2", bestTimeSolo: [11, 22, 33], bestTimeOnline: [44, 55, 66]}];
+*/
+    this.gameCardsService.getGameCards(POVType.Simple).subscribe((cards) => {
+      this.simplePOVgames = cards;
+    });
+
+    this.gameCardsService.getGameCards(POVType.Free).subscribe((cards) => {
+      this.freePOVgames = cards;
+    });
 
   }
 
