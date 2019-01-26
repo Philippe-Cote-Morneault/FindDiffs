64b1a552aad47cbad7595d72d101417a8de9ca6b
Phil <philbond1997@hotmail.com>
2019-01-26 18:04:25 -0500

    (d) Added route to router. (PC)

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { AdminViewComponent } from "./admin-view/admin-view.component";
 import { InitialViewComponent } from "./initial-view/initial-view.component";
+import { GamesListViewComponent } from "./games-list-view/games-list-view.component";
 
 const routes: Routes = [
   { path: "", component: InitialViewComponent},
   { path: "initilView", component: InitialViewComponent},
   { path: "admin", component: AdminViewComponent},
+  { path: "gamesList", component: GamesListViewComponent},
 ];
 
 @NgModule({
