6c99d9eddd2d928b1a711603aeae0091576a652a
Phil <philbond1997@hotmail.com>
2019-01-27 17:07:18 -0500

    (r) Removed useless dependency. (PC)

--- server/app/model/bitmap/header.ts
+++ server/app/model/bitmap/header.ts
@@ -1,4 +1,3 @@
-import { Bitmap } from "./bitmap";
 
 export class Header {
     public static BYTES_OFFSET: number = 0;
@@ -23,16 +22,12 @@ export class InfoHeader {
 
     public width: Int32Array;
     public height: Int32Array;
-    public imageSize: Uint32Array;
     public xPixelsPerM: Uint32Array;
     public yPixelsPerM: Uint32Array;
 
     public constructor(width: Int32Array, height: Int32Array, xPixelsPerM: Uint32Array, yPixelsPerM: Uint32Array) {
         this.width = width;
         this.height = height;
-        const bytesFromPadding: number = (width[0] * Bitmap.BYTES_PER_PIXEL) % Bitmap.ROW_BYTE_MULTIPLE * Bitmap.BYTES_PER_PIXEL;
-        const bytesFromPixels: number = height[0] * width[0] * Bitmap.BYTES_PER_PIXEL;
-        this.imageSize = new Uint32Array([bytesFromPadding + bytesFromPixels]);
         this.xPixelsPerM = xPixelsPerM;
         this.yPixelsPerM = yPixelsPerM;
     }
