7eceb62ac93d6a97d7f0ba6cbc65d2af6e6828ab
Felix <felix-atnoine.bourbonnais@polymtl.ca>
2019-01-28 13:13:23 -0500

    (r) tslint appmodule and socketservice (FAB)

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -7,8 +7,8 @@ import { InitialViewComponent } from "./initial-view/initial-view.component";
 import { InitialViewService } from "./initial-view.service";
 import { NgModule } from "@angular/core";
 
-import { GamesCardViewComponent } from './games-card-view/games-card-view.component';
-import { GamesListViewComponent } from './games-list-view/games-list-view.component';
+import { GamesCardViewComponent } from "./games-card-view/games-card-view.component";
+import { GamesListViewComponent } from "./games-list-view/games-list-view.component";
 @NgModule({
   declarations: [
     AppComponent,
