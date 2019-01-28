38d8418661b7779376ff3cfe68ec3f4e66956573
Martin Pouliot <martinp507@gmail.com>
2019-01-27 23:10:31 -0500

    (t) Added tests for difference detector (MP)

---
+++ server/app/services/differenceGenerator/differenceDetector.spec.ts
@@ -0,0 +1,55 @@
+import {expect} from "chai";
+import * as fs from "fs";
+import { Bitmap } from "../../model/bitmap/bitmap";
+import { BitmapDecoder } from "./bitmapDecoder";
+import { DifferenceDector } from "./differenceDetector";
+
+interface BmpTests {
+    bitmap?: Bitmap;
+    path: string;
+    differenceCount: number;
+}
+
+describe("DiffenceDetector", () => {
+    describe("countDifferences()", () => {
+        const bitmapTests: BmpTests[] = [
+            {
+                bitmap: undefined,
+                path: "test/testBitmaps/differenceCount/blank.bmp",
+                differenceCount: 0,
+            },
+            {
+                bitmap: undefined,
+                path: "test/testBitmaps/differenceCount/3.bmp",
+                differenceCount: 3,
+            },
+            {
+                bitmap: undefined,
+                path: "test/testBitmaps/differenceCount/7.bmp",
+                differenceCount: 7,
+            },
+            {
+                bitmap: undefined,
+                path: "test/testBitmaps/differenceCount/12.bmp",
+                differenceCount: 12,
+            },
+        ];
+
+        before(async () => {
+            await Promise.all(bitmapTests.map(async(bmp: BmpTests) => {
+                await new Promise((resolve: Function) => fs.readFile(bmp.path, (err: Error, data: Buffer) => {
+                    bmp.bitmap = BitmapDecoder.FromArrayBuffer(data.buffer);
+                    resolve();
+                }));
+            }));
+        });
+
+        bitmapTests.forEach((bmp: BmpTests) => {
+            it("Should return " + bmp.differenceCount + " for " + bmp.path, () => {
+                const detector: DifferenceDector = new DifferenceDector(bmp.bitmap as Bitmap);
+                expect(detector.countDifferences()).to.equal(bmp.differenceCount);
+            });
+        });
+
+    });
+});
