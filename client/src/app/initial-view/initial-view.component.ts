d336f506b88b3166120f83b06e24b22fd5be1de0
Laurent Barberis <laurent.barberis15@gmail.com>
2019-01-28 15:50:53 -0500

    (d) Replaced websocket by http request (LB CB)

--- client/src/app/initial-view/initial-view.component.ts
+++ client/src/app/initial-view/initial-view.component.ts
@@ -15,28 +15,18 @@ export class InitialViewComponent {
 
   public verifyUsername(): void {
     const username: string = (document.getElementById("usernameInput") as HTMLInputElement).value;
-
-    console.log("sending Username");
-    this.socketService.sendUsername(username);
-
-    console.log("waiting for the event................................................");
-    this.socketService.validationInfo();
-
-    // this.initialViewService.getUsernameValidation(username).subscribe(this.correctUsername);
+    this.initialViewService.getUsernameValidation(username).subscribe(this.correctUsername);
   }
 
-  @HostListener("window:unload") public UnloadHander(): void {
+  @HostListener("window:beforeunload", ["$event"])
+  public beforeUnload($event): void  {
     const user: string = JSON.parse(localStorage.getItem("user") || "{}");
-    this.initialViewService.getDeleteUsername(user).subscribe();
+    this.initialViewService.getDeleteUsername(user).toPromise();
   }
   public correctUsername(message: Message): void {
-    console.log("============================================================");
-    console.log("tryin to enter correctUsername function!!!");
     if (message != null) {
-      console.log("entered in if of correctUsername!!");
-      // localStorage.setItem("user", message.body);
-      // this.socketService.sendUsername(message.body);
+      localStorage.setItem("user", message.body);
+      // TODO: Change view
     }
-    console.log("=============================================================");
   }
 }
