99c1e3f8be51686ccb84701fc1c8743b982d7c6a
Basil <bas98c@hotmail.com>
2019-01-29 10:53:21 -0500

    (D) Design of create a game in simple view is finished (BC)

--- client/src/app/create-game-simple-view/create-game-simple-view/create-game-simple-view.component.ts
+++ client/src/app/create-game-simple-view/create-game-simple-view/create-game-simple-view.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-create-game-simple-view',
-  templateUrl: './create-game-simple-view.component.html',
-  styleUrls: ['./create-game-simple-view.component.css']
+  selector: "app-create-game-simple-view",
+  templateUrl: "./create-game-simple-view.component.html",
+  styleUrls: ["./create-game-simple-view.component.css"],
 })
 export class CreateGameSimpleViewComponent implements OnInit {
+  public title: string = "Create a simple point of view game";
+  public submitButton: string = "Submit";
+  public cancelButton: string = "Cancel";
+  public gameName: string = "Name of the game :";
 
-  constructor() { }
+  public constructor() {}
 
-  ngOnInit() {
-  }
+  public ngOnInit(): void {}
 
 }
