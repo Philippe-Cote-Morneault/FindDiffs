87f568522c747c0e2e1056b034d56e27e7ff3555
Laurent Barberis <laurent.barberis15@gmail.com>
2019-01-29 18:50:50 -0500

    (r) Deleting useless boolean (LB)

--- client/src/app/initial-view/initial-view.component.ts
+++ client/src/app/initial-view/initial-view.component.ts
@@ -13,7 +13,6 @@ export class InitialViewComponent {
   public constructor(public initialViewService: InitialViewService, private router: Router) { }
   public title: string = "Spot the Differences";
   public button: string = "Accept";
-  public isLogged: boolean = false;
 
   public verifyUsername(): void {
     const username: string = (document.getElementById("usernameInput") as HTMLInputElement).value;
@@ -24,11 +23,9 @@ export class InitialViewComponent {
   public beforeUnload($event: Event): void  {
     const user: string = JSON.parse(localStorage.getItem("user") || "{}");
     this.initialViewService.getDeleteUsername(user).toPromise();
-    this.isLogged = false;
   }
   public correctUsername(message: Message): void {
     if (message != null) {
-      this.isLogged = true;
       localStorage.setItem("user", message.body);
       this.router.navigateByUrl("/admin");
 
