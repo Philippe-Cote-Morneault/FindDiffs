0972df977fcb17f62200af4718c7930ed30461d4
Felix <felix-atnoine.bourbonnais@polymtl.ca>
2019-01-31 16:17:47 -0500

    (d) Modified gameCard to imagePair (FAB)

--- client/src/app/games-card-view/games-card-view.component.ts
+++ client/src/app/games-card-view/games-card-view.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input } from "@angular/core";
-import { GameCard } from "../../../../common/communication/gameCard";
+import { ImagePair } from "../../../../common/communication/imagePair";
 
 @Component({
   selector: "app-games-card-view",
@@ -7,7 +7,7 @@ import { GameCard } from "../../../../common/communication/gameCard";
   styleUrls: ["./games-card-view.component.css"],
 })
 export class GamesCardViewComponent {
-  @Input() public gameCard: GameCard;
+  @Input() public imagePair: ImagePair;
   public buttonSolo: string = "Solo";
   public buttonOnline: string = "1 vs. 1";
 }
