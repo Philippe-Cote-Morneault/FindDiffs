09d3df2353b5fe8e55e7dfbe0ab0225a5a8a9be9
Felix <felix-atnoine.bourbonnais@polymtl.ca>
2019-01-31 16:31:27 -0500

    (r) GameCard link remake (FAB)

--- client/src/app/games-card-view/games-card-view.component.ts
+++ client/src/app/games-card-view/games-card-view.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input } from "@angular/core";
-import { ImagePair } from "../../../../common/communication/imagePair";
+import { GameCard } from "../../../../common/model/gameCard/gameCard";
 
 @Component({
   selector: "app-games-card-view",
@@ -7,7 +7,7 @@ import { ImagePair } from "../../../../common/communication/imagePair";
   styleUrls: ["./games-card-view.component.css"],
 })
 export class GamesCardViewComponent {
-  @Input() public imagePair: ImagePair;
+  @Input() public gameCard: GameCard;
   public buttonSolo: string = "Solo";
   public buttonOnline: string = "1 vs. 1";
 }
