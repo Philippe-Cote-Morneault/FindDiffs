aa8203b32fc887cbce0fd76cef5c9272316b0809
Martin Pouliot <martinp507@gmail.com>
2019-01-31 15:09:23 -0500

    (r) Changed some todos. (PC)

--- server/app/services/database/user/userGateway.spec.ts
+++ server/app/services/database/user/userGateway.spec.ts
@@ -1,11 +1,11 @@
 import {expect} from "chai";
-import { DatabaseConnectionHandler } from "../databaseConnectionHandler";
+import { DbConnectionHandler } from "../dbConnectionHandler";
 import { UserGateway } from "./userGateway";
 
 describe("Usergateway", () => {
 
     it("connect", () => {
-        let db: DatabaseConnectionHandler = new DatabaseConnectionHandler();
+        let db: DbConnectionHandler = new DbConnectionHandler();
         let gateway: UserGateway = new UserGateway(db.database);
         let doc: any = gateway.getUsername("test");
         //db.connect();
