50ad82058981b47785a1dcb7135f33e2db0d39e5
Basil <bas98c@hotmail.com>
2019-01-31 09:25:45 -0500

    (D) Adding resources to verify the characteristics of the image (BC)

--- client/src/app/create-game-simple-view/create-game-simple-view/create-game-simple-view.component.ts
+++ client/src/app/create-game-simple-view/create-game-simple-view/create-game-simple-view.component.ts
@@ -6,13 +6,58 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./create-game-simple-view.component.css"],
 })
 export class CreateGameSimpleViewComponent implements OnInit {
+
+  public constructor() {}
+
   public title: string = "Create a simple point of view game";
   public submitButton: string = "Submit";
   public cancelButton: string = "Cancel";
   public gameName: string = "Name of the game :";
 
-  public constructor() {}
+  public verifyInfoGame(): void {
+    // const gameName: string = (document.getElementById("gameInput") as HTMLInputElement).value;
+
+    // if()
+    // let img = new Image();
+
+    /*
+      window.URL = window.URL || window.webkitURL;
+
+      $("form").submit( function( e ) {
+          var form = this;
+          e.preventDefault(); //Stop the submit for now
+                                      //Replace with your selector to find the file input in your form
+          var fileInput = $(this).find("input[type=file]")[0],
+              file = fileInput.files && fileInput.files[0];
+
+          if( file ) {
+              var img = new Image();
+
+              img.src = window.URL.createObjectURL( file );
+
+              img.onload = function() {
+                  var width = img.naturalWidth,
+                      height = img.naturalHeight;
+
+                  window.URL.revokeObjectURL( img.src );
+
+                  if( width == 400 && height == 300 ) {
+                      form.submit();
+                  }
+                  else {
+                      //fail
+                  }
+              };
+          }
+          else { //No file was input or browser doesn't support client side reading
+              form.submit();
+          }
+
+      });
+    */
+  }
 
   public ngOnInit(): void {}
 
 }
+// https://www.bootply.com/oFPl81lzM6
