2506e5a7ba83136c6a835cffaf4d185edb52e36d
Martin Pouliot <martinp507@gmail.com>
2019-01-31 15:57:40 -0500

    (r) added TODOs (JM)(LB)(BC)

--- server/app/services/usernameHandler/usernameHandler.service.ts
+++ server/app/services/usernameHandler/usernameHandler.service.ts
@@ -62,6 +62,7 @@ export class UsernameHandler {
         return true;
     }
 
+    // TODO isAlpha already exists? use regex
     public isAlpha(letter: string): boolean {
         const MAX_VALUE: Number = 122;
         const MIN_VALUE: Number = 97;
@@ -69,6 +70,7 @@ export class UsernameHandler {
         return (letter.charCodeAt(0) <= MAX_VALUE && letter.charCodeAt(0) >= MIN_VALUE);
     }
 
+    // TODO isNumber already exists
     public isNumeric(letter: string): boolean {
         const MAX_VALUE: Number = 58;
         const MIN_VALUE: Number = 47;
@@ -76,6 +78,7 @@ export class UsernameHandler {
         return (letter.charCodeAt(0) < MAX_VALUE && letter.charCodeAt(0) > MIN_VALUE);
     }
 
+    // TODO change to regex
     public isCorrectLength(username: string): boolean {
         const MAX_VALUE: Number = 13;
         const MIN_VALUE: Number = 2;
