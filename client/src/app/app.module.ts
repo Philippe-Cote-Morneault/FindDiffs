3fd46faf826e320802c7bd4f5f8dd4edd13b4819
Basil <bas98c@hotmail.com>
2019-02-01 10:16:14 -0500

    (D) Implementation of buttons and fields on creation of a new game (BC)

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,14 +1,14 @@
 import { HttpClientModule } from "@angular/common/http";
 import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
 import { BrowserModule } from "@angular/platform-browser";
-// import { RouterModule } from "@angular/router";
 import { AdminViewComponent } from "./admin-view/admin-view.component";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 // import { AuthGuard } from "./auth.guard";
+import { CreateGameSimpleViewComponent } from "./create-game-simple-view/create-game-simple-view.component";
 import { InitialViewService } from "./initial-view.service";
 import { InitialViewComponent } from "./initial-view/initial-view.component";
-import { CreateGameSimpleViewComponent } from './create-game-simple-view/create-game-simple-view/create-game-simple-view.component';
 
 @NgModule({
   declarations: [
@@ -21,6 +21,7 @@ import { CreateGameSimpleViewComponent } from './create-game-simple-view/create-
     BrowserModule,
     HttpClientModule,
     AppRoutingModule,
+    FormsModule,
   ],
   providers: [InitialViewService],
   bootstrap: [AppComponent],
