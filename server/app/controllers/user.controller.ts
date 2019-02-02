66612b8bf701a64e08175654599513d0262a98b6
Martin Pouliot <martinp507@gmail.com>
2019-02-02 04:01:59 -0500

    (d) Created a user controller (MP)

---
+++ server/app/controllers/user.controller.ts
@@ -0,0 +1,35 @@
+import { NextFunction, Request, Response, Router } from "express";
+import {inject, injectable} from "inversify";
+import { UserService } from "../services/user/user.service";
+import TYPES from "../types";
+import { IController } from "./interfaces";
+
+@injectable()
+export class UserController implements IController {
+    public constructor(@inject(TYPES.IUserService) private userService: UserService) {}
+
+    public get router(): Router {
+        const router: Router = Router();
+
+        router.get("/", async (req: Request, res: Response, next: NextFunction) => {
+            const response: string = await this.userService.index();
+            res.send(response);
+        });
+
+        router.post("/", (req: Request, res: Response, next: NextFunction) => {
+            const response: string = this.userService.post(req);
+            res.send(response);
+        });
+        router.get("/:id", async (req: Request, res: Response, next: NextFunction) => {
+            const response: string = await this.userService.single(req.params.id);
+            res.send(response);
+        });
+
+        router.delete("/:id", async (req: Request, res: Response, next: NextFunction) => {
+            const response: string = await this.userService.delete(req.params.id);
+            res.send(response);
+        });
+
+        return router;
+    }
+}
