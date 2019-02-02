bb329d57c574a13a6c6a37db62a1a6a92a33116a
Martin Pouliot <martinp507@gmail.com>
2019-02-02 13:51:53 -0500

    (r) Changed the interface declaration (MP)

--- common/model/gameCard.ts
+++ common/model/gameCard.ts
@@ -1,12 +1,12 @@
 import { ICommonImagePair } from "./imagePair";
 
 export interface ICommonGameCard {
-    id: string,
-    pov: POVType,
-    title: string,
-    images: ICommonImagePair,
-    bestTimesSolo: number[],
-    bestTimesOnline: number[],
+    id: string;
+    pov: POVType;
+    title: string;
+    image_pair?: ICommonImagePair;
+    best_time_solo: number[];
+    best_time_online: number[];
 }
 
 export enum POVType{Simple, Free};
