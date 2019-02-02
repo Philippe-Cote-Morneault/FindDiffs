44b7288fea8a4303bb92c6885e63c8d0f9abce9d
Martin Pouliot <martinp507@gmail.com>
2019-02-02 13:01:47 -0500

    (r) Changed the pov to an enum (MP)

--- common/model/gameCard.ts
+++ common/model/gameCard.ts
@@ -2,11 +2,11 @@ import { CommonImagePair } from "./imagePair";
 
 export interface CommonGameCard {
     guid: string,
-    pov: string,
+    pov: POVType,
     title: string,
     images: CommonImagePair,
     bestTimesSolo: number[],
     bestTimesOnline: number[],
 }
 
-export const POVType: string[] =  ["Simple",  "Free"];
+export enum POVType{Simple, Free};
