4d75b9a41c1e49e1444e5e484b35a83d43d1007c
Martin Pouliot <martinp507@gmail.com>
2019-02-03 11:39:37 -0500

    (r) Changed mongoose mock to put it inside a namespace (MP)

--- server/app/tests/mocks.ts
+++ server/app/tests/mocks.ts
@@ -1,13 +1,29 @@
-export class MongooseMockQuery {
-    private data: Object;
-    private isResolved: boolean;
+export namespace MongooseMock {
+    export class Query {
+        private data: Object;
+        private isResolved: boolean;
 
-    public constructor(data: Object, isResolved: boolean) {
-        this.data = data;
-        this.isResolved = isResolved;
+        public constructor(data: Object, isResolved: boolean) {
+            this.data = data;
+            this.isResolved = isResolved;
+        }
+
+        public async select(): Promise<Object> {
+            return (this.isResolved) ? Promise.resolve(this.data) : Promise.reject(this.data);
+        }
     }
 
-    public async select(): Promise<Object> {
-        return (this.isResolved) ? Promise.resolve(this.data) : Promise.reject(this.data);
+    export class Schema {
+        private data: Object;
+        private isNotAnArray: boolean;
+        public constructor(data: Object, isNotAnArray: boolean) {
+            this.data = data;
+            this.isNotAnArray = isNotAnArray;
+        }
+
+        public get(id: string): Object {
+            return this.isNotAnArray ? this.data : this.data[id];
+        }
+
     }
 }
