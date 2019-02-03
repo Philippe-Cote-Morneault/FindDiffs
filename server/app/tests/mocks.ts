541d229060469030c2a5a717f95ce5ed7fec2fb6
Martin Pouliot <martinp507@gmail.com>
2019-02-03 01:51:02 -0500

    (t) Added more tests for game card (MP)

--- server/app/tests/mocks.ts
+++ server/app/tests/mocks.ts
@@ -1,11 +1,13 @@
 export class MongooseMockQuery {
     private data: Object;
+    private isResolved: boolean;
 
-    public constructor(data: Object) {
+    public constructor(data: Object, isResolved: boolean) {
         this.data = data;
+        this.isResolved = isResolved;
     }
 
     public async select(): Promise<Object> {
-        return Promise.resolve(this.data);
+        return (this.isResolved) ? Promise.resolve(this.data) : Promise.reject(this.data);
     }
 }
