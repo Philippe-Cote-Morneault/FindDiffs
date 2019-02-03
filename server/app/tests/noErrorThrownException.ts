d3c5de2673155bebca55d2d9a8eef8d4117aabe2
Martin Pouliot <martinp507@gmail.com>
2019-02-03 11:37:25 -0500

    (d) Added name to a missing exception (MP)

--- server/app/tests/noErrorThrownException.ts
+++ server/app/tests/noErrorThrownException.ts
@@ -2,6 +2,7 @@ export class NoErrorThrownException extends Error {
 
     public constructor() {
         super("Expected an exception to be thrown in this test case. No error was thrown.");
+        this.name = "NoErrorThrownException";
         Object.setPrototypeOf(this, NoErrorThrownException.prototype);
     }
 }
