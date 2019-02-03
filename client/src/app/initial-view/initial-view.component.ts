0a31029da698c83b6e6727aab945b480450d1513
Jacob Meilleur <jacob.meilleur@polymtl.ca>
2019-02-03 12:38:51 -0500

    (d) change url to localhost:4200 on refresh (JM)

--- client/src/app/initial-view/initial-view.component.ts
+++ client/src/app/initial-view/initial-view.component.ts
@@ -5,33 +5,35 @@ import { Message } from "../../../../common/communication/message";
 import { InitialViewService } from "../initial-view.service";
 
 @Component({
-  selector: "app-initial-view",
-  templateUrl: "./initial-view.component.html",
-  styleUrls: ["./initial-view.component.css"],
+    selector: "app-initial-view",
+    templateUrl: "./initial-view.component.html",
+    styleUrls: ["./initial-view.component.css"],
 })
 export class InitialViewComponent {
 
-  public constructor(public initialViewService: InitialViewService, private router: Router) { }
-  public title: string = "Spot the Differences";
-  public button: string = "Accept";
+    public constructor(public initialViewService: InitialViewService, private router: Router) {
+        this.router.navigateByUrl("/");
+     }
+    public title: string = "Spot the Differences";
+    public button: string = "Accept";
 
-  public verifyUsername(): void {
-    const username: string = (document.getElementById("usernameInput") as HTMLInputElement).value;
-    this.initialViewService.postUsernameValidation(username).subscribe(this.correctUsername.bind(this));
-  }
-
-  @HostListener("window:beforeunload", ["$event"])
-  public beforeUnload($event: Event): void  {
-    const user: User = JSON.parse(localStorage.getItem("user") || "{}");
-    this.initialViewService.deleteUsername(user.id).toPromise();
-  }
-
-  public correctUsername(response: User | Message): void {
-    if ((response as User).id) {
-      localStorage.setItem("user", JSON.stringify(response));
-      this.router.navigateByUrl("/admin");
-    } else {
-      alert((response as Message).body);
+    public verifyUsername(): void {
+        const username: string = (document.getElementById("usernameInput") as HTMLInputElement).value;
+        this.initialViewService.postUsernameValidation(username).subscribe(this.correctUsername.bind(this));
     }
-  }
+
+    @HostListener("window:beforeunload", ["$event"])
+    public beforeUnload($event: Event): void {
+        const user: User = JSON.parse(localStorage.getItem("user") || "{}");
+        this.initialViewService.deleteUsername(user.id).toPromise();
+    }
+
+    public correctUsername(response: User | Message): void {
+        if ((response as User).id) {
+            localStorage.setItem("user", JSON.stringify(response));
+            this.router.navigateByUrl("/admin");
+        } else {
+            alert((response as Message).body);
+        }
+    }
 }
