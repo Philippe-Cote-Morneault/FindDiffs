7be946ebeadffb4f5cd4c68f3e3a53400cc96bd8
Felix <felix-atnoine.bourbonnais@polymtl.ca>
2019-02-02 22:15:28 -0500

    (d) tslint image-pair service (FAB)

--- client/src/app/services/image-pair.service.ts
+++ client/src/app/services/image-pair.service.ts
@@ -1,14 +1,12 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
 
 @Injectable()
 export class ImagePairService {
   private readonly BASE_URL: string = "http://localhost:3000/image-pair/";
 
-
   private http: HttpClient;
-
 
   public constructor(http: HttpClient) {
     this.http = http;
