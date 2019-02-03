deaab43fd21f4fbb6615377ff634533968d4bc89
Laurent Barberis <laurent.barberis15@gmail.com>
2019-02-03 14:43:09 -0500

    (t) tests for user service by JM (LB)

--- client/src/app/services/user.service.spec.ts
+++ client/src/app/services/user.service.spec.ts
@@ -1,1 +1,66 @@
+import { Message } from "../../../../common/communication/message";
+import { User } from "../../../../common/communication/user";
+import { TestHelper } from "../../test.helper";
+import { InitialViewService } from "./initial-view.service";
 
+// tslint:disable-next-line:no-any Used to mock the http call
+let httpClientSpyPost: any;
+// tslint:disable-next-line:no-any Used to mock the http call
+let httpClientSpyDelete: any;
+let initialViewServicePost: InitialViewService;
+let initialViewServiceDelete: InitialViewService;
+
+describe("InitialViewService", () => {
+
+        beforeEach(() => {
+            httpClientSpyPost = jasmine.createSpyObj("HttpClient", ["post"]);
+            httpClientSpyDelete = jasmine.createSpyObj("HttpClient", ["delete"]);
+            initialViewServicePost = new InitialViewService(httpClientSpyPost);
+            initialViewServiceDelete = new InitialViewService(httpClientSpyDelete);
+        });
+
+        it("should return expected message on verifyUsername request (HttpClient called once)", () => {
+            const expectedUser: User = { id: "1", username: "user1" };
+            const mockUsername: string = "user1";
+            httpClientSpyPost.post.and.returnValue(TestHelper.asyncData(expectedUser));
+
+            initialViewServicePost.postUsernameValidation(mockUsername).subscribe(
+                (response: User) => {
+                    expect(response.id).toEqual(expectedUser.id, "Id check");
+                    expect(response.username).toEqual(expectedUser.username, "Username check");
+                },
+                fail,
+            );
+
+            expect(httpClientSpyPost.post.calls.count()).toBe(1, "one call");
+        });
+
+        it("should return expected message on deleteUsername request (HttpClient called once)", () => {
+            const expectedMessage: Message = { title: "UsernameDeleted", body: "user1" };
+            const mockUserId: string = "user1";
+            httpClientSpyDelete.delete.and.returnValue(TestHelper.asyncData(expectedMessage));
+
+            initialViewServiceDelete.deleteUsername(mockUserId).subscribe(
+                (response: Message) => {
+                    expect(response.title).toEqual(expectedMessage.title, "Title check");
+                    expect(response.body).toEqual(expectedMessage.body, "body check");
+                },
+                fail,
+            );
+
+            expect(httpClientSpyDelete.delete.calls.count()).toBe(1, "one call");
+        });
+
+        it("should return an error", () => {
+            const expectedMessageError: Message = { title: "Error", body: "There is already a user with the same username on the server" };
+            const mockUsername: string = "user1";
+            httpClientSpyPost.post.and.returnValue(TestHelper.asyncData(expectedMessageError));
+
+            initialViewServicePost.postUsernameValidation(mockUsername).subscribe();
+            initialViewServicePost.postUsernameValidation(mockUsername).subscribe((response: Message) => {
+                expect(response.title).toEqual(expectedMessageError.title, "TitleCheck");
+                expect(response.body).toEqual(expectedMessageError.body, "Body check");
+            });
+        });
+
+    });
