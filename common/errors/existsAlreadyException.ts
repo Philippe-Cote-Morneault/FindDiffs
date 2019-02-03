44bac21e45aa4b66783ca4be8267bd429688d039
Martin Pouliot <martinp507@gmail.com>
2019-02-03 11:36:47 -0500

    (d) Added names to exceptions (MP)

--- common/errors/existsAlreadyException.ts
+++ common/errors/existsAlreadyException.ts
@@ -3,6 +3,7 @@ export class ExistsAlreadyException extends Error {
     public constructor(m: string) {
         super(m);
         this.status = 409;
+        this.name = "ExistsAlreadyException";
         Object.setPrototypeOf(this, ExistsAlreadyException.prototype);
     }
     public status: number;
