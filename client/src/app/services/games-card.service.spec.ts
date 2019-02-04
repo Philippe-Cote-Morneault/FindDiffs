dffebea211fe3ee282d55b08f45296a45442651c
Phil <philbond1997@hotmail.com>
2019-02-03 22:11:21 -0500

    (r) Added some tests. (PC)

--- client/src/app/services/games-card.service.spec.ts
+++ client/src/app/services/games-card.service.spec.ts
@@ -3,7 +3,6 @@ import { TestBed } from "@angular/core/testing";
 import { expect } from "chai";
 import { ICommonGameCard, POVType } from "../../../../common/model/gameCard";
 import { GamesCardService } from "./games-card.service";
-import { POVType, ICommonGameCard } from "../../../../common/model/gameCard";
 import { Message } from "../../../../common/communication/message";
 
 // tslint:disable:no-magic-numbers
@@ -78,46 +77,110 @@ describe("GamesCardService", () => {
         httpMock = TestBed.get(HttpTestingController);
     });
 
-    it("Should return a new GameCard when posting an ImagePair", () => {
-        service.addGameCard("game1", "12345678", POVType.Simple).subscribe((gameCard: ICommonGameCard) => {
-            expect(gameCard).to.equal(mockGameCard1);
-        });
-
-        const testRequest: TestRequest = httpMock.expectOne("http://localhost:3000/gamecard/");
-        expect(testRequest.request.method).to.equal("POST");
-
-        testRequest.flush(mockGameCard1);
-
-        httpMock.verify();
-    });
-
-    it("Should return an array of GameCards when requesting all GameCards", () => {
-        service.getGameCards().subscribe((gameCards: ICommonGameCard[]) => {
-            expect(gameCards).to.equal(mockGameArray);
-        });
-
-        const testRequest: TestRequest = httpMock.expectOne("http://localhost:3000/gamecard/");
-        expect(testRequest.request.method).to.equal("GET");
-
-        testRequest.flush(mockGameArray);
-
-        httpMock.verify();
-    });
-
-    it("Should return an array of GameCards when requesting all GameCards", () => {
-        service.deleteGameCard(mockGameCard1.id).subscribe((message: Message) => {
-            expect(message.title).to.equal("success");
-            expect(message.body).to.equal("Gamecard was deleted successfully");
-        });
-
-        const testRequest: TestRequest = httpMock.expectOne("http://localhost:3000/gamecard/" + mockGameCard1.id);
-        expect(testRequest.request.method).to.equal("DELETE");
-
-        testRequest.flush({
-            "title": "success",
-            "body": "Gamecard was deleted successfully",
+    describe("getGameCards()", () => {
+        it("Should return an array of GameCards when requesting all GameCards", () => {
+            service.getGameCards().subscribe((gameCards: ICommonGameCard[]) => {
+                expect(gameCards).to.equal(mockGameArray);
             });
 
-        httpMock.verify();
+            const testRequest: TestRequest = httpMock.expectOne("http://localhost:3000/gamecard/");
+            expect(testRequest.request.method).to.equal("GET");
+
+            testRequest.flush(mockGameArray);
+
+            httpMock.verify();
+        });
+    });
+
+    describe("addGameCard()", () => {
+        it("Should return a new GameCard when posting an ImagePair", () => {
+            service.addGameCard("game1", "12345678", POVType.Simple).subscribe((gameCard: ICommonGameCard) => {
+                expect(gameCard).to.equal(mockGameCard1);
+            });
+
+            const testRequest: TestRequest = httpMock.expectOne("http://localhost:3000/gamecard/");
+            expect(testRequest.request.method).to.equal("POST");
+
+            testRequest.flush(mockGameCard1);
+
+            httpMock.verify();
+        });
+
+        it("Should return an error message when sending a wrong POV", () => {
+            service.addGameCard(mockGameCard1.title, mockGameCard1.image_pair.id, mockGameCard1.pov)
+                .subscribe((message: Message) => {
+                    expect(message.title).to.equal("Error");
+                    expect(message.body).to.equal("The pov is not a Simple or free type");
+            });
+
+            const testRequest: TestRequest = httpMock.expectOne("http://localhost:3000/gamecard/");
+            expect(testRequest.request.method).to.equal("POST");
+
+            testRequest.flush({
+                "title": "Error",
+                "body": "The pov is not a Simple or free type",
+                });
+
+            httpMock.verify();
+        });
+    });
+
+    describe("deleteGameCard()", () => {
+        it("Should return a sucess message when proper deletion", () => {
+            service.deleteGameCard(mockGameCard1.id).subscribe((message: Message) => {
+                expect(message.title).to.equal("success");
+                expect(message.body).to.equal("Gamecard was deleted successfully");
+            });
+
+            const testRequest: TestRequest = httpMock.expectOne("http://localhost:3000/gamecard/" + mockGameCard1.id);
+            expect(testRequest.request.method).to.equal("DELETE");
+
+            testRequest.flush({
+                "title": "success",
+                "body": "Gamecard was deleted successfully",
+                });
+
+            httpMock.verify();
+        });
+    });
+
+    describe("resetBestTimes", () => {
+        it("Should return a success message when reseting times", () => {
+            service.resetBestTimes(mockGameCard1).subscribe((message: Message) => {
+                expect(message.title).to.equal("success");
+                expect(message.body).to.equal("Gamecard was updated successfully");
+            });
+
+            const testRequest: TestRequest = httpMock.expectOne("http://localhost:3000/gamecard/" + mockGameCard1.id);
+            expect(testRequest.request.method).to.equal("PUT");
+
+            testRequest.flush(
+                {
+                    "title": "success",
+                    "body": "Gamecard was updated successfully",
+                },
+            );
+
+            httpMock.verify();
+        });
+
+        it("Should return an error message if the request is not valid", () => {
+            service.resetBestTimes(mockGameCard1).subscribe((message: Message) => {
+                expect(message.title).to.equal("Error");
+                expect(message.body).to.equal("The request is not valid");
+            });
+
+            const testRequest: TestRequest = httpMock.expectOne("http://localhost:3000/gamecard/" + mockGameCard1.id);
+            expect(testRequest.request.method).to.equal("PUT");
+
+            testRequest.flush(
+                {
+                    "title": "Error",
+                    "body": "The request is not valid",
+                },
+            );
+
+            httpMock.verify();
+        });
     });
 });
--- client/src/app/services/games-card.service.ts
+++ client/src/app/services/games-card.service.ts
@@ -1,7 +1,7 @@
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpErrorResponse } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { of, Observable } from "rxjs";
-import { catchError, map } from "rxjs/operators";
+import { catchError } from "rxjs/operators";
 import { Message } from "../../../../common/communication/message";
 import { ICommonGameCard, POVType } from "../../../../common/model/gameCard";
 import { SERVER_URL } from "../../../../common/url";
@@ -14,34 +14,35 @@ export class GamesCardService {
 
     public constructor(private http: HttpClient, private socketService: SocketService) { }
 
-    public getGameCards(): Observable<ICommonGameCard[]> {
+    public getGameCards(): Observable<ICommonGameCard[] | Message> {
         return this.http.get<ICommonGameCard[]>(`${SERVER_URL}/gamecard/`).pipe(
-            map((res) => res),
+            catchError((error) => this.handleError(error)),
         );
     }
 
-    public addGameCard(gameName: string, imagePairId: string, pov: POVType): Observable<ICommonGameCard> {
+    public addGameCard(gameName: string, imagePairId: string, pov: POVType): Observable<ICommonGameCard | Message> {
         const requestBody: Object = { "name": gameName, "image-pair-id": imagePairId, "pov": "Simple" };
 
-        return this.http.post<ICommonGameCard>(`${SERVER_URL}/gamecard/`, requestBody);
+        return this.http.post<ICommonGameCard>(`${SERVER_URL}/gamecard/`, requestBody).pipe(
+            catchError((error) => this.handleError(error)),
+        );
     }
 
     public deleteGameCard(gameCardId: string): Observable<Message> {
         return this.http.delete<Message>(`${SERVER_URL}/gamecard/${gameCardId}`).pipe(
-            catchError(this.handleError<Message>("deleteGameCard")),
+            catchError((error) => this.handleError(error)),
         );
     }
 
-    public resetBestTimes(gameCard: ICommonGameCard): Observable<ICommonGameCard | Message> {
+    public resetBestTimes(gameCard: ICommonGameCard): Observable<Message> {
 
         const requestBody: Object = {
             "best_time_solo": gameCard.best_time_solo.toString(),
             "best_time_online": gameCard.best_time_online.toString(),
         };
 
-        return this.http.put(`${SERVER_URL}/gamecard/${gameCard.id}`, requestBody).pipe(
-            map((value) => value as ICommonGameCard),
-            catchError(this.handleError<Message>("resetBestTimes")),
+        return this.http.put<Message>(`${SERVER_URL}/gamecard/${gameCard.id}`, requestBody).pipe(
+            catchError((error) => this.handleError(error)),
         );
     }
 
@@ -72,9 +73,10 @@ export class GamesCardService {
         });
     }
 
-    private handleError<T>(request: string, result?: T): (error: Error) => Observable<T> {
-        return (error: Error): Observable<T> => {
-            return of(result as T);
-        };
+    private handleError(error: HttpErrorResponse): Observable<Message> {
+        return of({
+            title: "error",
+            body: error.error.body,
+        });
     }
 }
