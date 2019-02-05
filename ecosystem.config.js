13a0468b7a470d50c128668c4cf03eac6ba40d27
Martin Pouliot <martinp507@gmail.com>
2019-02-04 22:49:47 -0500

    (d) Added files for continious deployment (MP)

---
+++ ecosystem.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  /**
+   * Application configuration section
+   * http://pm2.keymetrics.io/docs/usage/application-declaration/
+   */
+  apps: [
+    // First application
+    {
+      name: 'LOG2990',
+      script: 'server/out/server/app/www.js',
+      env_production: {NODE_ENV: 'production'}
+    }
+  ],
+
+  /**
+   * Deployment section
+   * http://pm2.keymetrics.io/docs/usage/deployment/
+   */
+  deploy: {
+    production: {
+      user: 'root',
+      host: 'martinpouliot.ca:8478',
+      ref: 'origin/develop',
+      repo: 'git@gitlab.com:log2990-106/log2990.git',
+      path: '/root/log2990/',
+      'post-deploy':
+          'cd server && npm install && npm build && cd .. && pm2 reload ecosystem.config.js --env production'
+    }
+  }
+}
