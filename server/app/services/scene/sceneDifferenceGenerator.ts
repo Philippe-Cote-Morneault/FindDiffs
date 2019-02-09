e87cd847d6db5eb4a0b367ac6acfb12a3c087b09
Phil <philbond1997@hotmail.com>
2019-02-09 18:36:25 -0500

    (d) Added more fields to SceneDifferenceGenerator. (PC)

--- server/app/services/scene/sceneDifferenceGenerator.ts
+++ server/app/services/scene/sceneDifferenceGenerator.ts
@@ -3,30 +3,47 @@ import { SceneTransformation } from "./sceneTransformation";
 import { SceneObjectAdder } from "./sceneObjectAdder";
 import { SceneObjectRemover } from "./sceneObjectRemover";
 import { SceneObjectColorChanger } from "./sceneObjectColorChanger";
+import { ICommonSceneModifications } from "../../../../common/model/scene/sceneModifications";
+import { stringify } from "querystring";
 
 export class SceneDifferenceGenerator {
     private readonly NUMBER_OF_ERRORS: number = 7;
+    // Holds the modifications that can be applied to a scene according to user preference
     private transformationsToApply: SceneTransformation[] = [];
-    private originalObjects: ICommonSceneObject[];
-    private indexOfModifiedObjects: number[] = [];
+    // Objects that can be modified from the original scene. Modified objects will be removed from this array
+    private transformationEligibleObjects: ICommonSceneObject[];
+    // Objects that have been added to the scene or that their color has changed
+    private modifiedObjects: ICommonSceneObject[];
+    private modifications: ICommonSceneModifications;
 
     public generateModifiedScene(originalObjects: ICommonSceneObject[], requiresInsertion: boolean, requiresRemoval: boolean,
-                                 requiresColorChange: boolean): ICommonSceneObject[] {
+                                 requiresColorChange: boolean): ICommonSceneModifications {
+
+        // Deep copy the originalObjects array
+        this.transformationEligibleObjects = JSON.parse(JSON.stringify(originalObjects));
 
         this.setTransformationsToApply(requiresInsertion, requiresRemoval, requiresColorChange);
-        this.originalObjects = originalObjects;
+
+        // TODO: Find out how to get id
+        this.modifications = {
+            id: "test",
+            addedObjects: [],
+            deletedObjects: [],
+            colorChangedObjects: new Map<string, string>(),
+        };
 
         for (let i: number = 0; i < this.NUMBER_OF_ERRORS; ++i) {
             this.applyRandomModification();
         }
 
-      
+        return this.modifications;
+
     }
 
     private applyRandomModification(): void {
         const indexOfTransformation: number = Math.floor(Math.random() * this.transformationsToApply.length);
         const transformation: SceneTransformation = this.transformationsToApply[indexOfTransformation];
-        transformation.applyTransformation(this.originalObjects, this.indexOfModifiedObjects);
+        transformation.applyTransformation(this.transformationEligibleObjects, this.modifiedObjects, this.modifications);
     }
 
     private setTransformationsToApply(requiresInsertion: boolean, requiresRemoval: boolean, requiresColorChange: boolean ): void {
