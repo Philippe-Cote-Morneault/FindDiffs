1ce17f4f94d7c79e3c3d6991239f8d09ce0d0465
Phil <philbond1997@hotmail.com>
2019-02-09 18:50:28 -0500

    (d) Implemented objectRemoval From Scene. (PC)

--- server/app/services/scene/sceneObjectRemover.ts
+++ server/app/services/scene/sceneObjectRemover.ts
@@ -9,5 +9,10 @@ export class SceneObjectRemover implements SceneTransformation {
     public applyTransformation(transformationEligibleObjects: ICommonSceneObject[], modifiedObjects: ICommonSceneObject[],
                                modifications: ICommonSceneModifications): void {
 
+        const indexOfObject: number = Math.floor(Math.random() * transformationEligibleObjects.length);
+        const removedObject: ICommonSceneObject = transformationEligibleObjects[indexOfObject];
+        modifiedObjects.push(removedObject);
+        modifications.deletedObjects.push(removedObject.id);
+        transformationEligibleObjects.splice(indexOfObject, 1);
     }
 }
