dff10b1995acf5ff8621ea3168ce38a61dcc4ebf
Phil <philbond1997@hotmail.com>
2019-02-09 18:43:47 -0500

    (r) Fixed some lint errors and documented some classes. (PC)

--- server/app/services/scene/sceneObjectAdder.ts
+++ server/app/services/scene/sceneObjectAdder.ts
@@ -1,8 +1,13 @@
-import { SceneTransformation } from "./sceneTransformation";
+import { ICommonSceneModifications } from "../../../../common/model/scene/sceneModifications";
 import { ICommonSceneObject } from "../../../../common/model/scene/sceneObject";
+import { SceneTransformation } from "./sceneTransformation";
 
+/**
+ * In charge of adding a random object to a scene
+ */
 export class SceneObjectAdder implements SceneTransformation {
-    public applyTransformation(originalObjects: ICommonSceneObject[],  indexOfModifiedObjects: number[]): void {
+    public applyTransformation(transformationEligibleObjects: ICommonSceneObject[], modifiedObjects: ICommonSceneObject[],
+                               modifications: ICommonSceneModifications): void {
 
     }
 }
--- server/app/services/scene/sceneObjectColorChanger.ts
+++ server/app/services/scene/sceneObjectColorChanger.ts
@@ -1,8 +1,13 @@
 import { ICommonSceneObject } from "../../../../common/model/scene/sceneObject";
 import { SceneTransformation } from "./sceneTransformation";
+import { ICommonSceneModifications } from "../../../../common/model/scene/sceneModifications";
 
+/**
+ * In charge of changing the color of a random scene object to a random color
+ */
 export class SceneObjectColorChanger implements SceneTransformation {
-    public applyTransformation(originalObjects: ICommonSceneObject[],  indexOfModifiedObjects: number[]): void {
+    public applyTransformation(transformationEligibleObjects: ICommonSceneObject[], modifiedObjects: ICommonSceneObject[],
+                               modifications: ICommonSceneModifications): void {
 
     }
 }
