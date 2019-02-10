f5435873d69b6de10858a0f8fc23641a0c959960
Laurent Barberis <laurent.barberis15@gmail.com>
2019-02-10 15:42:42 -0500

    (d) First version of getClickPosition on canvas (LB)

--- client/src/app/game-view/game-view.component.ts
+++ client/src/app/game-view/game-view.component.ts
@@ -2,34 +2,51 @@ import { Component, OnInit } from "@angular/core";
 import { ActivatedRoute } from "@angular/router";
 import { ICommonGameCard } from "../../../../common/model/gameCard";
 import { GamesCardService } from "../services/games-card.service";
+// import { ConvertActionBindingResult } from "@angular/compiler/src/compiler_util/expression_converter";
 
 @Component({
-  selector: "app-game-view",
-  templateUrl: "./game-view.component.html",
-  styleUrls: ["./game-view.component.css"],
+    selector: "app-game-view",
+    templateUrl: "./game-view.component.html",
+    styleUrls: ["./game-view.component.css"],
 })
 export class GameViewComponent implements OnInit {
-  public gameCard: ICommonGameCard;
-  private gamesCardService: GamesCardService;
+    public gameCard: ICommonGameCard;
+    private gamesCardService: GamesCardService;
 
-  private id: string;
+    private id: string;
+    private canvas: HTMLCanvasElement;
 
-  public constructor(gamesCardService: GamesCardService, private route: ActivatedRoute) {
-    this.gamesCardService = gamesCardService;
-  }
+    public constructor(gamesCardService: GamesCardService, private route: ActivatedRoute) {
+        this.gamesCardService = gamesCardService;
+    }
 
-  public ngOnInit(): void {
-    this.route.params.subscribe((params) => {
-      this.id = params["id"];
-    });
+    public ngOnInit(): void {
+        this.route.params.subscribe((params) => {
+            this.id = params["id"];
+        });
 
-    this.getGameById();
-  }
+        this.getGameById();
+        this.canvas = (document.getElementById("myCanvas") as HTMLCanvasElement);
+        this.canvas.addEventListener("click", this.getClickPosition);
+    }
 
-  private getGameById(): void {
-    this.gamesCardService.getGameById(this.id).subscribe((gameCard: ICommonGameCard) => {
-      this.gameCard = gameCard;
-    });
-  }
+    private getGameById(): void {
+        this.gamesCardService.getGameById(this.id).subscribe((gameCard: ICommonGameCard) => {
+            this.gameCard = gameCard;
+        });
+    }
+
+    // tslint:disable-next-line:no-any
+    public getClickPosition(e: any): void {
+        this.canvas = (document.getElementById("myCanvas") as HTMLCanvasElement);
+        const ctx: CanvasRenderingContext2D | null = this.canvas.getContext("2d");
+        const xPosition: number = e.layerX;
+        const yPosition: number = e.layerY;
+        if (ctx) {
+            const pixel: ImageData = ctx.getImageData(xPosition, yPosition, 1, 1);
+            console.log(pixel);
+        }
+        alert(xPosition + "::" + yPosition);
+    }
 
 }
