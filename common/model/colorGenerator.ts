3e8d89269f358b30cef592e9d0e0aa9c598ff9c8
Basil Çapar <bas98c@hotmail.com>
2019-02-09 23:18:06 -0500

    (r) added a comment (BC)

--- common/model/colorGenerator.ts
+++ common/model/colorGenerator.ts
@@ -1,5 +1,5 @@
 export class ColorGenerator {
-    public static readonly MAX_COLOR: number = 16777215; // 0xFFFFFF
+    public static readonly MAX_COLOR: number = 16777215; // 0xFFFFFF (white) 
 
     public static generateColor(): number {
         return Math.round(Math.random() * this.MAX_COLOR);
