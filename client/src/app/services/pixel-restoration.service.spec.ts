826c6cefd219cdd1a4f50ed3f9124a56d9744cf5
Jacob Meilleur <jacob.meilleur@polymtl.ca>
2019-02-10 12:06:11 -0500

    (t) first test to add pixel to canvas (JM)

--- client/src/app/services/pixel-restoration.service.spec.ts
+++ client/src/app/services/pixel-restoration.service.spec.ts
@@ -1,12 +1,18 @@
 import { TestBed } from "@angular/core/testing";
-
+import * as sinon from "sinon";
+import { IPixel } from "../../Objects/Pixel";
 import { PixelRestorationService } from "./pixel-restoration.service";
 
 describe("PixelRestorationService", () => {
+
+  const pixelRestorationService: PixelRestorationService = new PixelRestorationService();
+
   beforeEach(() => TestBed.configureTestingModule({}));
 
-  it("should be created", () => {
-    const service: PixelRestorationService = TestBed.get(PixelRestorationService);
-    expect(service).toBeTruthy();
+  it("should add a pixel to the canvas", () => {
+    const data: Uint8ClampedArray = new Uint8ClampedArray([0, 0, 0, 255]);
+    const expectedImageData: ImageData = new ImageData(data, 1, 1);
+    pixelRestorationService.addPixel(expectedImageData);
+    expect(pixelRestorationService.context.getImageData()).toEqual(expectedImageData);
   });
 });
