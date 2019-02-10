14c977780e5409336b2b8ea4d2f789566dd7f9cd
Basil Çapar <bas98c@hotmail.com>
2019-02-09 21:07:21 -0500

    (r) added a comment to indicate white color

--- common/model/colorGenerator.ts
+++ common/model/colorGenerator.ts
@@ -1,8 +1,7 @@
 export class ColorGenerator {
-    public static readonly MAX_COLOR: number = 16777215;
+    public static readonly MAX_COLOR: number = 16777215; // 0xFFFFFF
 
     public static generateColor(): number {
-
         return Math.round(Math.random() * this.MAX_COLOR);
     }
 }
