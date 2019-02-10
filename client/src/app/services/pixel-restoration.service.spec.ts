b22c7c76bc9fe5cd72716ba19574900e76067b5b
Jacob Meilleur <jacob.meilleur@polymtl.ca>
2019-02-10 14:35:25 -0500

    (t) added test (JM)

--- client/src/app/services/pixel-restoration.service.spec.ts
+++ client/src/app/services/pixel-restoration.service.spec.ts
@@ -1,6 +1,6 @@
 import { TestBed } from "@angular/core/testing";
-import * as sinon from "sinon";
-import { IPixel } from "../../Objects/Pixel";
+/*import * as sinon from "sinon";
+import { IPixel } from "../../Objects/Pixel";*/
 import { PixelRestorationService } from "./pixel-restoration.service";
 
 describe("PixelRestorationService", () => {
@@ -9,10 +9,31 @@ describe("PixelRestorationService", () => {
 
   beforeEach(() => TestBed.configureTestingModule({}));
 
-  it("should add a pixel to the canvas", () => {
+  it("should have no pixels", () => {
+      const blankCanvas: HTMLCanvasElement = (document.createElement("canvas") as HTMLCanvasElement);
+      if (pixelRestorationService.canvas) {
+        expect(pixelRestorationService.canvas.toDataURL).toEqual(blankCanvas.toDataURL);
+      }
+  });
+
+  it("should add a black pixel to the canvas", () => {
     const data: Uint8ClampedArray = new Uint8ClampedArray([0, 0, 0, 255]);
     const expectedImageData: ImageData = new ImageData(data, 1, 1);
-    pixelRestorationService.addPixel(expectedImageData);
-    expect(pixelRestorationService.context.getImageData()).toEqual(expectedImageData);
+    pixelRestorationService.addPixel(expectedImageData, 0, 0);
+    if (pixelRestorationService.context) {
+          expect(pixelRestorationService.context.getImageData(0, 0, 1, 1)).toEqual(expectedImageData);
+    }
+  });
+
+  it("should add multiple black pixels to the canvas", () => {
+    const data: Uint8ClampedArray = new Uint8ClampedArray([0, 0, 0, 255,
+                                                          0, 0, 0 , 255,
+                                                          0, 0, 0, 255,
+                                                          0, 0, 0, 255]);
+    const expectedImageData: ImageData = new ImageData(data, 4, 1);
+    pixelRestorationService.addPixel(expectedImageData, 0, 0);
+    if (pixelRestorationService.context) {
+          expect(pixelRestorationService.context.getImageData(0, 0, 4, 1)).toEqual(expectedImageData);
+    }
   });
 });
--- client/src/app/services/pixel-restoration.service.ts
+++ client/src/app/services/pixel-restoration.service.ts
@@ -5,20 +5,25 @@ import { IPixel } from "../../Objects/Pixel";
   providedIn: "root",
 })
 export class PixelRestorationService {
-  private canvas: HTMLCanvasElement | null = (document.getElementById("canvasStyle")) as HTMLCanvasElement;
+  public canvas: HTMLCanvasElement | null = (document.getElementById("canvas")) as HTMLCanvasElement;
+  public context: CanvasRenderingContext2D | null;
 
   public addPixel(imageData: ImageData, posX: number, posY: number): void {
+
     if (this.canvas) {
-      const context: CanvasRenderingContext2D | null = this.canvas.getContext("2d");
-      if (context) {
+      this.context = this.canvas.getContext("2d");
+      if (this.context) {
         const pixel: IPixel = {
-          posX: posX, posY: posY,
-          r: imageData.data[0], g: imageData.data[1],
-          b: imageData.data[2], a: imageData.data[3]
+          posX: posX,
+          posY: posY,
+          r: imageData.data[0],
+          g: imageData.data[1],
+          b: imageData.data[2],
+          a: imageData.data[3]
         };
 
-        context.fillStyle = "rgba(" + pixel.r + "," + pixel.g + "," + pixel.b + "," + pixel.a + ")";
-        context.fillRect(posX, posY, imageData.width, imageData.height);
+        this.context.fillStyle = "rgba(" + pixel.r + "," + pixel.g + "," + pixel.b + "," + pixel.a + ")";
+        this.context.fillRect(posX, posY, imageData.width, imageData.height);
       }
     }
   }
