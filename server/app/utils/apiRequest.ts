1fe36618a469d6f04329d84cd6640ae29a977586
Martin Pouliot <martinp507@gmail.com>
2019-02-12 00:07:04 -0500

    (r) Added a todo (MP)

--- server/app/utils/apiRequest.ts
+++ server/app/utils/apiRequest.ts
@@ -7,6 +7,7 @@ import { R } from "../strings";
 export class ApiRequest {
 
     private static BASE_URL: string = `http://${Config.hostname}:${Config.port}`;
+    //TODO include this in the gamecard service
     public static async  getImagePairId(id: string): Promise<ICommonImagePair> {
         return Axios.get<ICommonImagePair>(`${ApiRequest.BASE_URL}/image-pair/${id}`)
         .then((response: AxiosResponse<ICommonImagePair>) => {
