cf13c010b4f963b94d027d6984538fb5b4840123
Laurent Barberis <laurent.barberis15@gmail.com>
2019-02-12 17:24:28 -0500

    (t) Fixed a bug for coverage (LB JM)

--- client/src/app/services/pixel-position.service.spec.ts
+++ client/src/app/services/pixel-position.service.spec.ts
@@ -1,4 +1,3 @@
-import { HttpErrorResponse } from "@angular/common/http";
 import { Message } from "../../../../common/communication/message";
 import { ICommonReveal } from "../../../../common/model/reveal";
 import { TestHelper } from "../../test.helper";
@@ -36,17 +35,20 @@ describe("UserService", () => {
     });
 
     it("should return an error message", () => {
-        const expectedMessageError: Message = { title: "Error", body: "No difference was found at the specified position" };
-        const mockHttpError: HttpErrorResponse = new HttpErrorResponse({
-            error: {
-                title: "Error", body: "No difference was found at the specified position",
+        const expectedResponse: Message = { title: "Error", body: "Miss" };
+        const mockID: string = "128391";
+        const mockX: number = 2;
+        const mockY: number = 3;
+        httpClientSpyPost.post.and.returnValue(TestHelper.asyncData(expectedResponse));
+
+        pixelPositionService.postPixelPosition(mockID, mockX, mockY).subscribe(
+            (response: Message) => {
+                expect(response.body).toEqual("Miss");
             },
-            status: 404 });
-        pixelPositionService.handleError(mockHttpError).subscribe(
-            (data) => {
-                expect(data).toEqual(expectedMessageError);
-            },
+            fail,
         );
+
+        expect(httpClientSpyPost.post.calls.count()).toBe(1, "one call");
     });
 
 });
