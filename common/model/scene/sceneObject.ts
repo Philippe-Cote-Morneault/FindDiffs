369d48f74e50816682266de349eeb44dae917995
Basil <bas98c@hotmail.com>
2019-02-13 15:59:49 -0500

    (d) Created position interface for models (BC)

--- common/model/scene/sceneObject.ts
+++ common/model/scene/sceneObject.ts
@@ -7,6 +7,12 @@ export interface ICommonSceneObject {
     texture: Textures;
 }
 
+export interface ICommonPositionObjects {
+    x: number;
+    y: number;
+    z: number;
+}
+
 export enum GeometricShapes { SPHERE = 0, CUBE = 1, CONE = 2, CYLINDER = 3, SQUARED_BASE_PYRAMID = 4, NUMBER_ELEMENTS = 5};
 
 // TODO: Define what texture to put
