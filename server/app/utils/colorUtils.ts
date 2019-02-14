ea2e9cd6c4d32000e63a863b029cc885b3555d5b
Basil Çapar <bas98c@hotmail.com>
2019-02-14 17:19:54 -0500

    (r) removed blank space (BC)

--- server/app/utils/colorUtils.ts
+++ server/app/utils/colorUtils.ts
@@ -1,5 +1,5 @@
 export class ColorUtils {
-    public static readonly MAX_COLOR: number = 16777215; // 0xFFFFFF (white) 
+    public static readonly MAX_COLOR: number = 16777215; // 0xFFFFFF (white)
 
     public static generateColor(): number {
         return Math.round(Math.random() * this.MAX_COLOR);
