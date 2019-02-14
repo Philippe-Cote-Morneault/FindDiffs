b17993605d402238566f4523abe56d9a2ccfe655
Phil <philbond1997@hotmail.com>
2019-02-14 17:01:31 -0500

    (t) Added mock for test. (PC)

--- server/app/services/scene/sceneDifferenceGenerator.spec.ts
+++ server/app/services/scene/sceneDifferenceGenerator.spec.ts
@@ -1,7 +1,26 @@
 import { expect } from "chai";
+import { ICommon3DPosition } from "../../../../common/model/positions";
+import { ICommonScene } from "../../../../common/model/scene/scene";
+import { GeometricShape, ICommonSceneObject } from "../../../../common/model/scene/sceneObject";
 import { SceneDifferenceGenerator } from "./sceneDifferenceGenerator";
 
 describe("SceneDifferenceGenerator", () => {
+    const sceneObjects: ICommonSceneObject[] = [
+        {
+            id: "1",
+            color: 293,
+            dimensions: [2, 2, 3],
+            type: GeometricShape.CUBE,
+            position: {
+                
+            },
+            texture: null,
+        },
+    ];
+
+    const originalScene: ICommonScene = {
+
+    }
     describe("generateModifiedScene", () => {
         const differenceGenerator: SceneDifferenceGenerator = new SceneDifferenceGenerator();
     });
