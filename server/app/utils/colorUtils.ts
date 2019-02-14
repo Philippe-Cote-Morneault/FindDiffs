0b82f2a7bb5106e43031f807f050cb7cda2c5bfa
Basil Çapar <bas98c@hotmail.com>
2019-02-14 17:12:49 -0500

    (r) changed name class and repository (BC)

---
+++ server/app/utils/colorUtils.ts
@@ -0,0 +1,7 @@
+export class ColorUtils {
+    public static readonly MAX_COLOR: number = 16777215; // 0xFFFFFF (white) 
+
+    public static generateColor(): number {
+        return Math.round(Math.random() * this.MAX_COLOR);
+    }
+}
