9420e0e2938e76e36f76c22efd04aad7bcf2f9e2
Basil Çapar <bas98c@hotmail.com>
2019-02-14 17:13:21 -0500

    (r) removed file (BC)

--- common/model/colorGenerator.ts
+++
@@ -1,7 +0,0 @@
-export class ColorGenerator {
-    public static readonly MAX_COLOR: number = 16777215; // 0xFFFFFF (white) 
-
-    public static generateColor(): number {
-        return Math.round(Math.random() * this.MAX_COLOR);
-    }
-}
