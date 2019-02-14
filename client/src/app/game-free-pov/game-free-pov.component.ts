c2fcc53af7d290df865e8ca88759326b1e3e2334
Felix <felix-atnoine.bourbonnais@polymtl.ca>
2019-02-14 11:18:28 -0500

    (r) Fixed basic tslint (FAB)

--- client/src/app/game-free-pov/game-free-pov.component.ts
+++ client/src/app/game-free-pov/game-free-pov.component.ts
@@ -1,9 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-game-free-pov',
-  templateUrl: './game-free-pov.component.html',
-  styleUrls: ['./game-free-pov.component.css']
+  selector: "app-game-free-pov",
+  templateUrl: "./game-free-pov.component.html",
+  styleUrls: ["./game-free-pov.component.css"],
 })
 export class GameFreePovComponent implements OnInit {
 
