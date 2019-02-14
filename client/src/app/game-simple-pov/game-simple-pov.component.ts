1bd20a14182a00ee1852df1aa241d52663dbfdc4
Felix <felix-atnoine.bourbonnais@polymtl.ca>
2019-02-14 11:14:43 -0500

    (d) Added game-simple-pov component (FAB)

---
+++ client/src/app/game-simple-pov/game-simple-pov.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-game-simple-pov',
+  templateUrl: './game-simple-pov.component.html',
+  styleUrls: ['./game-simple-pov.component.css']
+})
+export class GameSimplePovComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
