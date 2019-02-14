1fb587856d7d0bd11a9bfa82c604d03192d9087b
Felix <felix-atnoine.bourbonnais@polymtl.ca>
2019-02-14 11:19:57 -0500

    (r) Fixed app.module tslint (FAB)

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -8,14 +8,14 @@ import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { CreateGameFreeViewComponent } from "./create-game-free-view/create-game-free-view.component";
 import { CreateGameSimpleViewComponent } from "./create-game-simple-view/create-game-simple-view.component";
+import { GameFreePovComponent } from "./game-free-pov/game-free-pov.component";
+import { GameSimplePovComponent } from "./game-simple-pov/game-simple-pov.component";
 import { GameViewComponent } from "./game-view/game-view.component";
 import { GamesCardViewComponent } from "./games-card-view/games-card-view.component";
 import { GamesListViewComponent } from "./games-list-view/games-list-view.component";
 import { InitialViewComponent } from "./initial-view/initial-view.component";
 import { GameCardLoaderService } from "./services/game-card-loader.service";
 import { UserService } from "./services/user.service";
-import { GameSimplePovComponent } from './game-simple-pov/game-simple-pov.component';
-import { GameFreePovComponent } from './game-free-pov/game-free-pov.component';
 
 @NgModule({
     declarations: [
