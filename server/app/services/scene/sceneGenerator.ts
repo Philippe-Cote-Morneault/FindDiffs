655a5e682b4b5dc74b473d794dc203add1ef0a79
Basil <bas98c@hotmail.com>
2019-02-15 12:28:16 -0500

    (d/b) Implemented collisions (BC)

--- server/app/services/scene/sceneGenerator.ts
+++ server/app/services/scene/sceneGenerator.ts
@@ -33,14 +33,16 @@ export class SceneGenerator {
         const sceneObjects: ICommonSceneObject[] = new Array(this.totalShapes);
         const validPosition: boolean[] = new Array(Math.pow(this.sceneObjectPosition.sizeScene, CUBE));
         validPosition.fill(true);
+        let validPosition2: boolean[][][] = this.initializeArray(this.sceneObjectPosition.sizeScene);
         for (let i: number = 0; i < this.NUMBER_SHAPES_ENUM - 1; i++) {
             const numberShape: number = (this.shapes.get(i)) as number;
             if (numberShape) {
                 for (let j: number = 0; j < numberShape; j++) {
                     // tslint:disable-next-line
                     let positionObject: { modelPosition: ICommon3DPosition, position: number } = this.sceneObjectPosition.modelPosition();
-                    positionObject = this.waitUntilValidPosition(validPosition, positionObject);
+                    positionObject = this.waitUntilValidPosition(validPosition2, validPosition, positionObject);
                     validPosition[positionObject.position] = false;
+                    validPosition2 = this.updateArray(validPosition2, positionObject);
                     this.addModel(i, sceneObjects, positionObject);
                 }
             }
@@ -49,14 +51,93 @@ export class SceneGenerator {
         return sceneObjects;
     }
 
-    public waitUntilValidPosition(validPosition: boolean[], positionObject:
+    public initializeArray(sizeScene: number): boolean[][][] {
+        const array: boolean[][][] = [];
+
+        for (let i: number = 0; i < sizeScene; ++i) {
+            array[i] = [];
+        }
+
+        for (let i: number = 0; i < sizeScene; ++i) {
+            for (let j: number = 0; j < sizeScene; ++j) {
+                array[i][j] = [];
+            }
+        }
+
+        for (let i: number = 0; i < sizeScene; ++i) {
+            for (let j: number = 0; j < sizeScene; ++j) {
+                for (let k: number = 0; k < sizeScene; ++k) {
+                    array[i][j][k] = false;
+                }
+            }
+        }
+
+        return array;
+    }
+
+    public waitUntilValidPosition(validPosition2: boolean[][][], validPosition: boolean[], positionObject:
                                                             { modelPosition: ICommon3DPosition; position: number; }):
                                                             { modelPosition: ICommon3DPosition; position: number } {
         while (validPosition[positionObject.position] === false) {
             positionObject = this.sceneObjectPosition.modelPosition();
         }
 
+        // tslint:disable
+        while (validPosition2[positionObject.modelPosition.x][positionObject.modelPosition.y][positionObject.modelPosition.z] === false &&
+
+            validPosition2[positionObject.modelPosition.x + 1][positionObject.modelPosition.y][positionObject.modelPosition.z] === false &&
+            validPosition2[positionObject.modelPosition.x - 1][positionObject.modelPosition.y][positionObject.modelPosition.z] === false &&
+
+
+            validPosition2[positionObject.modelPosition.x][positionObject.modelPosition.y + 1][positionObject.modelPosition.z] === false &&
+            validPosition2[positionObject.modelPosition.x][positionObject.modelPosition.y - 1][positionObject.modelPosition.z] === false &&
+
+            validPosition2[positionObject.modelPosition.x][positionObject.modelPosition.y][positionObject.modelPosition.z + 1] === false &&
+            validPosition2[positionObject.modelPosition.x][positionObject.modelPosition.y][positionObject.modelPosition.z - 1] === false &&
+
+            validPosition2[positionObject.modelPosition.x + 1][positionObject.modelPosition.y + 1][positionObject.modelPosition.z] === false &&
+            validPosition2[positionObject.modelPosition.x + 1][positionObject.modelPosition.y - 1][positionObject.modelPosition.z] === false &&
+            validPosition2[positionObject.modelPosition.x - 1][positionObject.modelPosition.y + 1][positionObject.modelPosition.z] === false &&
+            validPosition2[positionObject.modelPosition.x - 1][positionObject.modelPosition.y - 1][positionObject.modelPosition.z] === false &&
+
+            validPosition2[positionObject.modelPosition.x + 1][positionObject.modelPosition.y][positionObject.modelPosition.z + 1] === false &&
+            validPosition2[positionObject.modelPosition.x + 1][positionObject.modelPosition.y][positionObject.modelPosition.z - 1] === false &&
+            validPosition2[positionObject.modelPosition.x - 1][positionObject.modelPosition.y][positionObject.modelPosition.z + 1] === false &&
+            validPosition2[positionObject.modelPosition.x - 1][positionObject.modelPosition.y][positionObject.modelPosition.z - 1] === false &&
+            
+            validPosition2[positionObject.modelPosition.x][positionObject.modelPosition.y + 1][positionObject.modelPosition.z + 1] === false &&
+            validPosition2[positionObject.modelPosition.x][positionObject.modelPosition.y + 1][positionObject.modelPosition.z - 1] === false &&
+            validPosition2[positionObject.modelPosition.x][positionObject.modelPosition.y - 1][positionObject.modelPosition.z + 1] === false &&
+            validPosition2[positionObject.modelPosition.x][positionObject.modelPosition.y - 1][positionObject.modelPosition.z - 1] === false &&
+
+            validPosition2[positionObject.modelPosition.x + 1][positionObject.modelPosition.y + 1][positionObject.modelPosition.z + 1] === false &&
+            validPosition2[positionObject.modelPosition.x + 1][positionObject.modelPosition.y + 1][positionObject.modelPosition.z - 1] === false &&
+            validPosition2[positionObject.modelPosition.x + 1][positionObject.modelPosition.y - 1][positionObject.modelPosition.z + 1] === false &&
+            validPosition2[positionObject.modelPosition.x + 1][positionObject.modelPosition.y - 1][positionObject.modelPosition.z - 1] === false &&
+    
+            validPosition2[positionObject.modelPosition.x - 1][positionObject.modelPosition.y + 1][positionObject.modelPosition.z + 1] ===false &&
+            validPosition2[positionObject.modelPosition.x - 1][positionObject.modelPosition.y + 1][positionObject.modelPosition.z - 1] === false &&
+            validPosition2[positionObject.modelPosition.x - 1][positionObject.modelPosition.y - 1][positionObject.modelPosition.z + 1] === false &&
+            validPosition2[positionObject.modelPosition.x - 1][positionObject.modelPosition.y - 1][positionObject.modelPosition.z - 1] === false) {
+                positionObject = this.sceneObjectPosition.modelPosition();
+        }
+
+
         return positionObject;
+    }
+
+    public updateArray(validPosition2: boolean[][][], positionObject:
+        { modelPosition: ICommon3DPosition; position: number; }): boolean[][][] {
+            const SIX: number = 6;
+            for (let i: number = positionObject.modelPosition.x; i < SIX; ++i) {
+                for (let j: number = positionObject.modelPosition.y; j < SIX; ++j) {
+                    for (let k: number = positionObject.modelPosition.z; k < SIX; ++k) {
+                        validPosition2[i + ((SIX % i) * -1)][j + ((SIX % j) * -1)][k + ((SIX % k) * -1)] = false;
+                    }
+                }
+            }
+
+            return validPosition2;
     }
 
     public addModel(type: number, sceneObjects: ICommonSceneObject[], positionObject:
