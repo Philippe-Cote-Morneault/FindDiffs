0bcec381ad9deb554c8f40baeec8b9efa4aef517
Laurent Barberis <laurent.barberis15@gmail.com>
2019-02-16 13:19:28 -0500

    (r) removed response.hit (LB)

--- client/src/app/services/pixel-restoration.service.ts
+++ client/src/app/services/pixel-restoration.service.ts
@@ -19,28 +19,27 @@ export class PixelRestorationService {
     public restoreImage(response: ICommonReveal,
                         originalCanvas: HTMLCanvasElement | null,
                         modifiedCanvas: HTMLCanvasElement | null): void {
-        if (response.hit) {
-            if (originalCanvas) {
-                this.originalContext = originalCanvas.getContext("2d");
-                if (this.originalContext) {
-                    const imageData: ImageData =
-                    this.originalContext.getImageData(0, 0, originalCanvas.width, originalCanvas.height);
-                    if (modifiedCanvas) {
-                        this.modifiedContext = modifiedCanvas.getContext("2d");
-                        if (this.modifiedContext) {
-                            const imageData2: ImageData =
-                            this.modifiedContext.getImageData(0, 0, modifiedCanvas.width, modifiedCanvas.height);
-                            let pos: number;
-                            response.pixels_affected.forEach((element) => {
-                                pos = this.pixelPositionInImageData(element, originalCanvas.width);
-                                imageData2.data[pos + RgbaPosition.R] = imageData.data[pos + RgbaPosition.R];
-                                imageData2.data[pos + RgbaPosition.G] = imageData.data[pos + RgbaPosition.G];
-                                imageData2.data[pos + RgbaPosition.B] = imageData.data[pos + RgbaPosition.B];
-                                imageData2.data[pos + RgbaPosition.A] = imageData.data[pos + RgbaPosition.A];
 
-                            });
-                            this.modifiedContext.putImageData(imageData2, 0 , 0);
-                        }
+        if (originalCanvas) {
+            this.originalContext = originalCanvas.getContext("2d");
+            if (this.originalContext) {
+                const imageData: ImageData =
+                    this.originalContext.getImageData(0, 0, originalCanvas.width, originalCanvas.height);
+                if (modifiedCanvas) {
+                    this.modifiedContext = modifiedCanvas.getContext("2d");
+                    if (this.modifiedContext) {
+                        const imageData2: ImageData =
+                            this.modifiedContext.getImageData(0, 0, modifiedCanvas.width, modifiedCanvas.height);
+                        let pos: number;
+                        response.pixels_affected.forEach((element) => {
+                            pos = this.pixelPositionInImageData(element, originalCanvas.width);
+                            imageData2.data[pos + RgbaPosition.R] = imageData.data[pos + RgbaPosition.R];
+                            imageData2.data[pos + RgbaPosition.G] = imageData.data[pos + RgbaPosition.G];
+                            imageData2.data[pos + RgbaPosition.B] = imageData.data[pos + RgbaPosition.B];
+                            imageData2.data[pos + RgbaPosition.A] = imageData.data[pos + RgbaPosition.A];
+
+                        });
+                        this.modifiedContext.putImageData(imageData2, 0, 0);
                     }
                 }
             }
