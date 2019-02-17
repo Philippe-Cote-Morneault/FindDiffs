3532239e3aaf934053649e0690db7da4cffcd028
Phil <philbond1997@hotmail.com>
2019-02-17 00:55:41 -0500

    (d) GeometricObject completly implemented. (PC)

--- client/src/app/services/scene/geometricObjects.ts/geometricObject.ts
+++ client/src/app/services/scene/geometricObjects.ts/geometricObject.ts
@@ -1,20 +1,23 @@
 import * as THREE from "three";
 import { ICommon3DPosition } from "../../../../../../common/model/positions";
 import { ICommonEulerAngles } from "../../../../../../common/model/scene/eulerAngles";
-import { ICommonSceneObject } from "../../../../../../common/model/scene/objects/sceneObject";
+import { ICommonGeometricObject } from "../../../../../../common/model/scene/objects/geometricObject";
 
 export abstract class GeometricObject {
-    public parse(sceneObject: ICommonSceneObject): THREE.Object3D {
-        const geometricObject: THREE.Object3D = this.createShape(sceneObject);
+    public parse(sceneObject: ICommonGeometricObject): THREE.Object3D {
+        const geometricObject: THREE.Object3D = this.createShape(this.createMeshMaterial(sceneObject.color),
+                                                                 sceneObject.dimensions);
 
         this.setPosition(sceneObject.position, geometricObject);
 
         this.setOrientation(sceneObject.orientation, geometricObject);
 
+        geometricObject.userData = {id: sceneObject.id};
+
         return geometricObject;
     }
 
-    protected abstract createShape(sceneObject: ICommonSceneObject): THREE.Object3D;
+    protected abstract createShape(color: THREE.MeshBasicMaterial, dimensions: number[]): THREE.Object3D;
 
     private setPosition(position: ICommon3DPosition, geometricObject: THREE.Object3D): void {
         geometricObject.position.set(position.x, position.y, position.z);
@@ -22,5 +25,10 @@ export abstract class GeometricObject {
 
     private setOrientation(eulerAngles: ICommonEulerAngles, geometricObject: THREE.Object3D): void {
         geometricObject.setRotationFromEuler(new THREE.Euler(eulerAngles.xAngle, eulerAngles.yAngle, eulerAngles.zAngle));
+    }
+
+    // TODO: See if you need to convert to hex
+    private createMeshMaterial(color: number): THREE.MeshBasicMaterial {
+        return new THREE.MeshBasicMaterial({color: color});
     }
 }
