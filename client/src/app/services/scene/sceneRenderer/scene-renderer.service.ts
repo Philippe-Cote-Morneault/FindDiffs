2c5b4206923492032d27eba7f960919287328939
Phil <philbond1997@hotmail.com>
2019-02-17 00:12:19 -0500

    (d) Finished SceneRendererService. (PC)

--- client/src/app/services/scene/sceneRenderer/scene-renderer.service.ts
+++ client/src/app/services/scene/sceneRenderer/scene-renderer.service.ts
@@ -1,8 +1,11 @@
 import { Injectable } from "@angular/core";
+import * as THREE from "three";
 
 @Injectable({
     providedIn: "root",
 })
 export class SceneRendererService {
-
+    public renderScene(scene: THREE.Scene, renderer: THREE.WebGLRenderer, camera: THREE.PerspectiveCamera): void {
+        renderer.render(scene, camera);
+    }
 }
