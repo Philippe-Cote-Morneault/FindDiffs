875fa9f441a39f4db161abc7852af201af69f663
Phil <philbond1997@hotmail.com>
2019-02-16 21:28:28 -0500

    (r) Split scene interface. (PC)

--- common/model/scene/scene.ts
+++ common/model/scene/scene.ts
@@ -1,14 +1,22 @@
-import { ICommonSceneObject } from "./objects/sceneObject";
+import { ICommonGeometricShape } from "./objects/geometricShape";
+import { ICommonThematicObject } from "./objects/thematic";
 
 /**
  * Represents a ThreeJS scene
  */
 export interface ICommonScene {
-    sceneObjects: ICommonSceneObject[];
-    bg_color: string;
-    texture: string;
     id: string;
     dimensions: ICommonSceneDimensions;
+}
+
+export interface ICommonGeometricScene extends ICommonScene {
+    bg_color: string;
+    sceneObjects: ICommonGeometricShape[];
+}
+
+export interface ICommonThematicScene extends ICommonScene {
+    texture: string;
+    sceneObjects: ICommonThematicObject[];
 }
 
 /**
