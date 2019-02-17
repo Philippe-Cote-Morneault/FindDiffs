e8ea9870d8b37f9b7d54f5096e10a3c35744317f
Phil <philbond1997@hotmail.com>
2019-02-17 12:25:51 -0500

    (t) Added scene mock. (PC)

--- server/app/services/scene/sceneDifferenceGenerator.spec.ts
+++ server/app/services/scene/sceneDifferenceGenerator.spec.ts
@@ -1,10 +1,16 @@
-/*
 import { expect } from "chai";
 import { SceneDifferenceGenerator } from "./sceneDifferenceGenerator";
+import { ICommonScene } from "../../../../common/model/scene/scene";
 
 describe("SceneDifferenceGenerator", () => {
+    const sceneMockValid: ICommonScene = {
+
+    };
     describe("generateModifiedScene", () => {
         const differenceGenerator: SceneDifferenceGenerator = new SceneDifferenceGenerator();
+        it("Should", () => {
+            //differenceGenerator.generateModifiedScene()
+        });
     });
 });
-*/
+
