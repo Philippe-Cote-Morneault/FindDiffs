f19e05c88ae86be32494191f78a93ffde7efcf42
Phil <philbond1997@hotmail.com>
2019-02-17 16:51:56 -0500

    (d) Implemented CylinderFactory. (PC)

---
+++ server/app/services/scene/shapeCreation/cylinderFactory.ts
@@ -0,0 +1,19 @@
+import { ICommonCylinder } from "../../../../../common/model/scene/objects/geometricObjects/cylinder";
+import { GeometricShapeType, ICommonGeometricObject } from "../../../../../common/model/scene/objects/geometricObjects/geometricObject";
+import { GeometricObjectFactory } from "./geometricObjectFactory";
+
+export class CylinderFactory extends GeometricObjectFactory {
+    private static readonly REFERENCE_RADIUS: number = 2;
+    private static readonly REFERENCE_HEIGHT: number = 3;
+
+    protected createShape(geometricObjet: ICommonGeometricObject): ICommonCylinder {
+        const scalePercentage: number = this.generateRandomPercentage();
+        const cylinderShape: ICommonCylinder = geometricObjet as ICommonCylinder;
+        cylinderShape.shapeType = GeometricShapeType.CYLINDER;
+
+        cylinderShape.radius = scalePercentage * CylinderFactory.REFERENCE_RADIUS;
+        cylinderShape.height = scalePercentage * CylinderFactory.REFERENCE_HEIGHT;
+
+        return cylinderShape;
+    }
+}
