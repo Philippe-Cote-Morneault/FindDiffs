706d7e87093be4b2ce605fa954d9cc637a276b07
Phil <philbond1997@hotmail.com>
2019-02-17 16:46:35 -0500

    (r) Fixed lint errors. (PC)

--- server/app/services/scene/shapeCreation/sphereFactory.ts
+++ server/app/services/scene/shapeCreation/sphereFactory.ts
@@ -1,6 +1,6 @@
-import { GeometricObjectFactory } from "./geometricObjectFactory";
+import { GeometricShapeType, ICommonGeometricObject } from "../../../../../common/model/scene/objects/geometricObjects/geometricObject";
 import { ICommonSphere } from "../../../../../common/model/scene/objects/geometricObjects/sphere";
-import { ICommonGeometricObject, GeometricShapeType } from "../../../../../common/model/scene/objects/geometricObjects/geometricObject";
+import { GeometricObjectFactory } from "./geometricObjectFactory";
 
 export class SphereFactory extends GeometricObjectFactory {
     private static readonly REFERENCE_RADIUS: number = 3;
