d9637a6eceb974a7ff4967eab32fc8a4aeeb42a1
Phil <philbond1997@hotmail.com>
2019-02-17 16:32:56 -0500

    (r) Fixed import order. (PC)

--- server/app/services/scene/shapeCreation/geometricObjectFactory.ts
+++ server/app/services/scene/shapeCreation/geometricObjectFactory.ts
@@ -2,9 +2,9 @@ import { v4 as uuid } from "uuid";
 import { ICommon3DPosition } from "../../../../../common/model/positions";
 import { ICommonEulerAngles } from "../../../../../common/model/scene/eulerAngles";
 import { ICommonGeometricObject } from "../../../../../common/model/scene/objects/geometricObjects/geometricObject";
-import { NumberGenerationUtils } from "../../../utils/numerGenerationUtils";
 import { ObjectType } from "../../../../../common/model/scene/scene";
 import { ColorUtils } from "../../../utils/colorUtils";
+import { NumberGenerationUtils } from "../../../utils/numerGenerationUtils";
 
 export abstract class GeometricObjectFactory {
     private static readonly SIZE_MAX_PERCENTAGE: number = 150;
