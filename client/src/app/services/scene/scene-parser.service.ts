f51097ab383d81bc83680283c87be7204b159c80
Phil <philbond1997@hotmail.com>
2019-02-16 20:26:51 -0500

    (d) Added scene color generation. (PC)

--- client/src/app/services/scene/scene-parser.service.ts
+++ client/src/app/services/scene/scene-parser.service.ts
@@ -1,17 +1,26 @@
 import { Injectable } from "@angular/core";
-import { ICommonScene } from "../../../../../common/model/scene/scene";
 import * as THREE from "three";
+import { ICommonScene } from "../../../../../common/model/scene/scene";
 
 @Injectable({
   providedIn: "root",
 })
 export class SceneParserService {
 
-  	public constructor() { 
+    public parseScene(originalScene: ICommonScene): THREE.Scene {
+        const scene: THREE.Scene = this.generateSceneObject(parseInt(originalScene.bg_color));
 
-	}
+        return scene;
+    }
 
-	public parseScene(scene: ICommonScene): Scene {
-		
-	}
+    private generateGrid(size: number, divisions: number): THREE.GridHelper {
+        return new THREE.GridHelper(size, divisions);
+    }
+
+    private generateSceneObject(bgColor: number): THREE.Scene {
+        const scene: THREE.Scene = new Scene();
+        scene.background = new THREE.Color(bgColor);
+
+        return scene;
+    }
 }
--- client/src/app/util/colorsUtils.ts
+++
@@ -1,9 +0,0 @@
-
-export class ColorsUtils {
-    private static readonly HEX_NUMBER_BASE: number = 16;
-    private static readonly HEX_NUMBER_OF_DIGITS: number = 6;
-
-    public static generateRandomColor(): string {
-        return "#" + Math.random().toString(ColorsUtils.HEX_NUMBER_BASE).substr(-ColorsUtils.HEX_NUMBER_OF_DIGITS);
-    }
-}
