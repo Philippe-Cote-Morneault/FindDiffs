7ecb4a0258c6787efeef0e2f6a9e7514402ff592
Phil <philbond1997@hotmail.com>
2019-02-17 17:15:50 -0500

    (d) Implemented setFactories(). (PC)

--- server/app/services/scene/shapeCreation/geometricObjectGenerator.ts
+++ server/app/services/scene/shapeCreation/geometricObjectGenerator.ts
@@ -1,14 +1,18 @@
-//import { CubeFactory } from "./cubeFactory";
-//import { GeometricObjectFactory } from "./geometricObjectFactory";
-//import { ICommonEulerAngles } from "../../../../../common/model/scene/eulerAngles";
-//import { ICommonGeometricObject } from "../../../../../common/model/scene/objects/geometricObjects/geometricObject";
+import { CubeFactory } from "./cubeFactory";
+import { GeometricObjectFactory } from "./geometricObjectFactory";
+import { ICommonEulerAngles } from "../../../../../common/model/scene/eulerAngles";
+import { ICommonGeometricObject } from "../../../../../common/model/scene/objects/geometricObjects/geometricObject";
+import { ConeFactory } from "./coneFactory";
+import { CylinderFactory } from "./cylinderFactory";
+import { PyramidFactory } from "./pyramidFactory";
+import { SphereFactory } from "./sphereFactory";
 
 export class GeometricObjectGenerator {
     private static instance: GeometricObjectGenerator;
-   // private geometricShapeFactories: GeometricObjectFactory[] = [];
+    private geometricShapeFactories: GeometricObjectFactory[] = [];
 
     public constructor() {
-        //
+        this.setFactories();
     }
 
     public static getInstance(): GeometricObjectGenerator {
@@ -19,14 +23,16 @@ export class GeometricObjectGenerator {
         return GeometricObjectGenerator.instance;
     }
 
-    /*
+
     public createObject(position: ICommonEulerAngles): ICommonGeometricObject {
 
     }
 
     private setFactories(): void {
         this.geometricShapeFactories.push(new CubeFactory());
-
+        this.geometricShapeFactories.push(new ConeFactory());
+        this.geometricShapeFactories.push(new CylinderFactory());
+        this.geometricShapeFactories.push(new PyramidFactory());
+        this.geometricShapeFactories.push(new SphereFactory());
     }
-    */
 }
