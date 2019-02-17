5745f347932c78c20da8036f28a6094720614d4d
Felix <felix-atnoine.bourbonnais@polymtl.ca>
2019-02-17 17:29:59 -0500

    (r) Modified return type of post request (FAB)

--- client/src/app/services/scene-pair.service.ts
+++ client/src/app/services/scene-pair.service.ts
@@ -2,6 +2,8 @@ import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Observable } from "rxjs";
 import { catchError } from "rxjs/operators";
+import { Message } from "../../../../common/communication/message";
+import { ICommonScene } from "../../../../common/model/scene/scene";
 import { SERVER_URL } from "../../../../common/url";
 import { HTTPService } from "./HTTP.service";
 
@@ -15,16 +17,16 @@ export class ScenePairService extends HTTPService {
         super();
         this.http = http;
     }
-    // TODO: What does it return? (replace any)
+
     public addScenePair(gameName: string, objectType: string,
                         quantity: number, isAddModif: boolean,
-                        isRemoveModif: boolean, isModifiedModif: boolean): Observable<any> {
+                        isRemoveModif: boolean, isModifiedModif: boolean): Observable<ICommonScene | Message> {
 
         const requestBody: Object = {"name": gameName, "object_type": objectType,
                                      "object_qty": quantity, "add": isAddModif,
                                      "remove": isRemoveModif, "modified": isModifiedModif };
 
-        return this.http.post<any>(`${SERVER_URL}/scene/`, requestBody).pipe(
+        return this.http.post<ICommonScene>(`${SERVER_URL}/scene/`, requestBody).pipe(
             catchError((error) => this.handleError(error)),
         );
     }
