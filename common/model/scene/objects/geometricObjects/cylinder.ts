8ee24667d7bfb1ba4be66709f01f64faf5560764
Phil <philbond1997@hotmail.com>
2019-02-17 12:53:18 -0500

    (d) Added ICommonCylinder. (PC)

---
+++ common/model/scene/objects/geometricObjects/cylinder.ts
@@ -0,0 +1,7 @@
+import { ICommonGeometricObject } from "./geometricObject";
+
+export interface ICommonCylinder extends ICommonGeometricObject {
+    radius: number;
+    height: number;
+    radialSegments: number;
+}
