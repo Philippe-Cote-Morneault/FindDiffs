d6dbb9ef168efea2d396cee284e5a3ef1ddff616
Phil <philbond1997@hotmail.com>
2019-02-17 18:34:51 -0500

    (d) Implemented scene service. (PC)

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -5,9 +5,11 @@ import { AdminViewComponent } from "./admin-view/admin-view.component";
 import { GameViewComponent } from "./game-view/game-view.component";
 import { GamesListViewComponent } from "./games-list-view/games-list-view.component";
 import { InitialViewComponent } from "./initial-view/initial-view.component";
+import { TestComponent } from "./test/test.component";
 
 const routes: Routes = [
-    { path: "", component: InitialViewComponent },
+   // { path: "", component: InitialViewComponent },
+    { path: "", component: TestComponent},
     { path: "admin", component: AdminViewComponent },
     { path: "gamesList", component: GamesListViewComponent },
     { path: "game/:id", component: GameViewComponent },
