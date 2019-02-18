a20ac7912d827a5d0334396c231a1c876b709d13
Phil <philbond1997@hotmail.com>
2019-02-18 15:45:01 -0500

    (r) Changed camera height to be in y axis. (PC)

--- client/src/app/services/scene/sceneRenderer/cameraGenerator.ts
+++ client/src/app/services/scene/sceneRenderer/cameraGenerator.ts
@@ -4,8 +4,7 @@ export class CameraGenerator {
     private static readonly FOV: number = 90;
     private static readonly NEAR_CLIP: number = 0.1;
     private static readonly FAR_CLIP: number = 1000;
-    private static readonly CAMERA_HEIGHT: 20;
-
+    private static readonly CAMERA_HEIGHT: number = 10;
     public static createCamera(containerWidth: number, containerHeight: number): THREE.PerspectiveCamera {
         const camera: THREE.PerspectiveCamera =  new THREE.PerspectiveCamera(
             CameraGenerator.FOV,
@@ -14,7 +13,7 @@ export class CameraGenerator {
             CameraGenerator.FAR_CLIP,
         );
 
-        camera.position.z = CameraGenerator.CAMERA_HEIGHT;
+        camera.position.y = CameraGenerator.CAMERA_HEIGHT;
 
         return camera;
     }
