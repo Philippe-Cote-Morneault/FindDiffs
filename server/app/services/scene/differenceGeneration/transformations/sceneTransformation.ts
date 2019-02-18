1e6fe8b5c29f1d2c16f8e28e2ce2677308b71e52
Martin Pouliot <martinp507@gmail.com>
2019-02-17 21:05:43 -0500

    (r) decoupled scene from interface (MP)

--- server/app/services/scene/differenceGeneration/transformations/sceneTransformation.ts
+++ server/app/services/scene/differenceGeneration/transformations/sceneTransformation.ts
@@ -1,11 +1,10 @@
 import { ICommonSceneModifications } from "../../../../../../common/model/scene/modifications/sceneModifications";
 import { ICommonSceneObject } from "../../../../../../common/model/scene/objects/sceneObject";
-import { ICommonScene } from "../../../../../../common/model/scene/scene";
 
 /**
  * Represents a transformation to apply to a scene
  */
 export interface SceneTransformation {
-   applyTransformation(modifiedScene: ICommonScene, transformationEligibleObjects: ICommonSceneObject[],
+   applyTransformation(objectsToTransform: ICommonSceneObject[],
                        modifications: ICommonSceneModifications): void;
 }
