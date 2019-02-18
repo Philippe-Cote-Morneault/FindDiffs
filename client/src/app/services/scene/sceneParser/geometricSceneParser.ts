fa91600d2c0b86deb0f6eed7dfc5e8d58c8534e8
Martin Pouliot <martinp507@gmail.com>
2019-02-17 22:35:34 -0500

    (d) Added lights to the scene (MP)

--- client/src/app/services/scene/sceneParser/geometricSceneParser.ts
+++ client/src/app/services/scene/sceneParser/geometricSceneParser.ts
@@ -13,6 +13,20 @@ export class GeometricSceneParser {
         // TODO: Find a way to hold dimensions and divisions in scene object
         scene.add(GeometricSceneParser.generateGrid(originalScene.dimensions.x, 10));
 
+        const light1: THREE.PointLight = new THREE.PointLight( 0xffffff, 5, 350 );
+        light1.position.set( 150, 100, 150 );
+        scene.add(light1);
+
+        const light2: THREE.PointLight = new THREE.PointLight( 0xffffff, 5, 350 );
+        light2.position.set( -150, 100, 150 );
+        scene.add(light2);
+
+        const light3: THREE.PointLight = new THREE.PointLight( 0xffffff, 2, 300 );
+        light3.position.set( 0, 100, -150 );
+        scene.add(light3);
+
+        const ambiantLight = new THREE.AmbientLight(0x303030); // soft white light
+        scene.add( ambiantLight );
         return scene;
     }
 
