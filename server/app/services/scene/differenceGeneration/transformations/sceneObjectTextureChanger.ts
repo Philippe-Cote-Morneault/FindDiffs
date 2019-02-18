9384aae65c9ba533c4abac7c8e1c1a88bb0ddcd7
Martin Pouliot <martinp507@gmail.com>
2019-02-17 21:05:07 -0500

    (r) Changed a variable name and decoupled scene (MP)

--- server/app/services/scene/differenceGeneration/transformations/sceneObjectTextureChanger.ts
+++ server/app/services/scene/differenceGeneration/transformations/sceneObjectTextureChanger.ts
@@ -1,10 +1,9 @@
 import { ICommonThematicModifications } from "../../../../../../common/model/scene/modifications/thematicModifications";
 import { ICommonThematicObject } from "../../../../../../common/model/scene/objects/thematicObjects/thematicObject";
-import { ICommonScene } from "../../../../../../common/model/scene/scene";
 import { SceneTransformation } from "./sceneTransformation";
 
 export class SceneObjectTextureChanger implements SceneTransformation {
-    public applyTransformation(modifiedScene: ICommonScene, transformationEligibleObjects: ICommonThematicObject[],
+    public applyTransformation(objectsToTransform: ICommonThematicObject[],
                                modifications: ICommonThematicModifications): void {
 
         // tslint:disable-next-line:no-suspicious-comment
