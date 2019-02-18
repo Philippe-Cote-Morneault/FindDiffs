77c2308820fc09ed170bef2d24f57de0c4621b51
Phil <philbond1997@hotmail.com>
2019-02-18 12:27:39 -0500

    (r) Changed camera height. (PC)

--- client/src/app/services/scene/sceneRenderer/cameraGenerator.ts
+++ client/src/app/services/scene/sceneRenderer/cameraGenerator.ts
@@ -4,6 +4,7 @@ export class CameraGenerator {
     private static readonly FOV: number = 90;
     private static readonly NEAR_CLIP: number = 0.1;
     private static readonly FAR_CLIP: number = 1000;
+    private static readonly CAMERA_HEIGHT: 20;
 
     public static createCamera(containerWidth: number, containerHeight: number): THREE.PerspectiveCamera {
         const camera: THREE.PerspectiveCamera =  new THREE.PerspectiveCamera(
@@ -13,7 +14,7 @@ export class CameraGenerator {
             CameraGenerator.FAR_CLIP,
         );
 
-        camera.position.z = 50;
+        camera.position.z = CameraGenerator.CAMERA_HEIGHT;
 
         return camera;
     }
