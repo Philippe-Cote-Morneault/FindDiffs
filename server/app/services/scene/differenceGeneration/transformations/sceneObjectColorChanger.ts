2fcb2cfc9a63a85b5073ed0748239bc36d4f407d
Martin Pouliot <martinp507@gmail.com>
2019-02-17 21:04:11 -0500

    (r) Decoupled class scene and changed a variable name (MP)

--- server/app/services/scene/differenceGeneration/transformations/sceneObjectColorChanger.ts
+++ server/app/services/scene/differenceGeneration/transformations/sceneObjectColorChanger.ts
@@ -1,6 +1,5 @@
 import { ICommonGeometricModifications } from "../../../../../../common/model/scene/modifications/geometricModifications";
 import { ICommonGeometricObject } from "../../../../../../common/model/scene/objects/geometricObjects/geometricObject";
-import { ICommonScene } from "../../../../../../common/model/scene/scene";
 import { ColorUtils } from "../../../../utils/colorUtils";
 import { SceneTransformation } from "./sceneTransformation";
 
@@ -9,14 +8,14 @@ import { SceneTransformation } from "./sceneTransformation";
  */
 export class SceneObjectColorChanger implements SceneTransformation {
 
-    public applyTransformation(modifedScene: ICommonScene, transformationEligibleObjects: ICommonGeometricObject[],
+    public applyTransformation(objectsToTransform: ICommonGeometricObject[],
                                modifications: ICommonGeometricModifications): void {
 
-        const indexOfObject: number = Math.floor(Math.random() * transformationEligibleObjects.length);
-        const modifiedObject: ICommonGeometricObject = transformationEligibleObjects[indexOfObject];
+        const indexOfObject: number = Math.floor(Math.random() * objectsToTransform.length);
+        const modifiedObject: ICommonGeometricObject = objectsToTransform[indexOfObject];
 
         modifications.colorChangedObjects.set(modifiedObject.id, ColorUtils.generateRandomColor());
-        transformationEligibleObjects.splice(indexOfObject, 1);
+        objectsToTransform.splice(indexOfObject, 1);
     }
 
 }
