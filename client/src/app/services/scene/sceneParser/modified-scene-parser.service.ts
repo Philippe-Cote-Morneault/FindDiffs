50eb250ce478184a8396c26fe82e6d6131c98680
Phil <philbond1997@hotmail.com>
2019-02-18 21:25:28 -0500

    (r) Refactored scene parsers. (PC)

--- client/src/app/services/scene/sceneParser/modified-scene-parser.service.ts
+++ client/src/app/services/scene/sceneParser/modified-scene-parser.service.ts
@@ -1,55 +1,61 @@
 import { Injectable } from "@angular/core";
+import { InvalidFormatException } from "../../../../../../common/errors/invalidFormatException";
+import { ICommonGeometricModifications } from "../../../../../../common/model/scene/modifications/geometricModifications";
 import { ICommonSceneModifications } from "../../../../../../common/model/scene/modifications/sceneModifications";
+import { ICommonGeometricObject } from "../../../../../../common/model/scene/objects/geometricObjects/geometricObject";
 import { ICommonSceneObject } from "../../../../../../common/model/scene/objects/sceneObject";
-import { ICommonGeometricScene, ICommonScene, ICommonThematicScene, ObjectType } from "../../../../../../common/model/scene/scene";
-import { GeometricSceneParser } from "./geometricSceneParser";
-import { GeometricObjectParser } from "./objectParser/geometricObjectParser";
-import { ThematicObjectParser } from "./objectParser/thematicObjectParser";
-import { SceneObjectParser } from "./sceneObjectParser";
-import { ThematicSceneParser } from "./thematicSceneParser";
-import { SceneParserService } from "./scene-parser.service";
+import { ICommonScene, ObjectType } from "../../../../../../common/model/scene/scene";
+import { AbstractSceneParser } from "./abstractSceneParserService";
 
 @Injectable({
     providedIn: "root",
 })
-export class ModifiedSceneParserService {
+export class ModifiedSceneParserService extends AbstractSceneParser {
 
-    private sceneObjectParser: SceneObjectParser;
+    public parseModifiedScene(originalSceneModel: ICommonScene, sceneModifications: ICommonSceneModifications): THREE.Scene {
+        const scene: THREE.Scene = this.createScene(originalSceneModel);
 
-    public parseModifiedScene(originalScene: ICommonScene, sceneModifications: ICommonSceneModifications): THREE.Scene {
-        let scene: THREE.Scene;
+        originalSceneModel.type === ObjectType.Geometric ?
+            this.parseGeometricObjects(scene, sceneModifications as ICommonGeometricModifications,
+                                       originalSceneModel.sceneObjects as ICommonGeometricObject[]) :
+            this.parseThematicObjects(scene, sceneModifications, originalSceneModel.sceneObjects);
 
-        if (originalScene.type === ObjectType.Geometric) {
-            this.sceneObjectParser = new GeometricObjectParser();
-            scene = GeometricSceneParser.parseScene(originalScene as ICommonGeometricScene);
-        } else {
-            this.sceneObjectParser = new ThematicObjectParser();
-            scene = ThematicSceneParser.parseScene(originalScene as ICommonThematicScene);
-        }
-
-        this.parseObjects(scene, sceneModifications, originalScene.sceneObjects);
+        this.addAddedObjects(scene, sceneModifications.addedObjects);
 
         return scene;
     }
 
-    private parseObjects(scene: THREE.Scene, sceneModifications: ICommonSceneModifications,
-                         originalSceneObjects: ICommonSceneObject[]): void {
+    private parseThematicObjects(scene: THREE.Scene, sceneModifications: ICommonSceneModifications,
+                                 originalSceneObjects: ICommonSceneObject[]): void {
 
-        originalSceneObjects.forEach((sceneObject => {
-            if (sceneModifications.deletedObjects.includes(sceneObject.id)) {
-
-            } 
-            else if (sceneModifications)
-
-            }
-        }))
-        this.deleteSceneObjects(scene.children, sceneModifications.deletedObjects);
+        // tslint:disable-next-line:no-suspicious-comment
+        // TODO: Implement this in sprint 3
     }
 
-    private deleteSceneObjects(sceneObjects: THREE.Object3D[], objectIdsToDelete: String[]): void {
-        objectIdsToDelete.forEach((objectIdToDelete: String) => {
+    private parseGeometricObjects(scene: THREE.Scene, sceneModifications: ICommonGeometricModifications,
+                                  originalSceneObjects: ICommonGeometricObject[]): void {
 
-        })
+        originalSceneObjects.forEach((object: ICommonGeometricObject) => {
+            if (!sceneModifications.deletedObjects.includes(object.id)) {
+                if (sceneModifications.colorChangedObjects.has(object.id)) {
+                    this.changeObjectColor(object, sceneModifications.colorChangedObjects.get(object.id));
+                }
+                scene.add(this.sceneObjectParser.parse(object));
+            }
+        });
+    }
+
+    private addAddedObjects(scene: THREE.Scene, objectsToAdd: ICommonSceneObject[]): void {
+        objectsToAdd.forEach((object: ICommonSceneObject) => {
+            scene.add(this.sceneObjectParser.parse(object));
+        });
+    }
+
+    private changeObjectColor(objectToModify: ICommonGeometricObject, color: number | undefined): void {
+        if (color === undefined) {
+            throw new InvalidFormatException("Color not valid!");
+        }
+        objectToModify.color = color;
     }
 
 
