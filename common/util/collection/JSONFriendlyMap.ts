6b5b11f23d9f4e0e58a9dc5e502825edea320906
Phil <philbond1997@hotmail.com>
2019-02-19 19:47:32 -0500

    (d) Implemented has() of JSONFriendlyMap. (PC)

--- common/util/collection/JSONFriendlyMap.ts
+++ common/util/collection/JSONFriendlyMap.ts
@@ -1,7 +1,23 @@
 
 // TODO: Implement hashing if we have time
 export class JSONFriendlyMap<K, V> {
+    public size: number;
+
     private pairs: Pair<K, V>[];
+
+    public clear(): void {
+
+    }
+
+    public has(key: K): boolean {
+        this.pairs.forEach((pair: Pair<K, V>) => {
+            if (pair.key === key) {
+                return true;
+            }
+        });
+
+        return false;
+    }
 }
 
 class Pair<K, V> {
