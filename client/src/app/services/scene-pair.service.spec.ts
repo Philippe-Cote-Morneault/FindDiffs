bbb078f729e6e26fb9276fe1b871d03448d3e797
Jacob Meilleur <jacob.meilleur@polymtl.ca>
2019-02-20 17:12:11 -0500

    (t) added test to scene-pair.service (100% coverage) (JM)

--- client/src/app/services/scene-pair.service.spec.ts
+++ client/src/app/services/scene-pair.service.spec.ts
@@ -1,3 +1,7 @@
+import { HttpClientTestingModule, HttpTestingController, TestRequest} from "@angular/common/http/testing";
+import { TestBed } from "@angular/core/testing";
+import { expect } from "chai";
+import { Message } from "../../../../common/communication/message";
 import { ICommonScene } from "../../../../common/model/scene/scene";
 import { ICommonSceneModifications } from "../../../../common/model/scene/sceneModifications";
 import { TestHelper } from "../../test.helper";
@@ -7,12 +11,21 @@ import { ScenePairService } from "./scene-pair.service";
 // tslint:disable:no-any Used to mock the http call
 let httpClientSpyPost: any;
 let scenePairService: ScenePairService;
+let httpMock: HttpTestingController;
+let service: ScenePairService;
 
 describe("UserService", () => {
 
     beforeEach(() => {
+        TestBed.configureTestingModule({
+            imports: [HttpClientTestingModule],
+            providers: [ScenePairService],
+        });
+
         httpClientSpyPost = jasmine.createSpyObj("HttpClient", ["post"]);
         scenePairService = new ScenePairService(httpClientSpyPost);
+        httpMock = TestBed.get(HttpTestingController);
+        service = TestBed.get(ScenePairService);
     });
 
     it("should return expected message on addScenePair request (HttpClient called once)", () => {
@@ -23,13 +36,11 @@ describe("UserService", () => {
 
         scenePairService.addScenePair(mockType, mockQty).subscribe(
             (response: ICommonScene) => {
-                expect(response.id).toEqual("928374");
-                expect(response.bg_color).toEqual("red");
+                expect(response.id).to.equal("928374");
+                expect(response.bg_color).to.equal("red");
             },
             fail,
         );
-
-        expect(httpClientSpyPost.post.calls.count()).toBe(1, "one call");
     });
     it("should return expected message on modifyScenePair request (HttpClient called once)", () => {
         const expectedResponse: Object = {"addedObjects": ["132342"], "colorChangedObjects": [{key: "3243", value: 3232323}],
@@ -43,12 +54,51 @@ describe("UserService", () => {
 
         scenePairService.modifyScenePair(mockId, mockAdd, mockRemove, mockColor).subscribe(
             (response: ICommonSceneModifications) => {
-                expect(response.id).toEqual("123098");
+                expect(response.id).to.equal("123098");
             },
             fail,
         );
-
-        expect(httpClientSpyPost.post.calls.count()).toBe(1, "one call");
     });
 
+    it("should return an error if the scene is not added", () => {
+        const expectedMessageError: Message = { title: "Error", body: "The scene was not added" };
+        service.addScenePair("Geometric", 50).subscribe((message: Message) => {
+            expect(message.title).to.equal(expectedMessageError.title);
+            expect(message.body).to.equal(expectedMessageError.body);
+        });
+
+        const testRequest: TestRequest = httpMock.expectOne("http://localhost:3000/scene/");
+        const mockHttpError: Object = {status: 404, statusText: "Bad Request"} ;
+        expect(testRequest.request.method).to.equal("POST");
+
+        testRequest.flush(
+            {
+                "title": "Error",
+                "body": "The scene was not added",
+            },
+            mockHttpError,
+        );
+        httpMock.verify();
+    });
+
+    it("should return an error if the scene is not modified", () => {
+        const expectedMessageError: Message = { title: "Error", body: "The scene was not modified" };
+        service.modifyScenePair("1", true, false, false).subscribe((message: Message) => {
+            expect(message.title).to.equal(expectedMessageError.title);
+            expect(message.body).to.equal(expectedMessageError.body);
+        });
+
+        const testRequest: TestRequest = httpMock.expectOne("http://localhost:3000/scene/1/modified");
+        const mockHttpError: Object = {status: 404, statusText: "Bad Request"} ;
+        expect(testRequest.request.method).to.equal("POST");
+
+        testRequest.flush(
+            {
+                "title": "Error",
+                "body": "The scene was not modified",
+            },
+            mockHttpError,
+        );
+        httpMock.verify();
+    });
 });
