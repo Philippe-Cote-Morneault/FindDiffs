0608a5882400715571c8ba74d93751fa3fd8db32
Basil <bas98c@hotmail.com>
2019-02-21 01:21:49 -0500

    (t) Added a test when we are deleting objects from the original scene (BC)

--- server/app/services/scene/differenceGeneration/sceneDifferenceGenerator.spec.ts
+++ server/app/services/scene/differenceGeneration/sceneDifferenceGenerator.spec.ts
@@ -21,5 +21,19 @@ describe("GeometricObjectGenerator", () => {
 
             expect(differenceObj.addedObjects.length + sceneObj.sceneObjects.length).to.be.gte(sceneObj.sceneObjects.length);
         });
+        it("Should return an array of objects which is lower than the original", () => {
+            const SCENE_SIZE: number = 1000;
+            const SCENE_OBJECT_MARGIN: number = 20;
+            const grid: DefaultGrid = new DefaultGrid(SCENE_SIZE, SCENE_SIZE, SCENE_OBJECT_MARGIN);
+
+            // tslint:disable:no-magic-numbers
+            const generateScene: SceneGenerator = new SceneGenerator(100);
+            const sceneObj: ICommonScene = generateScene.generateScene();
+
+            const differenceGenerator: SceneDifferenceGenerator = new SceneDifferenceGenerator(sceneObj, grid);
+            const differenceObj: ICommonSceneModifications = differenceGenerator.generateModifiedScene(false, true, false);
+
+            expect(sceneObj.sceneObjects.length - differenceObj.addedObjects.length).to.be.lte(sceneObj.sceneObjects.length);
+        });
     });
 });
