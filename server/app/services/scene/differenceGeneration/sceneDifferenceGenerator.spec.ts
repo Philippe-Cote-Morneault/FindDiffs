ac9bcc5621f9c56912b317e04b1ec870503d35cb
Basil <bas98c@hotmail.com>
2019-02-21 12:35:53 -0500

    (t) added a test for the modification of the color (BC)

--- server/app/services/scene/differenceGeneration/sceneDifferenceGenerator.spec.ts
+++ server/app/services/scene/differenceGeneration/sceneDifferenceGenerator.spec.ts
@@ -1,5 +1,6 @@
 import { expect } from "chai";
 import { ICommonSceneModifications } from "../../../../../common/model/scene/modifications/sceneModifications";
+import { ICommonGeometricObject } from "../../../../../common/model/scene/objects/geometricObjects/geometricObject";
 import { ICommonScene } from "../../../../../common/model/scene/scene";
 import { DefaultGrid } from "../grid";
 import { SceneGenerator } from "../sceneGenerator";
@@ -34,6 +35,36 @@ describe("GeometricObjectGenerator", () => {
             const differenceObj: ICommonSceneModifications = differenceGenerator.generateModifiedScene(false, true, false);
 
             expect(sceneObj.sceneObjects.length - differenceObj.deletedObjects.length).to.be.lte(sceneObj.sceneObjects.length);
+        });
+        it("Should return an array of objects with different colors than the original", () => {
+            const SCENE_SIZE: number = 1000;
+            const SCENE_OBJECT_MARGIN: number = 20;
+            const grid: DefaultGrid = new DefaultGrid(SCENE_SIZE, SCENE_SIZE, SCENE_OBJECT_MARGIN);
+
+            // tslint:disable:no-magic-numbers
+            const generateScene: SceneGenerator = new SceneGenerator(100);
+            const sceneObj: ICommonScene = generateScene.generateScene();
+
+            const differenceGenerator: SceneDifferenceGenerator = new SceneDifferenceGenerator(sceneObj, grid);
+            const differenceObj: ICommonSceneModifications = differenceGenerator.generateModifiedScene(false, false, true);
+
+            const objectIdColors: Map<string, number> = new Map();
+
+            for (const obj of sceneObj.sceneObjects) {
+                objectIdColors.set(obj.id, (obj as ICommonGeometricObject).color);
+            }
+
+            const objectIdModifiedColors: Map<string, number> = differenceObj["colorChangedObjects"];
+            let counterDifferences: number = 0;
+            for (const [id, color] of objectIdModifiedColors) {
+                const originalColor: number | undefined = objectIdColors.get(id);
+                const modifiedColor: number | undefined = color;
+                if (originalColor !== modifiedColor) {
+                    counterDifferences++;
+                }
+            }
+
+            expect(counterDifferences).to.equal(objectIdModifiedColors.size);
         });
     });
 });
