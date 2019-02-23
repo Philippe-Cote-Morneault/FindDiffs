8dc5d365ba4a31b108ba10e6ae89a7486421454d
Felix <felix-atnoine.bourbonnais@polymtl.ca>
2019-02-23 00:37:28 -0500

    (r) Removed testComponent from app-routing module (FAB)

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -5,11 +5,9 @@ import { GameViewFreeComponent } from "./game-view-free/game-view-free.component
 import { GameViewSimpleComponent } from "./game-view-simple/game-view-simple.component";
 import { GamesListViewComponent } from "./games-list-view/games-list-view.component";
 import { InitialViewComponent } from "./initial-view/initial-view.component";
-//import { TestComponent } from "./test/test.component";
 
 const routes: Routes = [
     { path: "", component: InitialViewComponent },
-    // { path: "", component: TestComponent },
     { path: "admin", component: AdminViewComponent },
     { path: "gamesList", component: GamesListViewComponent },
     { path: "gameSimple/:id", component: GameViewSimpleComponent },
