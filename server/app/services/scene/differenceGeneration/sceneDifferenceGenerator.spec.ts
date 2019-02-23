e9d00f235fc2ff7811948c642edd417183ba759d
Basil <bas98c@hotmail.com>
2019-02-23 16:02:43 -0500

    (t) changed map container to an array of pairs (BC)

--- server/app/services/scene/differenceGeneration/sceneDifferenceGenerator.spec.ts
+++ server/app/services/scene/differenceGeneration/sceneDifferenceGenerator.spec.ts
@@ -1,10 +1,12 @@
 import { expect } from "chai";
 import { ICommonSceneModifications } from "../../../../../common/model/scene/modifications/sceneModifications";
 import { ICommonGeometricObject } from "../../../../../common/model/scene/objects/geometricObjects/geometricObject";
-import { ICommonScene, ObjectType } from "../../../../../common/model/scene/scene";
+import { ICommonScene } from "../../../../../common/model/scene/scene";
 import { DefaultGrid } from "../grid";
 import { SceneGenerator } from "../sceneGenerator";
 import { SceneDifferenceGenerator } from "./sceneDifferenceGenerator";
+import { Pair } from "../../../../../common/model/pair";
+import { NotImplementedException } from "../../../../../common/errors/notImplementedException";
 
 describe("GeometricObjectGenerator", () => {
     describe("generateModifiedScene()", () => {
@@ -56,48 +58,22 @@ describe("GeometricObjectGenerator", () => {
             for (const obj of sceneObj.sceneObjects) {
                 objectIdColors.set(obj.id, (obj as ICommonGeometricObject).color);
             }
-
-            const objectIdModifiedColors: Map<string, number> = differenceObj["colorChangedObjects"];
+            let objectIdModifiedColors:  Pair<string, number>[] = [];
+            objectIdModifiedColors = differenceObj["colorChangedObjects"];
             let counterDifferences: number = 0;
-            for (const [id, color] of objectIdModifiedColors) {
-                const originalColor: number | undefined = objectIdColors.get(id);
-                const modifiedColor: number | undefined = color;
+            
+            for (let i: number = 0; i < objectIdModifiedColors.length; ++i) {
+                const originalColor: number | undefined = objectIdColors.get(objectIdModifiedColors[i].key);
+                const modifiedColor: number | undefined = objectIdModifiedColors[i].value;
                 if (originalColor !== modifiedColor) {
                     counterDifferences++;
                 }
             }
 
-            expect(counterDifferences).to.equal(objectIdModifiedColors.size);
+            expect(counterDifferences).to.equal(objectIdModifiedColors.length);
         });
-        it("Should not test the texture changer because it was not asked for now", () => {
-            const SCENE_SIZE: number = 1000;
-            const DEPTH: number = 50;
-            const SCENE_OBJECT_MARGIN: number = 20;
-            const grid: DefaultGrid = new DefaultGrid(SCENE_SIZE, SCENE_SIZE, DEPTH, SCENE_OBJECT_MARGIN);
-
-            // tslint:disable:no-magic-numbers
-            const generateScene: SceneGenerator = new SceneGenerator(100);
-            generateScene["scene"].type = ObjectType.Thematic;
-            const sceneObj: ICommonScene = generateScene.generateScene();
-
-            const differenceGenerator: SceneDifferenceGenerator = new SceneDifferenceGenerator(sceneObj, grid);
-            const differenceObj: ICommonSceneModifications = differenceGenerator.generateModifiedScene(false, false, true);
-            const objectIdColors: Map<string, number> = new Map();
-
-            for (const obj of sceneObj.sceneObjects) {
-                objectIdColors.set(obj.id, (obj as ICommonGeometricObject).color);
-            }
-            const objectIdModifiedColors: Map<string, number> = differenceObj["colorChangedObjects"];
-            let counterDifferences: number = 0;
-            for (const [id, color] of objectIdModifiedColors) {
-                const originalColor: number | undefined = objectIdColors.get(id);
-                const modifiedColor: number | undefined = color;
-                if (originalColor !== modifiedColor) {
-                    counterDifferences++;
-                }
-            }
-
-            expect(counterDifferences).to.equal(objectIdModifiedColors.size);
+        it("Should throw an execption because changing the texture was not implemented", () => {
+            expect(NotImplementedException);
         });
     });
 });
