2140acdc53d9f13155e192726eaa811229a7650e
Martin Pouliot <martinp507@gmail.com>
2019-02-24 14:16:32 -0500

    (d) Added a new method to random utils (MP)

--- server/app/utils/randomUtils.ts
+++ server/app/utils/randomUtils.ts
@@ -2,4 +2,7 @@ export class RandomUtils {
     public static inRange(min: number, max: number): number {
         return Math.random() * (max - min) + min;
     }
+    public static inRangeInt(min: number, max: number): number {
+        return Math.round(Math.random() * (max - min) + min);
+    }
 }
