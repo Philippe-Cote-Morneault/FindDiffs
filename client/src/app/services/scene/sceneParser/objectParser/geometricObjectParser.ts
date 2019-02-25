6d4690a997335f1d8d59c85a17cc5c282d0d7fba
Phil <philbond1997@hotmail.com>
2019-02-24 19:29:10 -0500

    (r) Removed useless breaks. (PC)

--- client/src/app/services/scene/sceneParser/objectParser/geometricObjectParser.ts
+++ client/src/app/services/scene/sceneParser/objectParser/geometricObjectParser.ts
@@ -16,19 +16,14 @@ export class GeometricObjectParser extends SceneObjectParser {
         switch (object.shapeType) {
             case GeometricShapeType.CONE:
                 return new ConeFactory().parse(object);
-                break;
             case GeometricShapeType.CUBE:
                 return new CubeFactory().parse(object);
-                break;
             case GeometricShapeType.CYLINDER:
                 return new CylinderFactory().parse(object);
-                break;
             case GeometricShapeType.SPHERE:
                 return new SphereFactory().parse(object);
-                break;
             case GeometricShapeType.TRIANGULAR_BASE_PYRAMID:
                 return new PyramidFactory().parse(object);
-                break;
             default:
                 throw new NotFoundException("This shape does not exist");
         }
