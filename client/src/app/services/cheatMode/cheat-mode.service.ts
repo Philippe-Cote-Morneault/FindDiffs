6294cd2646856a7ad42d01e3702387f8371fcad8
Jacob Meilleur <jacob.meilleur@polymtl.ca>
2019-03-07 23:27:35 -0500

    (d) pressing the letter t now changes the objects color (JM)

--- client/src/app/services/cheatMode/cheat-mode.service.ts
+++ client/src/app/services/cheatMode/cheat-mode.service.ts
@@ -1,8 +1,13 @@
 import { Injectable } from "@angular/core";
 import * as THREE from "three";
+import { ICommonGeometricModifications } from "../../../../../common/model/scene/modifications/geometricModifications";
 import { ICommonSceneModifications } from "../../../../../common/model/scene/modifications/sceneModifications";
+import { ICommonGeometricObject } from "../../../../../common/model/scene/objects/geometricObjects/geometricObject";
 import { ICommonSceneObject } from "../../../../../common/model/scene/objects/sceneObject";
 import { ICommonScene } from "../../../../../common/model/scene/scene";
+import { SceneLoaderService } from "../scene/sceneLoader/sceneLoader.service";
+import { ModifiedSceneParserService } from "../scene/sceneParser/modified-scene-parser.service";
+import { SceneParserService } from "../scene/sceneParser/scene-parser.service";
 
 @Injectable({
   providedIn: "root",
@@ -10,30 +15,51 @@ import { ICommonScene } from "../../../../../common/model/scene/scene";
 export class CheatModeService {
 
   public cheatActivated: boolean;
+  private sceneLoaderService: SceneLoaderService;
   public constructor() {
     this.cheatActivated = false;
+    this.sceneLoaderService = new SceneLoaderService();
   }
 
-  public toggleCheatMode(event: KeyboardEvent, originalScene: ICommonScene, modifiedScene: ICommonSceneModifications): void {
+  public toggleCheatMode(event: KeyboardEvent, originalScene: ICommonScene, modifiedScene: ICommonGeometricModifications,
+                         container: HTMLElement | null): void {
     this.cheatActivated = !this.cheatActivated;
     if (this.cheatActivated) {
-      this.enableCheats(originalScene, modifiedScene);
+      if (container) {
+        this.enableCheats(originalScene, modifiedScene, container);
+
+      }
     }
   }
 
-  private enableCheats(originalScene: ICommonScene, modifiedScene: ICommonSceneModifications): void {
+  private enableCheats(originalScene: ICommonScene, modifiedScene: ICommonGeometricModifications, container: HTMLElement): void {
+    const originalSceneThreeJs: THREE.Scene = new SceneParserService().parseScene(originalScene);
+    const modifiedSceneThreeJs: THREE.Scene = new ModifiedSceneParserService().parseModifiedScene(originalScene, modifiedScene);
+
+    container.removeChild(container.childNodes[0]);
+    originalSceneThreeJs.translateX(1);
+
     if (modifiedScene.addedObjects.length > 0) {
-      modifiedScene.addedObjects.forEach((object: ICommonSceneObject) => {
-        // ajouter code
+      modifiedScene.addedObjects.forEach((object: ICommonGeometricObject) => {
+        modifiedSceneThreeJs.children.forEach((objectJs) => {
+          if (objectJs.userData.id === object.id) {
+            // tslint:disable-next-line:no-magic-numbers
+            object.color = 0xFFFF;
+          }
+        });
       });
-    } else if (modifiedScene.deletedObjects.length  > 0) {
-      originalScene.sceneObjects.forEach((object: ICommonSceneObject) => {
-        if (modifiedScene.deletedObjects.includes(object.id)) {
-          // ajouter code
-        }
-      });
-    } else {
-      // ajouter code
     }
+    if (modifiedScene.deletedObjects.length  > 0) {
+      // add code
+    }
+    if (modifiedScene.colorChangedObjects.length > 0) {
+      // add code
+    }
+    this.renderNewScene(modifiedSceneThreeJs, container);
+  }
+
+  private renderNewScene(newScene: THREE.Scene, container: HTMLElement): void {
+    this.sceneLoaderService.scene = newScene;
+    this.sceneLoaderService.renderScene(container, true);
   }
 }
