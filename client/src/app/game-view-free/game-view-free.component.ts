b306a7b2a35f3a7416198d4dac1301bb8519d5d4
Laurent Barberis <laurent.barberis15@gmail.com>
2019-03-09 13:36:35 -0500

    (d) Added getGameCardById in free gameview (LB)

--- client/src/app/game-view-free/game-view-free.component.ts
+++ client/src/app/game-view-free/game-view-free.component.ts
@@ -1,8 +1,10 @@
 import { Component, ElementRef, OnInit, ViewChild } from "@angular/core";
 import { ActivatedRoute } from "@angular/router";
 import { Ng4LoadingSpinnerService } from "ng4-loading-spinner";
+import { ICommonGameCard } from "../../../../common/model/gameCard";
 import { ICommonSceneModifications } from "../../../../common/model/scene/modifications/sceneModifications";
 import { ICommonScene } from "../../../../common/model/scene/scene";
+import { GamesCardService } from "../services/gameCard/games-card.service";
 import { SceneService } from "../services/scene/scene.service";
 import { SceneLoaderService } from "../services/scene/sceneLoader/sceneLoader.service";
 import { TimerService } from "../services/timer/timer.service";
@@ -16,8 +18,10 @@ export class GameViewFreeComponent implements OnInit {
     @ViewChild("originalScene") private originalScene: ElementRef;
     @ViewChild("modifiedScene") private modifiedScene: ElementRef;
     @ViewChild("chronometer") private chronometer: ElementRef;
+    @ViewChild("gameTitle") private gameTitle: ElementRef;
 
     private scenePairID: string;
+    private gameCardID: string;
     private originalSceneLoader: SceneLoaderService;
     private modifiedSceneLoader: SceneLoaderService;
 
@@ -25,17 +29,26 @@ export class GameViewFreeComponent implements OnInit {
         private route: ActivatedRoute,
         private spinnerService: Ng4LoadingSpinnerService,
         public sceneService: SceneService,
-        public timerService: TimerService) {
+        public timerService: TimerService,
+        public gamesCardService: GamesCardService) {
             this.originalSceneLoader = new SceneLoaderService();
             this.modifiedSceneLoader = new SceneLoaderService();
     }
 
     public ngOnInit(): void {
         this.route.params.subscribe((params) => {
-            this.scenePairID = params["id"];
+            this.gameCardID = params["id"];
         });
         this.spinnerService.show();
-        this.getOriginalSceneById();
+        this.getGameCardById();
+    }
+
+    private getGameCardById(): void {
+        this.gamesCardService.getGameById(this.gameCardID).subscribe((gameCard: ICommonGameCard) => {
+            this.scenePairID = gameCard.resource_id;
+            this.gameTitle.nativeElement.innerText = gameCard.title;
+            this.getOriginalSceneById();
+        });
     }
 
     private getOriginalSceneById(): void {
