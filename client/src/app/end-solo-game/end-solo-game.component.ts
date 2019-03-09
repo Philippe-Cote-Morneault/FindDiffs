5747b8ab09a88b634f7dc8ad30cd893bd0027edb
Felix <felix-atnoine.bourbonnais@polymtl.ca>
2019-03-08 22:23:45 -0500

    (d) Added redirection to gameslist after popup simple pov (FAB)

--- client/src/app/end-solo-game/end-solo-game.component.ts
+++ client/src/app/end-solo-game/end-solo-game.component.ts
@@ -1,14 +1,14 @@
 import { Component, OnInit } from "@angular/core";
 import { ActivatedRoute } from "@angular/router";
+import { Router } from "@angular/router";
 import { ICommonGameCard, ICommonScoreEntry } from "../../../../common/model/gameCard";
 import { GamesCardService } from "../services/gameCard/games-card.service";
 import { StringFormater } from "../util/stringFormater";
 
-
 @Component({
   selector: "app-end-solo-game",
   templateUrl: "./end-solo-game.component.html",
-  styleUrls: ["./end-solo-game.component.css"]
+  styleUrls: ["./end-solo-game.component.css"],
 })
 export class EndSoloGameComponent implements OnInit {
 
@@ -16,7 +16,8 @@ export class EndSoloGameComponent implements OnInit {
   public gameCardId: string;
   public constructor(
     private route: ActivatedRoute,
-    public gamesCardService: GamesCardService) { }
+    public gamesCardService: GamesCardService,
+    private router: Router) { }
 
   public ngOnInit(): void {
     this.route.params.subscribe((params) => {
@@ -33,5 +34,9 @@ export class EndSoloGameComponent implements OnInit {
 
   public toMinutes(index: number, times: ICommonScoreEntry[]): string {
     return StringFormater.secondsToMinutes(times[index].score);
+  }
+
+  public async leaveGame(): Promise<void> {
+    await this.router.navigateByUrl("/gamesList");
   }
 }
