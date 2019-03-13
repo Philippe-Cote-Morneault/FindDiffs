6de586bfe94e1b5c7fbc2f299ffc5fd40ae1ba7a
Basil <bas98c@hotmail.com>
2019-03-13 19:34:44 -0400

    (d) changed addEventListener to an event click in the html (BC)

--- client/src/app/game-view-free/game-view-free.component.ts
+++ client/src/app/game-view-free/game-view-free.component.ts
@@ -59,38 +59,33 @@ export class GameViewFreeComponent implements OnInit {
             this.originalSceneLoader.loadOriginalScene(this.originalScene.nativeElement, response, true);
             this.getModifiedSceneById(response);
         });
-
-        this.clickScene();
     }
 
-    public clickScene(): void {
-        this.originalScene.nativeElement.addEventListener("click", (event) => {
-            const raycaster: THREE.Raycaster = new THREE.Raycaster();
-            const mouse: THREE.Vector2 = new THREE.Vector2();
-            let intersects: THREE.Intersection[];
-            const group: THREE.Object3D[] = [];
-            this.originalSceneLoader.scene.children.forEach((element) => {
-                if (element.type === "Mesh") {
-                    group.push(element);
-                }
-            });
-
-            const divBoxInformation: ClientRect | DOMRect = this.originalScene.nativeElement.getBoundingClientRect();
-            const differenceX: number = event.clientX - divBoxInformation.left;
-            const differenceY: number = event.clientY - divBoxInformation.top;
-
-            // tslint:disable:no-magic-numbers
-            mouse.x = (differenceX / this.originalScene.nativeElement.clientWidth) * 2 - 1;
-            mouse.y = -(differenceY / this.originalScene.nativeElement.clientHeight) * 2 + 1;
-
-            raycaster.setFromCamera(mouse, this.originalSceneLoader.camera );
-            intersects = raycaster.intersectObjects( group );
-
-            if (intersects.length > 0) {
-                console.log(intersects[0]);
+    public clickOriginalScene(event: MouseEvent): void {
+        const raycaster: THREE.Raycaster = new THREE.Raycaster();
+        const mouse: THREE.Vector2 = new THREE.Vector2();
+        let intersects: THREE.Intersection[];
+        const group: THREE.Object3D[] = [];
+        this.originalSceneLoader.scene.children.forEach((element) => {
+            if (element.type === "Mesh") {
+                group.push(element);
             }
-        },
-                                                          false);
+        });
+
+        const divBoxInformation: ClientRect | DOMRect = this.originalScene.nativeElement.getBoundingClientRect();
+        const differenceX: number = event.clientX - divBoxInformation.left;
+        const differenceY: number = event.clientY - divBoxInformation.top;
+
+        // tslint:disable:no-magic-numbers
+        mouse.x = (differenceX / this.originalScene.nativeElement.clientWidth) * 2 - 1;
+        mouse.y = -(differenceY / this.originalScene.nativeElement.clientHeight) * 2 + 1;
+
+        raycaster.setFromCamera(mouse, this.originalSceneLoader.camera );
+        intersects = raycaster.intersectObjects( group );
+
+        if (intersects.length > 0) {
+            console.log(intersects[0]);
+        }
     }
 
     private getModifiedSceneById(response: ICommonScene): void {
