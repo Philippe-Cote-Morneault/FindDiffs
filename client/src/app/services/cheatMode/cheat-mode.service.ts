f336332ae19247f1b4649ec050e72ef0dc542249
Jacob Meilleur <jacob.meilleur@polymtl.ca>
2019-03-13 22:29:33 -0400

    (d) added objects now change color when pressing t (JM)

--- client/src/app/services/cheatMode/cheat-mode.service.ts
+++ client/src/app/services/cheatMode/cheat-mode.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from "@angular/core";
 import * as THREE from "three";
+// import { Pair } from "../../../../../common/model/pair";
 import { ICommonGeometricModifications } from "../../../../../common/model/scene/modifications/geometricModifications";
-import { ICommonSceneModifications } from "../../../../../common/model/scene/modifications/sceneModifications";
 import { ICommonGeometricObject } from "../../../../../common/model/scene/objects/geometricObjects/geometricObject";
 import { ICommonSceneObject } from "../../../../../common/model/scene/objects/sceneObject";
 import { ICommonScene } from "../../../../../common/model/scene/scene";
@@ -13,53 +13,44 @@ import { SceneParserService } from "../scene/sceneParser/scene-parser.service";
   providedIn: "root",
 })
 export class CheatModeService {
-
+  private static WHITE: number = 0xFFFFFF;
   public cheatActivated: boolean;
-  private sceneLoaderService: SceneLoaderService;
+  public originalSceneLoaderService: SceneLoaderService;
+  public modifiedSceneLoaderService: SceneLoaderService;
+
   public constructor() {
     this.cheatActivated = false;
-    this.sceneLoaderService = new SceneLoaderService();
   }
 
   public toggleCheatMode(event: KeyboardEvent, originalScene: ICommonScene, modifiedScene: ICommonGeometricModifications,
-                         container: HTMLElement | null): void {
+                         originalContainer: HTMLElement | null, modifiedContainer: HTMLElement | null): void {
     this.cheatActivated = !this.cheatActivated;
     if (this.cheatActivated) {
-      if (container) {
-        this.enableCheats(originalScene, modifiedScene, container);
-
+      if (originalContainer && modifiedContainer) {
+        this.enableCheats(originalScene, modifiedScene);
       }
     }
   }
 
-  private enableCheats(originalScene: ICommonScene, modifiedScene: ICommonGeometricModifications, container: HTMLElement): void {
-    const originalSceneThreeJs: THREE.Scene = new SceneParserService().parseScene(originalScene);
+  private enableCheats(originalScene: ICommonScene, modifiedScene: ICommonGeometricModifications): void {
     const modifiedSceneThreeJs: THREE.Scene = new ModifiedSceneParserService().parseModifiedScene(originalScene, modifiedScene);
+    const originalSceneThreeJs: THREE.Scene = new SceneParserService().parseScene(originalScene);
 
-    container.removeChild(container.childNodes[0]);
-    originalSceneThreeJs.translateX(1);
-
-    if (modifiedScene.addedObjects.length > 0) {
-      modifiedScene.addedObjects.forEach((object: ICommonGeometricObject) => {
-        modifiedSceneThreeJs.children.forEach((objectJs) => {
-          if (objectJs.userData.id === object.id) {
-            // tslint:disable-next-line:no-magic-numbers
-            object.color = 0xFFFF;
+    if (modifiedScene.deletedObjects.length > 0) {
+      modifiedScene.deletedObjects.forEach((id: String) => {
+        originalScene.sceneObjects.forEach((object: ICommonGeometricObject) => {
+          if (object.id === id) {
+            object.color = CheatModeService.WHITE - object.color;
           }
         });
       });
     }
-    if (modifiedScene.deletedObjects.length  > 0) {
-      // add code
-    }
-    if (modifiedScene.colorChangedObjects.length > 0) {
-      // add code
-    }
-    this.renderNewScene(modifiedSceneThreeJs, container);
+    this.renderScene(this.originalSceneLoaderService, originalSceneThreeJs, this.originalSceneLoaderService.camera);
+    this.renderScene(this.modifiedSceneLoaderService, modifiedSceneThreeJs, this.modifiedSceneLoaderService.camera);
   }
 
-  private renderNewScene(newScene: THREE.Scene, container: HTMLElement): void {
-    this.sceneLoaderService.scene = newScene;
-    this.sceneLoaderService.renderScene(container, true);
+  private renderScene(sceneLoader: SceneLoaderService, scene: THREE.Scene, camera: THREE.PerspectiveCamera): void {
+    sceneLoader.scene = scene;
+    sceneLoader.renderer.render(scene, camera);
   }
 }
