dad4b0d9cff2ec1261358b44578496f2ac7db254
Martin Pouliot <martinp507@gmail.com>
2019-03-14 01:55:47 -0400

    (d) Created a theme grid (MP)

---
+++ server/app/services/scene/grid/theme/themeGrid.ts
@@ -0,0 +1,14 @@
+import { Grid } from "../grid";
+import { IThemeGridPosition } from "./IThemeGridPosition";
+
+export enum SurfaceType {Grass = 0, Parking = 1, Car = 2}
+
+export class ThemeGrid extends Grid {
+    private static readonly POSITION_FILE: string = "./positions.json";
+
+    protected generateGrid(): void {
+        const availablePositions: IThemeGridPosition = require(ThemeGrid.POSITION_FILE);
+
+    }
+
+}
