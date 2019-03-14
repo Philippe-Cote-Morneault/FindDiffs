aef0a0bc0c0bf3ff411375d2549b8cc357875658
Jacob Meilleur <jacob.meilleur@polymtl.ca>
2019-03-13 23:51:45 -0400

    (d) objects now change color on  first press of letter t (JM)

--- client/src/app/services/cheatMode/cheat-mode.service.ts
+++ client/src/app/services/cheatMode/cheat-mode.service.ts
@@ -12,7 +12,6 @@ import { SceneParserService } from "../scene/sceneParser/scene-parser.service";
   providedIn: "root",
 })
 export class CheatModeService {
-  private static WHITE: number = 0xFFFFFF;
   public cheatActivated: boolean;
   public originalSceneLoaderService: SceneLoaderService;
   public modifiedSceneLoaderService: SceneLoaderService;
@@ -34,22 +33,28 @@ export class CheatModeService {
   private enableCheats(originalScene: ICommonScene, modifiedScene: ICommonGeometricModifications): void {
     const modifiedSceneThreeJs: THREE.Scene = new ModifiedSceneParserService().parseModifiedScene(originalScene, modifiedScene);
     const originalSceneThreeJs: THREE.Scene = new SceneParserService().parseScene(originalScene);
-
     if (modifiedScene.deletedObjects.length > 0) {
-      modifiedScene.deletedObjects.forEach((id: String) => {
-        originalScene.sceneObjects.forEach((object: ICommonGeometricObject) => {
-          if (object.id === id) {
-            object.color = CheatModeService.WHITE - object.color;
+      originalScene.sceneObjects.forEach((object: ICommonGeometricObject) => {
+        originalSceneThreeJs.children.forEach((object3D: THREE.Mesh) => {
+          if (object3D.type === "Mesh") {
+            if (object3D.userData.id === object.id && modifiedScene.deletedObjects.includes(object.id)) {
+              const meshPhong: THREE.MeshPhongMaterial = object3D.material as THREE.MeshPhongMaterial;
+              object.color = ~object.color
+              meshPhong.color.setHex(object.color);
+            }
           }
         });
       });
     }
-
     if (modifiedScene.addedObjects.length > 0) {
       modifiedScene.addedObjects.forEach((object: ICommonGeometricObject) => {
-        modifiedSceneThreeJs.children.forEach((objectJs: THREE.Object3D) => {
-          if (objectJs.userData.id === object.id) {
-            object.color = CheatModeService.WHITE - object.color;
+        modifiedSceneThreeJs.children.forEach((object3D: THREE.Mesh) => {
+          if (object3D.type === "Mesh") {
+            if (object3D.userData.id === object.id) {
+              const meshPhong: THREE.MeshPhongMaterial = object3D.material as THREE.MeshPhongMaterial;
+              object.color = ~object.color
+              meshPhong.color.setHex(object.color);
+            }
           }
         });
       });
