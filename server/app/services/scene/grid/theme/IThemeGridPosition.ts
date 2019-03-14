f002ebeb9277e2b86d42891a29637ddfdaa63974
Martin Pouliot <martinp507@gmail.com>
2019-03-14 01:53:45 -0400

    (d) Added an interface for the positons file (MP)

---
+++ server/app/services/scene/grid/theme/IThemeGridPosition.ts
@@ -0,0 +1,7 @@
+import { ICommon3DPosition } from "../../../../../../common/model/positions";
+
+export interface IThemeGridPosition {
+    parking: ICommon3DPosition[];
+    grass: ICommon3DPosition[];
+    car: ICommon3DPosition[];
+}
