e115f58da43b67f35b5bdaf2e445a8b48e66e30b
Martin Pouliot <martinp507@gmail.com>
2019-03-14 22:43:58 -0400

    (d) Added new type for surface (MP)

--- common/model/scene/objects/thematicObjects/thematicObject.ts
+++ common/model/scene/objects/thematicObjects/thematicObject.ts
@@ -4,6 +4,10 @@ export enum ObjTheme {
     BENCH, BIN, CONE, ECLIPSE, LAMBO, LAMP, LEXUS, SIGN_FORBIDDEN, SIGN_SKIP, SIGN_STOP
 };
 
+export enum ThemeSurface {
+    GRASS, PARKING, CAR
+}
+
 export interface ICommonThematicObject extends ICommonSceneObject {
     isTextured: boolean;
     texture?: string;
