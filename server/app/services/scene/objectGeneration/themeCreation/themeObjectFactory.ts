d6351f3d3410321ff469dfe3b8d9c5c34bb0a3f9
Martin Pouliot <martinp507@gmail.com>
2019-03-15 16:49:07 -0400

    (d) Created themeObjectFactory (MP)

---
+++ server/app/services/scene/objectGeneration/themeCreation/themeObjectFactory.ts
@@ -0,0 +1,10 @@
+import { ObjectFactory } from "../objectFactory";
+import { ICommonThematicObject } from "../../../../../../common/model/scene/objects/thematicObjects/thematicObject";
+import { ObjectType } from "../../../../../../common/model/scene/scene";
+import { ObjectProperties } from "../../../../../../common/model/scene/objects/thematicObjects/objectProperties";
+import { RandomUtils } from "../../../../utils/randomUtils";
+
+export abstract class ThemeObjectFactory extends ObjectFactory{
+    
+    
+}
