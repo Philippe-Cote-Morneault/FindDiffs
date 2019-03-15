4f24db09c1482604699fe4e058c28971d8988d21
Felix <felix-atnoine.bourbonnais@polymtl.ca>
2019-03-14 21:28:31 -0400

    (d) Second test for updateScore (FAB)(BC)(LB)

--- server/app/services/gameCard/scoreUpdater.spec.ts
+++ server/app/services/gameCard/scoreUpdater.spec.ts
@@ -12,3 +12,16 @@ describe("ScoreUpdater", () => {
         const newEntries: ICommonScoreEntry[] = ScoreUpdater.updateScore(entries, newScore);
         expect(newEntries).to.equal(entries);
     });
+
+    it("Should return an array with new first place and other users down-shifted", () => {
+        const entries: ICommonScoreEntry[] = [{name: "Michel", score: 10},
+                                              {name: "Bob", score: 12},
+                                              {name: "Simon", score: 14}];
+        const newScore: ICommonScoreEntry = {name: "Sam", score: 8};
+        const newEntries: ICommonScoreEntry[] = ScoreUpdater.updateScore(entries, newScore);
+        console.log(newEntries[0]);
+        console.log(newScore);
+        expect(newEntries[0]).to.equal(newScore);
+        expect(newEntries[1]).to.equal(entries[0]);
+        expect(newEntries[2]).to.equal(entries[1]);
+    });
