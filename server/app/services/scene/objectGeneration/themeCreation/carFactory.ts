8273eb9f63ed65603ff1d214a589606b2ad77e52
Martin Pouliot <martinp507@gmail.com>
2019-03-15 16:54:21 -0400

    (d) Added a car factory (MP)

---
+++ server/app/services/scene/objectGeneration/themeCreation/carFactory.ts
@@ -0,0 +1,31 @@
+import { ICommonEulerAngles } from "../../../../../../common/model/scene/eulerAngles";
+import { RandomUtils } from "../../../../utils/randomUtils";
+import { ObjectFactory } from "../objectFactory";
+import { ThemeObjectFactory } from "./themeObjectFactory";
+
+export class CarFactory extends ThemeObjectFactory {
+    private name: string;
+
+    public constructor(name: string) {
+        super();
+        this.name = name;
+    }
+
+    protected getObjectName(): string {
+        return this.name;
+    }
+
+    protected generateRandomOrientation(): ICommonEulerAngles {
+        const orientation: ICommonEulerAngles = {
+            xAngle: 0,
+            yAngle: 0,
+            zAngle: 0,
+        };
+        if (RandomUtils.inRange(0, 1) === 1) {
+            // tslint:disable-next-line:no-magic-numbers
+            orientation.yAngle = ObjectFactory.MAX_RADIAN_ANGLE / 2;
+        }
+
+        return orientation;
+    }
+}
