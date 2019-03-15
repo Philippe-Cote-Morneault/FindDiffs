c5b48987de86e8226a1c3d498ed5fae9c8ee16bd
Martin Pouliot <martinp507@gmail.com>
2019-03-15 16:50:47 -0400

    (d) implemented requiered methods for themeobjectfactory (MP)

--- server/app/services/scene/objectGeneration/themeCreation/themeObjectFactory.ts
+++ server/app/services/scene/objectGeneration/themeCreation/themeObjectFactory.ts
@@ -6,5 +6,15 @@ import { RandomUtils } from "../../../../utils/randomUtils";
 
 export abstract class ThemeObjectFactory extends ObjectFactory{
     
+    protected abstract getObjectName(): string;
+
+    protected getFactoryType(): ObjectType{
+        return ObjectType.Thematic;
+    }
     
+    protected postCreate(): void{
+        const thematicObject = this.object as ICommonThematicObject;
+
+        this.object = thematicObject;
+    }
 }
