24b508498c72ebcd20285237cf1c2fb81f03ae84
Martin Pouliot <martinp507@gmail.com>
2019-03-16 19:01:43 -0400

    (d) Added cache to reduce memory usage (MP)

--- client/src/app/services/scene/sceneLoader/jsonLoader.ts
+++ client/src/app/services/scene/sceneLoader/jsonLoader.ts
@@ -5,6 +5,10 @@ export class JSONLoader {
     private static readonly ASSET_EXTENSION: string = ".json";
 
     public static async load(name: string): Promise<THREE.Object3D> {
+        if (!THREE.Cache.enabled) {
+            THREE.Cache.enabled = true;
+        }
+
         return new Promise((resolve: (object: THREE.Object3D) => void, reject) => {
             const path: string = this.ASSET_PATH + name + this.ASSET_EXTENSION;
             new THREE.ObjectLoader().load(path, resolve, undefined, reject);
