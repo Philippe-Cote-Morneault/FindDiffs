7b41c03cfe229a759739a640ba80ae4820823932
Martin Pouliot <martinp507@gmail.com>
2019-03-16 02:49:46 -0400

    (d) Created a jsonLoader (MP)

---
+++ client/src/app/services/scene/sceneLoader/jsonLoader.ts
@@ -0,0 +1,13 @@
+import * as THREE from "three";
+
+export class JSONLoader {
+    private static readonly ASSET_PATH: string = "../../assets/theme/models/";
+    private static readonly ASSET_EXTENSION: string = ".json";
+
+    public static async load(name: string): Promise<THREE.Object3D> {
+        return new Promise((resolve: (object: THREE.Object3D) => void, reject) => {
+            const path: string = this.ASSET_PATH + name + this.ASSET_EXTENSION;
+            new THREE.ObjectLoader().load(path, resolve, undefined, reject);
+          });
+    }
+}
