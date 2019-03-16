62096cd569367a4182a6c092f917108127a4152f
Martin Pouliot <martinp507@gmail.com>
2019-03-16 02:50:37 -0400

    (r) Made the method asynchronous (MP)

--- client/src/app/services/scene/sceneParser/sceneObjectParser.ts
+++ client/src/app/services/scene/sceneParser/sceneObjectParser.ts
@@ -2,5 +2,5 @@ import * as THREE from "three";
 import { ICommonSceneObject } from "../../../../../../common/model/scene/objects/sceneObject";
 
 export abstract class SceneObjectParser {
-    public abstract parse(object: ICommonSceneObject): THREE.Object3D;
+    public abstract async parse(object: ICommonSceneObject): Promise<THREE.Object3D>;
 }
