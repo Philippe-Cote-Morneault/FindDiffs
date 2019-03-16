c776db067dd76c4bea48bc20b914f52dc0b94eed
Martin Pouliot <martinp507@gmail.com>
2019-03-16 00:11:01 -0400

    (d) Implemented thematicSceneParser (MP)

--- client/src/app/services/scene/sceneParser/thematicSceneParser.ts
+++ client/src/app/services/scene/sceneParser/thematicSceneParser.ts
@@ -4,7 +4,18 @@ import { ICommonThematicScene } from "../../../../../../common/model/scene/scene
 // tslint:disable-next-line:no-suspicious-comment
 // TODO: Implement this in sprint 3
 export class ThematicSceneParser {
-    public static parseScene(originalScene: ICommonThematicScene): THREE.Scene {
-        return new THREE.Scene();
+    private static readonly SCENE_MODEL: string = "../../assets/theme/scene.json";
+    public static async parseScene(originalScene: ICommonThematicScene): Promise<THREE.Scene> {
+        const scene: THREE.Scene = new THREE.Scene();
+        const object: THREE.Object3D = await this.loadJSON(this.SCENE_MODEL);
+        scene.add(object);
+
+        return scene;
+    }
+
+    private static async loadJSON(url: string): Promise<THREE.Object3D> {
+        return new Promise((resolve: (object: THREE.Object3D) => void, reject) => {
+            new THREE.ObjectLoader().load(url, resolve, undefined, reject);
+          });
     }
 }
