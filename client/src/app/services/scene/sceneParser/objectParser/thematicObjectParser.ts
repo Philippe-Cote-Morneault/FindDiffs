38d53aa085eb5538a017a9dfe38049b28e4c1535
Martin Pouliot <martinp507@gmail.com>
2019-03-16 02:51:26 -0400

    (d) Added thematic object parser (MP)

--- client/src/app/services/scene/sceneParser/objectParser/thematicObjectParser.ts
+++ client/src/app/services/scene/sceneParser/objectParser/thematicObjectParser.ts
@@ -1,12 +1,24 @@
 import * as THREE from "three";
-import { ICommonThematicObject } from "../../../../../../../common/model/scene/objects/thematicObjects/thematicObject";
+import { ICommonThematicObject, ObjTheme } from "../../../../../../../common/model/scene/objects/thematicObjects/thematicObject";
+import { JSONLoader } from "../../sceneLoader/jsonLoader";
 import { SceneObjectParser } from "../sceneObjectParser";
 
 export class ThematicObjectParser extends SceneObjectParser {
 
-    public parse(object: ICommonThematicObject): THREE.Object3D {
-        // tslint:disable-next-line:no-suspicious-comment
-        // TODO: Implement this method in sprint 3
-        return new THREE.Object3D();
+    public async parse(object: ICommonThematicObject): Promise<THREE.Object3D> {
+        const objectName: string = ObjTheme[object.objectType].toLowerCase();
+        const object3D: THREE.Object3D = await JSONLoader.load(objectName);
+
+        object3D.position.x = object.position.x;
+        object3D.position.y = object.position.y;
+        object3D.position.z = object.position.z;
+
+        if (object3D.rotation.x !== 0) {
+            object3D.rotation.z = object.orientation.yAngle;
+        } else {
+            object3D.rotation.y = object.orientation.yAngle;
+        }
+
+        return object3D;
     }
 }
