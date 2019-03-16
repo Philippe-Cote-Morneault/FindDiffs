ac2b5405ae148dd777ec9e24857d1236d7b70d30
Martin Pouliot <martinp507@gmail.com>
2019-03-16 02:50:57 -0400

    (r) Removed json loader from this file (MP)

--- client/src/app/services/scene/sceneParser/thematicSceneParser.ts
+++ client/src/app/services/scene/sceneParser/thematicSceneParser.ts
@@ -1,21 +1,16 @@
 import * as THREE from "three";
 import { ICommonThematicScene } from "../../../../../../common/model/scene/scene";
+import { JSONLoader } from "../sceneLoader/jsonLoader";
 
 // tslint:disable-next-line:no-suspicious-comment
 // TODO: Implement this in sprint 3
 export class ThematicSceneParser {
-    private static readonly SCENE_MODEL: string = "../../assets/theme/scene.json";
+    private static readonly SCENE_MODEL: string = "scene";
     public static async parseScene(originalScene: ICommonThematicScene): Promise<THREE.Scene> {
         const scene: THREE.Scene = new THREE.Scene();
-        const object: THREE.Object3D = await this.loadJSON(this.SCENE_MODEL);
+        const object: THREE.Object3D = await JSONLoader.load(this.SCENE_MODEL);
         scene.add(object);
 
         return scene;
-    }
-
-    private static async loadJSON(url: string): Promise<THREE.Object3D> {
-        return new Promise((resolve: (object: THREE.Object3D) => void, reject) => {
-            new THREE.ObjectLoader().load(url, resolve, undefined, reject);
-          });
     }
 }
