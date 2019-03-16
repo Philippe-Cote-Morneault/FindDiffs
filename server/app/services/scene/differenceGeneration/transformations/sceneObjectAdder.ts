8693e5335a8d444cf61310f0d4fa91b6b07d5ba1
Martin Pouliot <martinp507@gmail.com>
2019-03-15 21:05:53 -0400

    (d) Added modification to to the sceneObjectAdder (MP)

--- server/app/services/scene/differenceGeneration/transformations/sceneObjectAdder.ts
+++ server/app/services/scene/differenceGeneration/transformations/sceneObjectAdder.ts
@@ -4,7 +4,9 @@ import { SceneTransformation } from "../../../../../../common/model/scene/modifi
 import { ICommonSceneObject } from "../../../../../../common/model/scene/objects/sceneObject";
 import { ObjectType } from "../../../../../../common/model/scene/scene";
 import { Grid } from "../../grid/grid";
+import { IPostionGridTheme } from "../../grid/theme/themeGrid";
 import { GeometricObjectGenerator } from "../../objectGeneration/shapeCreation/geometricObjectGenerator";
+import { ThemeObjectGenerator } from "../../objectGeneration/themeCreation/themeObjectGenerator";
 
 /**
  * In charge of adding a random object to a scene
@@ -21,8 +23,14 @@ export class SceneObjectAdder implements SceneTransformation {
                                type: ObjectType): void {
             const position: ICommon3DPosition = this.grid.getNextPosition();
 
-            modifications.addedObjects.push(
-                GeometricObjectGenerator.getInstance().createObject(position),
-            );
+            if (type === ObjectType.Geometric) {
+                modifications.addedObjects.push(
+                    GeometricObjectGenerator.getInstance().createObject(position),
+                );
+            } else {
+                modifications.addedObjects.push(
+                    ThemeObjectGenerator.getInstance().createObject(position as IPostionGridTheme),
+                );
+            }
     }
 }
