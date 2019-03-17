f3906830a12aa107f88592386dbe6ff6395f457f
Jacob Meilleur <jacob.meilleur@polymtl.ca>
2019-03-17 01:09:33 -0400

    (d) start and stop timeout for cheat mode (JM)

--- client/src/app/services/cheatMode/cheat-mode-timeout.service.ts
+++ client/src/app/services/cheatMode/cheat-mode-timeout.service.ts
@@ -1,9 +1,28 @@
-import { Injectable } from '@angular/core';
-
+import { Injectable } from "@angular/core";
+import { ICommonGeometricModifications } from "../../../../../common/model/scene/modifications/geometricModifications";
+import { ICommonSceneModifications } from "../../../../../common/model/scene/modifications/sceneModifications";
+import { ICommonScene } from "../../../../../common/model/scene/scene";
+import { CheatModeService } from "./cheat-mode.service";
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class CheatModeTimeoutService {
+  private static readonly intervalTime: number = 250;
+  private timeout: NodeJS.Timeout;
 
-  constructor() { }
+  public startCheatMode(cheatModeService: CheatModeService,
+                        currentOriginalScene: ICommonScene,
+                        currentModifiedScene: ICommonSceneModifications): void {
+
+    cheatModeService.toggleCheatMode(currentOriginalScene,
+                                     (currentModifiedScene as ICommonGeometricModifications));
+    this.timeout = setTimeout(() => {
+        this.startCheatMode(cheatModeService, currentOriginalScene, currentModifiedScene);
+    },                        CheatModeTimeoutService.intervalTime);
+  }
+
+  public stopCheatMode(): void {
+    clearTimeout(this.timeout);
+  }
+
 }
