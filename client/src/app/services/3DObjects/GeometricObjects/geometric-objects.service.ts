bda50d3fbb5037df5dd9f0315e59010c3e8c6a1c
Felix <felix-atnoine.bourbonnais@polymtl.ca>
2019-03-16 23:05:33 -0400

    (r) Modified difference3D service (FAB)(BC)

--- client/src/app/services/3DObjects/GeometricObjects/geometric-objects.service.ts
+++ client/src/app/services/3DObjects/GeometricObjects/geometric-objects.service.ts
@@ -13,10 +13,26 @@ export class GeometricObjectsService extends HTTPService {
     public constructor(private http: HttpClient) {
         super();
     }
+    /*
+        // tslint:disable: no-any
+        public post3DObject(originalObjectId: string | undefined, modifiedObjectId: string | undefined,
+                            originalIntersectLength: number, modifiedIntersectLength: number, originalIntersectDistance: number,
+                            modifiedIntersectDistance: number): Observable<any> {
+            const requestBody: Object = { "originalId": originalObjectId, "modifiedId": modifiedObjectId,
+                                          "originalIntersectLength": originalIntersectLength,
+                                          "modifiedIntersectLength": modifiedIntersectLength,
+                                          "originalIntersectDistance": originalIntersectDistance,
+                                          "modifiedIntersectDistance": modifiedIntersectDistance};
 
-    // tslint:disable: no-any
-    public post3DObject(originalObjectId: string | undefined, modifiedObjectId: string | undefined): Observable<any> {
-        const requestBody: Object = { "originalId": originalObjectId, "modifiedId": modifiedObjectId};
+            return this.http.post<any>(`${SERVER_URL}/difference/free`, requestBody).pipe(
+                catchError((error) => this.handleError(error)),
+            );
+        }
+    }*/
+    // tslint:disable:no-any
+    public post3DObject(originalSceneId: string, modifiedObjectId: string | undefined): Observable<any> {
+        const requestBody: Object = {
+            "originalSceneId": originalSceneId, "modifiedObjectId": modifiedObjectId};
 
         return this.http.post<any>(`${SERVER_URL}/difference/free`, requestBody).pipe(
             catchError((error) => this.handleError(error)),
