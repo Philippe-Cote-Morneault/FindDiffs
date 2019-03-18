435f59f45d777e806d5fb18243b9c9f888989975
Laurent Barberis <laurent.barberis15@gmail.com>
2019-03-18 15:57:24 -0400

    (d) added switch case (LB)

--- client/src/app/services/socket/socketMessageSender.service.ts
+++ client/src/app/services/socket/socketMessageSender.service.ts
@@ -15,13 +15,36 @@ export class SocketMessageSenderService implements SocketSubscriber {
     }
 
     private constructor(public socketStringFormaterService: SocketStringFormaterService) {
+    }
+
+    public subscribeToSocket(): void {
         SocketHandlerService.getInstance().subscribe(Event.UserConnected, this);
         SocketHandlerService.getInstance().subscribe(Event.UserDisconnected, this);
         SocketHandlerService.getInstance().subscribe(Event.NewUser, this);
     }
 
     public notify(event: Event, message: ICommonSocketMessage): void {
+        switch (event) {
+            case Event.UserDisconnected: {
+                const data: string = this.socketStringFormaterService.onUserDisconnected(message);
+                this.showMessage(data);
+                break;
+            }
+            case Event.NewUser: {
+                const data: string = this.socketStringFormaterService.onNewUser(message);
+                this.showMessage(data);
+                break;
+            }
+            default: {
+                const data: string = this.socketStringFormaterService.onDefault();
+                this.showMessage(data);
+             }
+         }
+    }
+
+    private showMessage(data: string): void {
+        // Met data dans chat
         // tslint:disable-next-line:no-console
-        console.log(this.socketStringFormaterService.messageFormater(event, message));
+        console.log(data + "data");
     }
 }
