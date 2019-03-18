290d08f84ed4b5ac6c23061698a03c450e1a3432
Felix <felix-atnoine.bourbonnais@polymtl.ca>
2019-03-18 12:31:54 -0400

    (d) Added color change condition (FAB)(BC)

--- client/src/app/services/3DObjects/GeometricObjects/object-restoration.ts
+++ client/src/app/services/3DObjects/GeometricObjects/object-restoration.ts
@@ -13,6 +13,23 @@ export class ObjectRestoration {
                          intersectsModified: THREE.Intersection[]): DifferenceTypeObject3D {
 
         let differenceType: DifferenceTypeObject3D = DifferenceTypeObject3D.none;
+        if (intersectsOriginal.length > 0 && intersectsModified.length > 0) {
+            if (hit) {
+                // tslint:disable:no-any
+                let intersectedModified: any;
+                let intersectedOriginal: any;
+                intersectedOriginal = intersectsOriginal[0].object;
+                intersectedModified = intersectsModified[0].object;
+
+                intersectedModified.material.color.getHex() !== intersectedOriginal.material.color.getHex() ?
+                    differenceType = DifferenceTypeObject3D.colorObject : differenceType = DifferenceTypeObject3D.none;
+
+            } else if (intersectsOriginal[0].distance > intersectsModified[0].distance) {
+                differenceType = DifferenceTypeObject3D.removeObject;
+            } else if (intersectsOriginal[0].distance < intersectsModified[0].distance) {
+                differenceType = DifferenceTypeObject3D.addObject;
+            }
+            // tslint:disable-next-line:prefer-conditional-expression
 
         return differenceType;
     }
