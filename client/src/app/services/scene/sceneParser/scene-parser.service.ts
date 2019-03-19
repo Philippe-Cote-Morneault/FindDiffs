97cbed7627dd452b614605a35b3f25165fde0e0a
Martin Pouliot <martinp507@gmail.com>
2019-03-18 22:28:23 -0400

    (r) Implemented requiered methods in scene parser (MP)

--- client/src/app/services/scene/sceneParser/scene-parser.service.ts
+++ client/src/app/services/scene/sceneParser/scene-parser.service.ts
@@ -8,18 +8,22 @@ import { AbstractSceneParser } from "./abstractSceneParserService";
     providedIn: "root",
 })
 export class SceneParserService extends AbstractSceneParser {
+    public constructor(scene: ICommonScene) {
+        super(scene);
+    }
 
-    public async parseScene(sceneModel: ICommonScene): Promise<THREE.Scene> {
-        const scene: THREE.Scene = await this.createScene(sceneModel);
+    public async parseScene(): Promise<THREE.Scene> {
+        const scene: THREE.Scene = await this.createScene();
 
-        await this.parseObjects(scene, sceneModel.sceneObjects);
+        await this.parseObjects(scene, this.sceneModel.sceneObjects);
 
         return scene;
     }
 
     private async parseObjects(scene: THREE.Scene, sceneObjects: ICommonSceneObject[]): Promise<void> {
 
-        const promises: Promise<THREE.Object3D>[] = sceneObjects.map((object: ICommonSceneObject) => this.sceneObjectParser.parse(object));
+        const promises: Promise<THREE.Object3D>[] = sceneObjects.map(
+            async (object: ICommonSceneObject) => this.sceneObjectParser.parse(object));
         await Promise.all(promises).then((v: THREE.Object3D[]) => {
             scene.add(...v);
         });
