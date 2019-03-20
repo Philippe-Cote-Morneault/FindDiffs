9112a31c6571af1d8a1222819e71e67c31d124ea
Martin Pouliot <martinp507@gmail.com>
2019-03-20 11:51:12 -0400

    (t) Added test for coneFactory (MP)

---
+++ server/app/services/scene/objectGeneration/shapeCreation/coneFactory.spec.ts
@@ -0,0 +1,9 @@
+import { expect } from "chai";
+import * as sinon from "sinon";
+import { ICommon3DPosition } from "../../../../../../common/model/positions";
+import { GeometricShapeType, ICommonGeometricObject } from "../../../../../../common/model/scene/objects/geometricObjects/geometricObject";
+import { ConeFactory } from "./coneFactory";
+import { GeometricObjectGenerator } from "./geometricObjectGenerator";
+describe("ConeFactory", () => {
+
+});
