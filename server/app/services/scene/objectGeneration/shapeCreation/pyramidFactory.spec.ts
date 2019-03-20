17848d291f0f201cd7918883fc564fcbb9c27000
Martin Pouliot <martinp507@gmail.com>
2019-03-20 12:01:25 -0400

    (t) Added tests for pyramid factory (MP)

---
+++ server/app/services/scene/objectGeneration/shapeCreation/pyramidFactory.spec.ts
@@ -0,0 +1,10 @@
+import { expect } from "chai";
+import * as sinon from "sinon";
+import { ICommon3DPosition } from "../../../../../../common/model/positions";
+import { GeometricShapeType, ICommonGeometricObject } from "../../../../../../common/model/scene/objects/geometricObjects/geometricObject";
+import { GeometricObjectGenerator } from "./geometricObjectGenerator";
+import { PyramidFactory } from "./pyramidFactory";
+describe("PyramidFactory", () => {
+
+    
+});
