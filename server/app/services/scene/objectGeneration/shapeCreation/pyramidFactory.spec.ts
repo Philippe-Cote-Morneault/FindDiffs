8dd62fe5c06b31a4d727c9708cd4e1cf31eb2f53
Martin Pouliot <martinp507@gmail.com>
2019-03-20 12:02:05 -0400

    (t) Added test to validate type of pyramidfactory (MP)

--- server/app/services/scene/objectGeneration/shapeCreation/pyramidFactory.spec.ts
+++ server/app/services/scene/objectGeneration/shapeCreation/pyramidFactory.spec.ts
@@ -6,5 +6,26 @@ import { GeometricObjectGenerator } from "./geometricObjectGenerator";
 import { PyramidFactory } from "./pyramidFactory";
 describe("PyramidFactory", () => {
 
+    beforeEach(() => {
+        sinon.stub(Math, "random");
+    });
+    afterEach(() => {
+        (Math.random as sinon.SinonStub).restore();
+    });
+
+    describe("createShape()", () => {
+        it("Should return a pyramid", () => {
+            // tslint:disable-next-line:no-magic-numbers
+            (Math.random as sinon.SinonStub).returns(0.6);
+            const generator: GeometricObjectGenerator = new GeometricObjectGenerator();
+            const position: ICommon3DPosition = {
+                x: 0,
+                y: 0,
+                z: 0,
+            };
+            const object: ICommonGeometricObject =  generator.createObject(position);
+            expect(object.shapeType).to.equal(GeometricShapeType.TRIANGULAR_BASE_PYRAMID);
+        });
     
+    });
 });
