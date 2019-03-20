5dea05377226310be36ecdd0ba736425bfb9bc80
Martin Pouliot <martinp507@gmail.com>
2019-03-20 12:10:00 -0400

    (t) Added test to validate orientation of the various objects (MP)

--- server/app/services/scene/objectGeneration/themeCreation/simpleObjectFactory.spec.ts
+++ server/app/services/scene/objectGeneration/themeCreation/simpleObjectFactory.spec.ts
@@ -6,6 +6,24 @@ import { ThemeObjectGenerator } from "./themeObjectGenerator";
 
 describe("simpleObjectFactory", () => {
     describe("createObject()", () => {
+        it("Should return an with the orientation facing various direction", () => {
+            const NUMBER_DIRECTION_MIN: number = 5;
+            const ITERATIONS: number = 50;
+            const position: IPositionGridTheme = {
+                surface: ThemeSurface.PARKING,
+                x: 50,
+                y: 45,
+                z: 34,
+            };
+            const direction: Set<number> = new Set<number>();
+            for (let i: number = 0; i < ITERATIONS; i++) {
+                if (direction.size === NUMBER_DIRECTION_MIN) {
+                    break;
+                }
+                direction.add(ThemeObjectGenerator.getInstance().createObject(position).orientation.yAngle);
+            }
+            expect(direction.size).to.equal(NUMBER_DIRECTION_MIN);
+        });
         
     });
 });
