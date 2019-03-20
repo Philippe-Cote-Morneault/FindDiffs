6d1fe0de66231af03cc5e71b5110a4f4d6f538a6
Martin Pouliot <martinp507@gmail.com>
2019-03-20 11:57:29 -0400

    (t) Added a test to validate the type of the cylinderfactory (MP)

--- server/app/services/scene/objectGeneration/shapeCreation/cylinderFactory.spec.ts
+++ server/app/services/scene/objectGeneration/shapeCreation/cylinderFactory.spec.ts
@@ -6,5 +6,26 @@ import { CylinderFactory } from "./cylinderFactory";
 import { GeometricObjectGenerator } from "./geometricObjectGenerator";
 describe("CylinderFactory", () => {
 
+    beforeEach(() => {
+        sinon.stub(Math, "random");
+    });
+    afterEach(() => {
+        (Math.random as sinon.SinonStub).restore();
+    });
+
+    describe("createShape()", () => {
+        it("Should return a cylinder", () => {
+            // tslint:disable-next-line:no-magic-numbers
+            (Math.random as sinon.SinonStub).returns(0.5);
+            const generator: GeometricObjectGenerator = new GeometricObjectGenerator();
+            const position: ICommon3DPosition = {
+                x: 0,
+                y: 0,
+                z: 0,
+            };
+            const object: ICommonGeometricObject =  generator.createObject(position);
+            expect(object.shapeType).to.equal(GeometricShapeType.CYLINDER);
+        });
     
+    });
 });
