3b65f7453e70078814cfdb83d506c53ba4970119
Martin Pouliot <martinp507@gmail.com>
2019-03-20 12:13:45 -0400

    (t) Added tests for the return type of car (MP)

--- server/app/services/scene/objectGeneration/themeCreation/themeObjectGenerator.spec.ts
+++ server/app/services/scene/objectGeneration/themeCreation/themeObjectGenerator.spec.ts
@@ -3,5 +3,16 @@ import { ICommonThematicObject, ObjTheme, ThemeSurface } from "../../../../../..
 import { IPositionGridTheme } from "../../grid/theme/themeGrid";
 import { ThemeObjectGenerator } from "./themeObjectGenerator";
 describe("ThemeObjectGenerator", () => {
-    
+    describe("createObject()", () => {
+        it("Should return a car when the position is on car", () => {
+            const position: IPositionGridTheme = {
+                surface: ThemeSurface.CAR,
+                x: 50,
+                y: 0,
+                z: 50,
+            };
+            const object: ICommonThematicObject = ThemeObjectGenerator.getInstance().createObject(position);
+            expect([ObjTheme.LEXUS, ObjTheme.ECLIPSE, ObjTheme.LAMBO]).to.include.members([object.objectType]);
+        });
+    });
 });
