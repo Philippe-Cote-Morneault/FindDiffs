e5fd3fe54a4f85e1ef82b0a2909018808735d382
Martin Pouliot <martinp507@gmail.com>
2019-03-20 11:52:46 -0400

    (t) Added test to validate cone height in range (MP)

--- server/app/services/scene/objectGeneration/shapeCreation/coneFactory.spec.ts
+++ server/app/services/scene/objectGeneration/shapeCreation/coneFactory.spec.ts
@@ -41,5 +41,20 @@ describe("ConeFactory", () => {
             .lte((ConeFactory.SIZE_MAX_PERCENTAGE / ConeFactory.PERCENTAGE_DIVISION) * ConeFactory.REFERENCE_RADIUS)
             .and.gte((ConeFactory.SIZE_MIN_PERCENTAGE / ConeFactory.PERCENTAGE_DIVISION) * ConeFactory.REFERENCE_RADIUS);
         });
+
+        it("Should return a cone with a height in range", () => {
+            // tslint:disable-next-line:no-magic-numbers
+            (Math.random as sinon.SinonStub).returns(0.2);
+            const generator: GeometricObjectGenerator = new GeometricObjectGenerator();
+            const position: ICommon3DPosition = {
+                x: 0,
+                y: 0,
+                z: 0,
+            };
+            const object: ICommonGeometricObject =  generator.createObject(position);
+            expect(object["height"]).to.be
+            .lte((ConeFactory.SIZE_MAX_PERCENTAGE / ConeFactory.PERCENTAGE_DIVISION) * ConeFactory.REFERENCE_HEIGHT)
+            .and.gte((ConeFactory.SIZE_MIN_PERCENTAGE / ConeFactory.PERCENTAGE_DIVISION) * ConeFactory.REFERENCE_HEIGHT);
+        });
     });
 });
