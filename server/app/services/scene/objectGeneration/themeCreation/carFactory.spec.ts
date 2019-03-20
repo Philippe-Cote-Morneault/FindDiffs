c048b046cc01856112155b53af61c548dac89b2b
Martin Pouliot <martinp507@gmail.com>
2019-03-20 12:05:41 -0400

    (t) Added tests for the car factory (MP)

---
+++ server/app/services/scene/objectGeneration/themeCreation/carFactory.spec.ts
@@ -0,0 +1,9 @@
+import { expect } from "chai";
+import { ICommonThematicObject, ThemeSurface } from "../../../../../../common/model/scene/objects/thematicObjects/thematicObject";
+import { IPositionGridTheme } from "../../grid/theme/themeGrid";
+import { ObjectFactory } from "../objectFactory";
+import { ThemeObjectGenerator } from "./themeObjectGenerator";
+
+describe("carFactory", () => {
+    
+});
