b226f741d1c660aeac997b0e50bb7a72cc6976b9
Martin Pouliot <martinp507@gmail.com>
2019-03-20 12:14:29 -0400

    (t) Added tests that validates the generation of grass objects (MP)

--- server/app/services/scene/objectGeneration/themeCreation/themeObjectGenerator.spec.ts
+++ server/app/services/scene/objectGeneration/themeCreation/themeObjectGenerator.spec.ts
@@ -14,5 +14,20 @@ describe("ThemeObjectGenerator", () => {
             const object: ICommonThematicObject = ThemeObjectGenerator.getInstance().createObject(position);
             expect([ObjTheme.LEXUS, ObjTheme.ECLIPSE, ObjTheme.LAMBO]).to.include.members([object.objectType]);
         });
+        it("Should return an object that goes on grass", () => {
+            const position: IPositionGridTheme = {
+                surface: ThemeSurface.GRASS,
+                x: 50,
+                y: 0,
+                z: 50,
+            };
+            const object: ICommonThematicObject = ThemeObjectGenerator.getInstance().createObject(position);
+            expect([ObjTheme.BIN,
+                    ObjTheme.METER,
+                    ObjTheme.LAMP,
+                    ObjTheme.SIGN_FORBIDDEN,
+                    ObjTheme.SIGN_SKIP,
+                    ObjTheme.SIGN_STOP]).to.include.members([object.objectType]);
+        });
     });
 });
