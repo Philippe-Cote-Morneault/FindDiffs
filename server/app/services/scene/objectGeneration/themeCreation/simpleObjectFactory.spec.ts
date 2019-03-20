99bc66c9293c1fecbf802d13c9d98676141ef5e1
Martin Pouliot <martinp507@gmail.com>
2019-03-20 12:10:27 -0400

    (t) Added test to test the range of the orientation (MP)

--- server/app/services/scene/objectGeneration/themeCreation/simpleObjectFactory.spec.ts
+++ server/app/services/scene/objectGeneration/themeCreation/simpleObjectFactory.spec.ts
@@ -24,6 +24,28 @@ describe("simpleObjectFactory", () => {
             }
             expect(direction.size).to.equal(NUMBER_DIRECTION_MIN);
         });
+        it("Should return values with orientation less than max radian and superior to zero for grass objects", () => {
+            const ITERATIONS: number = 50;
+            const NUMBER_DIRECTION_MIN: number = 5;
+            const position: IPositionGridTheme = {
+                surface: ThemeSurface.GRASS,
+                x: 50,
+                y: 34,
+                z: 321,
+            };
+            const directions: Set<number> = new Set<number>();
+            for (let i: number = 0; i < ITERATIONS; i++) {
+                if (directions.size === NUMBER_DIRECTION_MIN) {
+                    break;
+                }
+                directions.add(ThemeObjectGenerator.getInstance().createObject(position).orientation.yAngle);
+            }
+            Array.from(directions).forEach((direction: number) => {
+                expect(direction).to.be.lte(ObjectFactory.MAX_RADIAN_ANGLE);
+                expect(direction).to.be.gte(ObjectFactory.MIN_RADIAN_ANGLE);
+            });
+        });
+
         
     });
 });
