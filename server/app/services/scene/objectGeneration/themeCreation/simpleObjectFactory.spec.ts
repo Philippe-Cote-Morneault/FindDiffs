3cb0c3c0555a0757980dcc3123efab4ee7d4972f
Martin Pouliot <martinp507@gmail.com>
2019-03-20 12:09:29 -0400

    (t) Added test for simple object factory (MP)

---
+++ server/app/services/scene/objectGeneration/themeCreation/simpleObjectFactory.spec.ts
@@ -0,0 +1,11 @@
+import { expect } from "chai";
+import { ICommonThematicObject, ThemeSurface } from "../../../../../../common/model/scene/objects/thematicObjects/thematicObject";
+import { IPositionGridTheme } from "../../grid/theme/themeGrid";
+import { ObjectFactory } from "../objectFactory";
+import { ThemeObjectGenerator } from "./themeObjectGenerator";
+
+describe("simpleObjectFactory", () => {
+    describe("createObject()", () => {
+        
+    });
+});
