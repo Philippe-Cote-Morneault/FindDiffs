de5d9299d6dfc8641cdb81ff2de2b92015ad1e9e
Martin Pouliot <martinp507@gmail.com>
2019-03-20 12:05:12 -0400

    (t) Added missing lines to the last test (MP)

--- server/app/services/scene/objectGeneration/shapeCreation/sphereFactory.spec.ts
+++ server/app/services/scene/objectGeneration/shapeCreation/sphereFactory.spec.ts
@@ -26,6 +26,20 @@ describe("SphereFactory", () => {
             const object: ICommonGeometricObject =  generator.createObject(position);
             expect(object.shapeType).to.equal(GeometricShapeType.SPHERE);
         });
+
+        it("Should return a sphere with a radius in range", () => {
+            // tslint:disable-next-line:no-magic-numbers
+            (Math.random as sinon.SinonStub).returns(0.5);
+            const generator: GeometricObjectGenerator = new GeometricObjectGenerator();
             const position: ICommon3DPosition = {
+                x: 0,
+                y: 0,
+                z: 0,
+            };
+            const object: ICommonGeometricObject =  generator.createObject(position);
+            expect(object["radius"]).to.be
+            .lte((SphereFactory.SIZE_MAX_PERCENTAGE / SphereFactory.PERCENTAGE_DIVISION) * SphereFactory.REFERENCE_RADIUS)
+            .and.gte((SphereFactory.SIZE_MIN_PERCENTAGE / SphereFactory.PERCENTAGE_DIVISION) * SphereFactory.REFERENCE_RADIUS);
+        });
     });
 });
