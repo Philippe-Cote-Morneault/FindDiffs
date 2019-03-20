2fc3672ffe7e31301871d5539a7e2a153bf92845
Martin Pouliot <martinp507@gmail.com>
2019-03-20 11:53:40 -0400

    (t) Added tests for cube factory (MP)

---
+++ server/app/services/scene/objectGeneration/shapeCreation/cubeFactory.spec.ts
@@ -0,0 +1,10 @@
+import { expect } from "chai";
+import * as sinon from "sinon";
+import { ICommon3DPosition } from "../../../../../../common/model/positions";
+import { GeometricShapeType, ICommonGeometricObject } from "../../../../../../common/model/scene/objects/geometricObjects/geometricObject";
+import { CubeFactory } from "./cubeFactory";
+import { GeometricObjectGenerator } from "./geometricObjectGenerator";
+describe("CubeFactory", () => {
+
+
+});
