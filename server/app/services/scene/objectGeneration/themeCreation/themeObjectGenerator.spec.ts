e7aeaa434863fb92e65beff70fb8e332e63ff4e4
Martin Pouliot <martinp507@gmail.com>
2019-03-20 12:13:11 -0400

    (t) Added tests for themeobjectgenerator (MP)

---
+++ server/app/services/scene/objectGeneration/themeCreation/themeObjectGenerator.spec.ts
@@ -0,0 +1,7 @@
+import { expect } from "chai";
+import { ICommonThematicObject, ObjTheme, ThemeSurface } from "../../../../../../common/model/scene/objects/thematicObjects/thematicObject";
+import { IPositionGridTheme } from "../../grid/theme/themeGrid";
+import { ThemeObjectGenerator } from "./themeObjectGenerator";
+describe("ThemeObjectGenerator", () => {
+    
+});
