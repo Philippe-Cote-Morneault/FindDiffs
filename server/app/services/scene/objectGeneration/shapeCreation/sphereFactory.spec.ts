3e437c4ae7dcf58ac050d5b949cf82aa1c4c8440
Martin Pouliot <martinp507@gmail.com>
2019-03-20 12:03:36 -0400

    (t) Added test for the sphereFactory (MP)

---
+++ server/app/services/scene/objectGeneration/shapeCreation/sphereFactory.spec.ts
@@ -0,0 +1,10 @@
+import { expect } from "chai";
+import * as sinon from "sinon";
+import { ICommon3DPosition } from "../../../../../../common/model/positions";
+import { GeometricShapeType, ICommonGeometricObject } from "../../../../../../common/model/scene/objects/geometricObjects/geometricObject";
+import { GeometricObjectGenerator } from "./geometricObjectGenerator";
+import { SphereFactory } from "./sphereFactory";
+describe("SphereFactory", () => {
+
+
+});
