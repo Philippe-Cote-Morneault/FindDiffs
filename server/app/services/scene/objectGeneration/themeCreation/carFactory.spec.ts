05eb2105c4d3496b9099e97c0182f138fdd81ce4
Martin Pouliot <martinp507@gmail.com>
2019-03-20 12:06:19 -0400

    (t) Added test to validate orientation of car (MP)

--- server/app/services/scene/objectGeneration/themeCreation/carFactory.spec.ts
+++ server/app/services/scene/objectGeneration/themeCreation/carFactory.spec.ts
@@ -5,5 +5,26 @@ import { ObjectFactory } from "../objectFactory";
 import { ThemeObjectGenerator } from "./themeObjectGenerator";
 
 describe("carFactory", () => {
-    
+    describe("createObject()", () => {
+        it("Should return a car with the orientation facing north or south", () => {
+            const NUMBER_DIRECTION: number = 2;
+            const ITERATIONS: number = 50;
+            const position: IPositionGridTheme = {
+                surface: ThemeSurface.CAR,
+                x: 50,
+                y: 45,
+                z: 34,
+            };
+            const direction: Set<number> = new Set<number>();
+            for (let i: number = 0; i < ITERATIONS; i++) {
+                if (direction.size === NUMBER_DIRECTION) {
+                    break;
+                }
+                direction.add(ThemeObjectGenerator.getInstance().createObject(position).orientation.yAngle);
+            }
+            expect(direction.size).to.equal(NUMBER_DIRECTION);
+            expect([0,
+                    // tslint:disable-next-line:no-magic-numbers
+                    ObjectFactory.MAX_RADIAN_ANGLE / 2]).to.include.members(Array.from(direction));
+        });
 });
