87b503bafdbbbeba39bb8192373108b72430338a
Martin Pouliot <martinp507@gmail.com>
2019-03-20 12:04:23 -0400

    (t) Added a test to validate the type of the sphere factory (MP)

--- server/app/services/scene/objectGeneration/shapeCreation/sphereFactory.spec.ts
+++ server/app/services/scene/objectGeneration/shapeCreation/sphereFactory.spec.ts
@@ -6,5 +6,25 @@ import { GeometricObjectGenerator } from "./geometricObjectGenerator";
 import { SphereFactory } from "./sphereFactory";
 describe("SphereFactory", () => {
 
+    beforeEach(() => {
+        sinon.stub(Math, "random");
+    });
+    afterEach(() => {
+        (Math.random as sinon.SinonStub).restore();
+    });
 
+    describe("createShape()", () => {
+        it("Should return a sphere", () => {
+            // tslint:disable-next-line:no-magic-numbers
+            (Math.random as sinon.SinonStub).returns(0.8);
+            const generator: GeometricObjectGenerator = new GeometricObjectGenerator();
+            const position: ICommon3DPosition = {
+                x: 0,
+                y: 0,
+                z: 0,
+            };
+            const object: ICommonGeometricObject =  generator.createObject(position);
+            expect(object.shapeType).to.equal(GeometricShapeType.SPHERE);
+        });
+    });
 });
