9e15f648e873b8b9dfe5710da9cbf46d8992b3ee
Basil <bas98c@hotmail.com>
2019-03-20 17:12:25 -0400

    (d) added loops to verify if an object is in the scene (BC)

--- server/app/services/difference/revealDifference3D.ts
+++ server/app/services/difference/revealDifference3D.ts
@@ -1,24 +1,51 @@
-import { ICommonReveal3D } from "../../../../common/model/reveal";
-import { ICommonGeometricObject } from "../../../../common/model/scene/objects/geometricObjects/geometricObject";
-import { ICommonScene } from "../../../../common/model/scene/scene";
+import { DifferenceType, ICommonReveal3D } from "../../../../common/model/reveal";
+import { ICommonGeometricModifications } from "../../../../common/model/scene/modifications/geometricModifications";
+import { ICommonSceneModifications } from "../../../../common/model/scene/modifications/sceneModifications";
+// import { ICommonSceneObject } from "../../../../common/model/scene/objects/sceneObject";
+// import { ICommonGeometricObject } from "../../../../common/model/scene/objects/geometricObjects/geometricObject";
+// import { ICommonScene } from "../../../../common/model/scene/scene";
 
 export class RevealDifference3D {
-    private originalScene: ICommonScene;
-    private modifiedObjectId: string;
+    private modifiedScene: ICommonSceneModifications;
+    private originalObjectId: string;
 
-    public constructor(originalScene: ICommonScene, modifiedObjectId: string) {
-        this.originalScene = originalScene;
-        this.modifiedObjectId = modifiedObjectId;
+    public constructor(modifiedScene: ICommonSceneModifications, originalObjectId: string) {
+        this.modifiedScene = modifiedScene;
+        this.originalObjectId = originalObjectId;
     }
 
+    // tslint:disable
     public reveal(): ICommonReveal3D {
-        const returnValue: ICommonReveal3D = {hit: false};
-        this.originalScene.sceneObjects.forEach((element: ICommonGeometricObject) => {
-            if (element.id === this.modifiedObjectId) {
+        const returnValue: ICommonReveal3D = {
+            hit: false,
+            differenceType: DifferenceType.none,
+        };
+        
+        for (const element of this.modifiedScene.deletedObjects) {
+            if (element === this.originalObjectId) {
                 returnValue.hit = true;
+                returnValue.differenceType = DifferenceType.removedObject;
+
+                return returnValue;
             }
-        });
-        returnValue.hit = false;
+        }
+
+        for (const element of (this.modifiedScene as ICommonGeometricModifications).colorChangedObjects) {
+            if (element.key === this.originalObjectId) {
+                returnValue.hit = true;
+                returnValue.differenceType = DifferenceType.colorChanged;
+
+                return returnValue;
+            }
+        }
+
+
+        // this.modifiedScene.sceneObjects.forEach((element: ICommonGeometricObject) => {
+        //     if (element.id === this.modifiedObjectId) {
+        //         returnValue.hit = true;
+        //     }
+        // });
+        // returnValue.hit = false;
 
         return returnValue;
     }
