0cc440af6d42419b2c191480f999b7409fadb515
Jacob Meilleur <jacob.meilleur@polymtl.ca>
2019-03-20 21:03:15 -0400

    (t) boolean should be false on creation (JM)

--- client/src/app/services/cheatMode/cheat-mode.service.spec.ts
+++ client/src/app/services/cheatMode/cheat-mode.service.spec.ts
@@ -1,12 +1,14 @@
+
 import { TestBed } from "@angular/core/testing";
-
+import { expect } from "chai";
 import { CheatModeService } from "./cheat-mode.service";
+// tslint:disable
+describe("Tests pour le service: CheatModeService", () => {
 
-describe("CheatModeService", () => {
+  const cheatModeService: CheatModeService = new CheatModeService();
   beforeEach(() => TestBed.configureTestingModule({}));
 
-  it("should be created", () => {
-    const service: CheatModeService = TestBed.get(CheatModeService);
-    expect(service).toBeTruthy();
+  it("should be false on creation", () => {
+    expect(cheatModeService.cheatActivated).to.be.false;
   });
 });
