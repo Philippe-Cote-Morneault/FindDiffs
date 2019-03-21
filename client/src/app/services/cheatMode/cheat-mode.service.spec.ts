d87e58ea13fe659ce5d3e4182fe83c840066f791
Jacob Meilleur <jacob.meilleur@polymtl.ca>
2019-03-20 21:08:36 -0400

    (t) cheatActivated value change to true when method is called (JM)

--- client/src/app/services/cheatMode/cheat-mode.service.spec.ts
+++ client/src/app/services/cheatMode/cheat-mode.service.spec.ts
@@ -1,6 +1,10 @@
 
 import { TestBed } from "@angular/core/testing";
 import { expect } from "chai";
+import * as sinon from "sinon";
+import { ICommonGeometricModifications } from "../../../../../common/model/scene/modifications/geometricModifications";
+import { ICommonThematicModifications } from "../../../../../common/model/scene/modifications/thematicModifications";
+import { ObjectType } from "../../../../../common/model/scene/scene";
 import { CheatModeService } from "./cheat-mode.service";
 // tslint:disable
 describe("Tests for CheatModeService", () => {
@@ -14,6 +18,23 @@ describe("Tests for CheatModeService", () => {
   describe("testing the value of cheatActivated attribute in various situations", () => {
     it("should be false on creation of the service", () => {
       expect(cheatModeService.cheatActivated).to.be.false;
+    });
+
+    it("should change cheatActivated to true when the method toggleCheatMode is called once", () => {
+      const stub: sinon.SinonStub = sinon.stub(cheatModeService, "toggleCheatMode").callsFake(
+        () => {cheatModeService.cheatActivated = !cheatModeService.cheatActivated;
+      });
+      const blankModifiedScene: ICommonGeometricModifications & ICommonThematicModifications = {
+        id: "",
+        type: ObjectType.Geometric,
+        addedObjects: [],
+        deletedObjects: [],
+        colorChangedObjects: [],
+        texturesChangedObjects: [],
+      };
+      cheatModeService.toggleCheatMode(blankModifiedScene);
+      expect(cheatModeService.cheatActivated).to.be.true;
+      stub.restore();
     });
   });
 });
