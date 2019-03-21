324cc77b050b098af1c59a7eb9a89c75f1499439
Laurent Barberis <laurent.barberis15@gmail.com>
2019-03-21 16:36:13 -0400

    (d) IdError now works (LB)

--- client/src/app/services/IdentificationError/identificationError.service.ts
+++ client/src/app/services/IdentificationError/identificationError.service.ts
@@ -8,7 +8,6 @@ import { SocketSubscriber } from "../socket/socketSubscriber";
     providedIn: "root",
 })
 export class IdentificationError implements SocketSubscriber {
-    private static instance: IdentificationError;
     private static readonly ERROR_SOUND_SRC: string = "../../assets/no.mp3";
     private static readonly ONE_SECOND: number = 1000;
     private errorMessage: HTMLElement;
@@ -18,30 +17,24 @@ export class IdentificationError implements SocketSubscriber {
     public timeout: boolean;
     private errorSound: HTMLAudioElement;
 
-    public static getInstance(): IdentificationError {
-        if (!IdentificationError.instance) {
-            IdentificationError.instance = new IdentificationError();
-        }
-
-        return IdentificationError.instance;
-    }
-
-    public constructor() {
+    public constructor(errorMessage: HTMLElement, original: HTMLElement, modified: HTMLElement) {
+        this.errorMessage = errorMessage;
+        this.original = original;
+        this.modified = modified;
         this.timeout = false;
         this.errorSound = new Audio;
         this.errorSound.src = IdentificationError.ERROR_SOUND_SRC;
         this.errorSound.load();
-        this.subscribeToSocket();
     }
 
-    private subscribeToSocket(): void {
+    public subscribeToSocket(): void {
         SocketHandlerService.getInstance().subscribe(Event.InvalidClick, this);
     }
 
     public notify(event: Event, message: ICommonSocketMessage): void {
-        // Call showErrorMessage
         // tslint:disable-next-line:no-console
-        console.log(event + message + this.errorMessage + this.original + this.modified);
+        console.log(event + message);
+        this.showErrorMessage();
     }
 
     public setIdentificationError(errorMessage: HTMLElement, original: HTMLElement, modified: HTMLElement): void {
@@ -50,35 +43,32 @@ export class IdentificationError implements SocketSubscriber {
         this.modified = modified;
     }
 
-    public async showErrorMessage(xPosition: number, yPosition: number,
-                                  errorMessage: HTMLElement, original: HTMLElement, modified: HTMLElement ): Promise<void> {
+    public async showErrorMessage(): Promise<void> {
         this.timeout = true;
-        this.moveClickError(xPosition, yPosition, errorMessage);
-        this.showClickError(errorMessage, original, modified);
+        this.showClickError();
         await this.errorSound.play();
-
         setTimeout(() => {
-            this.hideClickError(errorMessage, original, modified);
+            this.hideClickError();
             this.timeout = false;
                 }, IdentificationError.ONE_SECOND);
     }
 
-    private moveClickError(xPosition: number, yPosition: number, errorMessage: HTMLElement): void {
-        errorMessage.style.top = yPosition + R.PIXEL;
-        errorMessage.style.left = xPosition + R.PIXEL;
+    public moveClickError(xPosition: number, yPosition: number): void {
+        this.errorMessage.style.top = yPosition + R.PIXEL;
+        this.errorMessage.style.left = xPosition + R.PIXEL;
     }
 
-    private showClickError(errorMessage: HTMLElement, original: HTMLElement, modified: HTMLElement): void {
-        errorMessage.style.display = R.INLINE;
-        original.style.cursor = R.NOT_ALLOWED;
-        modified.style.cursor = R.NOT_ALLOWED;
-        errorMessage.style.cursor = R.NOT_ALLOWED;
+    private showClickError(): void {
+        this.errorMessage.style.display = R.INLINE;
+        this.original.style.cursor = R.NOT_ALLOWED;
+        this.modified.style.cursor = R.NOT_ALLOWED;
+        this.errorMessage.style.cursor = R.NOT_ALLOWED;
     }
 
-    private hideClickError(errorMessage: HTMLElement, original: HTMLElement, modified: HTMLElement): void {
-        errorMessage.style.display = R.NONE;
-        original.style.cursor = R.CONTEXT_MENU;
-        modified.style.cursor = R.CONTEXT_MENU;
-        errorMessage.style.cursor = R.CONTEXT_MENU;
+    private hideClickError(): void {
+        this.errorMessage.style.display = R.NONE;
+        this.original.style.cursor = R.CONTEXT_MENU;
+        this.modified.style.cursor = R.CONTEXT_MENU;
+        this.errorMessage.style.cursor = R.CONTEXT_MENU;
     }
 }
