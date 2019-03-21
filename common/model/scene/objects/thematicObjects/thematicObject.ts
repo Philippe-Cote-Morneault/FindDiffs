e1dd55125220020856cfdcb99d6d4efa95a0c2f8
Jacob Meilleur <jacob.meilleur@polymtl.ca>
2019-03-21 15:51:46 -0400

    (r) added a missing space (JM)

--- common/model/scene/objects/thematicObjects/thematicObject.ts
+++ common/model/scene/objects/thematicObjects/thematicObject.ts
@@ -1,7 +1,7 @@
 import { ICommonSceneObject } from "../sceneObject";
 
 export enum ObjTheme {
-    BIN, CONE, ECLIPSE, LAMBO, LAMP, LEXUS,METER, SIGN_FORBIDDEN, SIGN_SKIP, SIGN_STOP
+    BIN, CONE, ECLIPSE, LAMBO, LAMP, LEXUS, METER, SIGN_FORBIDDEN, SIGN_SKIP, SIGN_STOP
 };
 
 export enum ThemeSurface {
