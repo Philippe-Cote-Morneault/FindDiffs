e53dfec8f44cd782bcb2ab35220d25ada5f529d6
Laurent Barberis <laurent.barberis15@gmail.com>
2019-03-21 11:49:16 -0400

    (d) Added subscribe (LB)

--- client/src/app/services/IdentificationError/identificationError.service.ts
+++ client/src/app/services/IdentificationError/identificationError.service.ts
@@ -1,25 +1,53 @@
 import { Injectable } from "@angular/core";
+import { Event, ICommonSocketMessage } from "../../../../../common/communication/webSocket/socketMessage";
+import { R } from "../../ressources/strings";
+import { SocketHandlerService } from "../socket/socketHandler.service";
+import { SocketSubscriber } from "../socket/socketSubscriber";
 
 @Injectable({
     providedIn: "root",
 })
-export class IdentificationError {
+export class IdentificationError implements SocketSubscriber {
+    private static instance: IdentificationError;
     private static readonly ERROR_SOUND_SRC: string = "../../assets/no.mp3";
     private static readonly ONE_SECOND: number = 1000;
-    private static readonly PIXEL: string = "px";
-    private static readonly INLINE: string = "inline";
-    private static readonly NONE: string = "none";
-    private static readonly NOT_ALLOWED: string = "not-allowed";
-    private static readonly CONTEXT_MENU: string = "context-menu";
+    private errorMessage: HTMLElement;
+    private original: HTMLElement;
+    private modified: HTMLElement;
 
     public timeout: boolean;
     private errorSound: HTMLAudioElement;
+
+    public static getInstance(): IdentificationError {
+        if (!IdentificationError.instance) {
+            IdentificationError.instance = new IdentificationError();
+        }
+
+        return IdentificationError.instance;
+    }
 
     public constructor() {
         this.timeout = false;
         this.errorSound = new Audio;
         this.errorSound.src = IdentificationError.ERROR_SOUND_SRC;
         this.errorSound.load();
+        this.subscribeToSocket();
+    }
+
+    private subscribeToSocket(): void {
+        SocketHandlerService.getInstance().subscribe(Event.InvalidClick, this);
+    }
+
+    public notify(event: Event, message: ICommonSocketMessage): void {
+        // Call showErrorMessage
+        // tslint:disable-next-line:no-console
+        console.log(event + message + this.errorMessage + this.original + this.modified);
+    }
+
+    public setIdentificationError(errorMessage: HTMLElement, original: HTMLElement, modified: HTMLElement): void {
+        this.errorMessage = errorMessage;
+        this.original = original;
+        this.modified = modified;
     }
 
     public async showErrorMessage(xPosition: number, yPosition: number,
@@ -36,21 +64,21 @@ export class IdentificationError {
     }
 
     private moveClickError(xPosition: number, yPosition: number, errorMessage: HTMLElement): void {
-        errorMessage.style.top = yPosition + IdentificationError.PIXEL;
-        errorMessage.style.left = xPosition + IdentificationError.PIXEL;
+        errorMessage.style.top = yPosition + R.PIXEL;
+        errorMessage.style.left = xPosition + R.PIXEL;
     }
 
     private showClickError(errorMessage: HTMLElement, original: HTMLElement, modified: HTMLElement): void {
-        errorMessage.style.display = IdentificationError.INLINE;
-        original.style.cursor = IdentificationError.NOT_ALLOWED;
-        modified.style.cursor = IdentificationError.NOT_ALLOWED;
-        errorMessage.style.cursor = IdentificationError.NOT_ALLOWED;
+        errorMessage.style.display = R.INLINE;
+        original.style.cursor = R.NOT_ALLOWED;
+        modified.style.cursor = R.NOT_ALLOWED;
+        errorMessage.style.cursor = R.NOT_ALLOWED;
     }
 
     private hideClickError(errorMessage: HTMLElement, original: HTMLElement, modified: HTMLElement): void {
-        errorMessage.style.display = IdentificationError.NONE;
-        original.style.cursor = IdentificationError.CONTEXT_MENU;
-        modified.style.cursor = IdentificationError.CONTEXT_MENU;
-        errorMessage.style.cursor = IdentificationError.CONTEXT_MENU;
+        errorMessage.style.display = R.NONE;
+        original.style.cursor = R.CONTEXT_MENU;
+        modified.style.cursor = R.CONTEXT_MENU;
+        errorMessage.style.cursor = R.CONTEXT_MENU;
     }
 }
