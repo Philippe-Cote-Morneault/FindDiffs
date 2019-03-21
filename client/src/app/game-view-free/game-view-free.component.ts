876fb7fc4f4c8b04db1d5db94c8194ae33fdfaf3
Jacob Meilleur <jacob.meilleur@polymtl.ca>
2019-03-20 20:54:46 -0400

    (r) lint corrections (JM)

--- client/src/app/game-view-free/game-view-free.component.ts
+++ client/src/app/game-view-free/game-view-free.component.ts
@@ -22,7 +22,7 @@ import { TimerService } from "../services/timer/timer.service";
 })
 
 export class GameViewFreeComponent implements OnInit {
-    private static T_KEYCODE: number = 84;
+    private static readonly T_STRING: string = "t";
 
     @ViewChild("originalScene") private originalScene: ElementRef;
     @ViewChild("modifiedScene") private modifiedScene: ElementRef;
@@ -61,14 +61,14 @@ export class GameViewFreeComponent implements OnInit {
 
     @HostListener("document:keydown", ["$event"])
     public async toggleCheatMode(event: KeyboardEvent): Promise<void> {
-        if (event.keyCode === GameViewFreeComponent.T_KEYCODE) {
+        if (event.key === GameViewFreeComponent.T_STRING) {
             this.cheatActivated = !this.cheatActivated;
             if (this.cheatActivated) {
                 this.copySceneLoaders();
-                await this.cheatModeTimeoutService.startCheatMode(
-                    this.cheatModeService,
-                    this.currentOriginalScene,
-                    this.currentModifiedScene,
+                this.cheatModeTimeoutService.startCheatMode(
+                                                            this.cheatModeService,
+                                                            this.currentOriginalScene,
+                                                            this.currentModifiedScene,
                 );
             } else {
                 this.cheatModeTimeoutService.stopCheatMode();
