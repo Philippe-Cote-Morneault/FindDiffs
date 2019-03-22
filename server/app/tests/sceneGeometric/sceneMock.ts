c95b59811af8ed65e87ba6e9839e19c385af0a51
Felix <felix-atnoine.bourbonnais@polymtl.ca>
2019-03-21 23:11:44 -0400

    (d) New mocks for scene object mock (FAB)

---
+++ server/app/tests/sceneGeometric/sceneMock.ts
@@ -0,0 +1,8 @@
+import { ICommonGeometricObject } from "../../../../common/model/scene/objects/geometricObjects/geometricObject";
+import { ICommonGeometricScene, ObjectType } from "../../../../common/model/scene/scene";
+import * as MOCKS from "./geometricObjectMocks";
+
+const sceneObjects: ICommonGeometricObject[] = [
+    MOCKS.cube1, MOCKS.cube2, MOCKS.cylinder1, MOCKS.cylinder2, MOCKS.sphere1, MOCKS.sphere2,
+    MOCKS.cone1, MOCKS.cone2, MOCKS.pyramid1, MOCKS.pyramid2,
+];
