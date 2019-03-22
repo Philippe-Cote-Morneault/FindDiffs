648e9e7f0cd9b65ad61f9900812b91a2b996b0b6
Phil <philbond1997@hotmail.com>
2019-03-21 20:46:15 -0400

    (r) Fixed bad socket initialization. (PC)

--- client/src/app/services/socket/socketHandler.service.ts
+++ client/src/app/services/socket/socketHandler.service.ts
@@ -3,10 +3,11 @@ import * as io from "socket.io-client";
 import { Event, ICommonSocketMessage } from "../../../../../common/communication/webSocket/socketMessage";
 import { SERVER_URL } from "../../../../../common/url";
 import { SocketSubscriber } from "./socketSubscriber";
+import { ICommonUser } from "../../../../../common/communication/webSocket/user";
+import { ICommonToken } from "../../../../../common/communication/webSocket/token";
 
-@Injectable({
-    providedIn: "root",
-})
+
+@Injectable()
 
 export class SocketHandlerService {
     private static instance: SocketHandlerService;
@@ -23,21 +24,51 @@ export class SocketHandlerService {
         return this.instance;
     }
     public constructor() {
+        console.log("constructo");
         this.subscribers = new Map<string, SocketSubscriber[]>();
         this.init();
     }
 
     public init(): void {
-        this.socket = io(SERVER_URL);
+        this.socket = io.connect(SERVER_URL, {
+            forceNew: true,
+            reconnection: false,
+        });
         this.id = this.socket.id;
         this.setEventListener();
     }
 
     private setEventListener(): void {
-        this.onNewUserConnected();
-        this.onUserDisconnected();
-        this.onDifferenceFound();
-        this.onInvalidClick();
+        this.socket.on("connect", () => {
+            console.log("onConnect");
+            let token: string | null = sessionStorage.getItem("token");
+            if(token) {
+                console.log("Token already exists = " + token);
+                const tokendata: ICommonToken = {
+                    token: token,
+                };
+                const response: ICommonSocketMessage = {
+                    data: tokendata,
+                    timestamp: new Date(),
+                };
+
+                this.socket.emit(Event.Authenticate, response);
+
+            } else {
+                console.log("token doesnt exist");
+                /*
+                token = (message.data as ICommonToken).token;
+                console.log("Token doesn't exist, one received is " + token);
+                sessionStorage.setItem("token", token);
+                */
+            }
+
+            this.onAuthenticate();
+            this.onNewUserConnected();
+            this.onUserDisconnected();
+            this.onDifferenceFound();
+            this.onInvalidClick();
+        });
     }
 
     public subscribe(event: Event, subscriber: SocketSubscriber): void {
@@ -72,7 +103,8 @@ export class SocketHandlerService {
     }
 
     public onUserDisconnected(): void {
-        this.socket.on(Event.UserDisconnected, (message: ICommonSocketMessage) => {
+        this.socket.on("disconnect", (message: ICommonSocketMessage) => {
+            console.log("disconnectEvent");
             this.notifySubsribers(Event.UserDisconnected, message);
         });
     }
@@ -87,5 +119,48 @@ export class SocketHandlerService {
         this.socket.on(Event.InvalidClick, (message: ICommonSocketMessage) => {
             this.notifySubsribers(Event.InvalidClick, message);
         });
+    }
+
+    // tslint:disable-next-line:max-func-body-length
+    public onAuthenticate(): void {
+        this.socket.on(Event.Authenticate, (message: ICommonSocketMessage) => {
+            console.log(JSON.stringify(message));
+
+            /*
+            let token: string | null = sessionStorage.getItem("token");
+            if(token) {
+                console.log("Token already exists = " + token);
+                const tokendata: ICommonToken = {
+                    token: token,
+                };
+                const response: ICommonSocketMessage = {
+                    data: tokendata,
+                    timestamp: new Date(),
+                };
+
+                this.socket.emit(Event.Authenticate, response);
+
+            } else {
+                token = (message.data as ICommonToken).token;
+                console.log("Token doesn't exist, one received is " + token);
+                sessionStorage.setItem("token", token);
+            }
+            */
+            const token = (message.data as ICommonToken).token;
+            console.log("Token doesn't exist, one received is " + token);
+            sessionStorage.setItem("token", token);
+        });
+    }
+
+    public sendNewUser(username: string): void {
+        console.log("sernding username");
+        const user: ICommonUser = {
+            username: username,
+        };
+        const response: ICommonSocketMessage = {
+            data: user,
+            timestamp: new Date(),
+        };
+        this.socket.emit(Event.NewUser, response);
     }
 }
