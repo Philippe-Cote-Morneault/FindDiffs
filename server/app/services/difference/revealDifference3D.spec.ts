48d289a61fa1a2e1ea50fe0323380a6076767a94
Felix <felix-atnoine.bourbonnais@polymtl.ca>
2019-03-21 23:14:15 -0400

    (t) revealDifference3D no difference test (FAB)

---
+++ server/app/services/difference/revealDifference3D.spec.ts
@@ -0,0 +1,28 @@
+import { expect } from "chai";
+import { DifferenceType, ICommonReveal3D } from "../../../../common/model/reveal";
+// import { scene } from "../../../../client/src/app/tests/scene/sceneMock";
+import { ICommonGeometricModifications } from "../../../../common/model/scene/modifications/geometricModifications";
+import { /*ICommonScene,*/ ObjectType } from "../../../../common/model/scene/scene";
+import { R } from "../../strings";
+import { NoErrorThrownException } from "../../tests/noErrorThrownException";
+import { sceneModifications } from "../../tests/sceneGeometric/geometricSceneModificationsMock";
+import { RevealDifference3D } from "./revealDifference3D";
+
+describe("RevealDifference3D", () => {
+    describe("reveal()", () => {
+        // tslint:disable:max-line-length
+        it("Should throw an error if there is no difference to be found", () => {
+            const mockSceneModifications: ICommonGeometricModifications = sceneModifications;
+            const originalObjectId: string = "randomNotExistingId";
+            const modifiedObjectId: string = "randomNotExistingId";
+
+            try {
+                new RevealDifference3D(mockSceneModifications, originalObjectId, modifiedObjectId, ObjectType.Geometric).reveal();
+                throw new NoErrorThrownException();
+            } catch (err) {
+                expect(err.message).to.equal(R.ERROR_NO_DIFFERENCE_FOUND);
+            }
+        });
+
+    });
+});
