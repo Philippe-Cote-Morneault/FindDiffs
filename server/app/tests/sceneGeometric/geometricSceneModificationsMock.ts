74b927cc331589850aaacac2bc897187034375c7
Felix <felix-atnoine.bourbonnais@polymtl.ca>
2019-03-21 23:10:37 -0400

    (d) Added mock objects to the sceneModifications (FAB)

--- server/app/tests/sceneGeometric/geometricSceneModificationsMock.ts
+++ server/app/tests/sceneGeometric/geometricSceneModificationsMock.ts
@@ -3,6 +3,25 @@ import { ICommonGeometricModifications } from "../../../../common/model/scene/mo
 import { ObjectType } from "../../../../common/model/scene/scene";
 import { cube3, cylinder3 } from "./geometricObjectMocks";
 
+const colorChangedObjects: Pair<string, number>[] = [
+    {
+        key: "sf34fsdfs",
+        value: 0x4444,
+    },
+    {
+        key: "435435wdc",
+        value: 0x4444,
+    },
+    {
+        key: "sddsdsdsdsddsfdsfsfds",
+        value: 0x4444,
+    },
+];
+
+const deletedObjects: string[] = [
+    "kjhdhgf", "hgfhfsadfsf",
+];
+
 export const sceneModifications: ICommonGeometricModifications = {
     id: "dsgskfds32",
     colorChangedObjects: colorChangedObjects,
