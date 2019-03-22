3142d2f859fee8225f8b2aff8cb2393c4440cf6c
Phil <philbond1997@hotmail.com>
2019-03-22 16:01:12 -0400

    (r) Removed SocketDispatcher. (PC)

--- server/app/services/socket/socketDispatcher.ts
+++
@@ -1,9 +0,0 @@
-import { SocketHandler } from "./socketHandler";
-
-export class SocketDispatcher {
-    private socketHandler: SocketHandler;
-
-    public constructor(socketHandler: SocketHandler) {
-        this.socketHandler = socketHandler;
-    }
-}
