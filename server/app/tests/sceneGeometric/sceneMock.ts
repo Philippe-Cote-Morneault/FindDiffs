3d16f935f1108a4623bc31a6f0bcf29cfd0b070e
Felix <felix-atnoine.bourbonnais@polymtl.ca>
2019-03-21 23:12:13 -0400

    (d) mock scene functionnal (FAB)

--- server/app/tests/sceneGeometric/sceneMock.ts
+++ server/app/tests/sceneGeometric/sceneMock.ts
@@ -6,3 +6,15 @@ const sceneObjects: ICommonGeometricObject[] = [
     MOCKS.cube1, MOCKS.cube2, MOCKS.cylinder1, MOCKS.cylinder2, MOCKS.sphere1, MOCKS.sphere2,
     MOCKS.cone1, MOCKS.cone2, MOCKS.pyramid1, MOCKS.pyramid2,
 ];
+
+export const scene: ICommonGeometricScene = {
+    id: "3rfdsf43",
+    dimensions: {
+        x: 300,
+        y: 200,
+        z: 300,
+    },
+    bg_color: 0x123454,
+    type: ObjectType.Geometric,
+    sceneObjects: sceneObjects,
+};
