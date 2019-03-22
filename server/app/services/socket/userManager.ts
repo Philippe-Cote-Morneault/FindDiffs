c0063925c943582b6bcaaddf551b22532e91cd5b
Phil <philbond1997@hotmail.com>
2019-03-21 20:42:14 -0400

    (d) Added username validation in UserManager. (PC)

--- server/app/services/socket/userManager.ts
+++ server/app/services/socket/userManager.ts
@@ -1,37 +1,61 @@
 import { NotFoundException } from "../../../../common/errors/notFoundException";
 import { _e, R } from "../../strings";
+import { UsernameValidator } from "../user/usernameValidator";
 
-export class UsernameManager  {
-    private static instance: UsernameManager;
+export class UserManager  {
+    private static instance: UserManager;
 
     private idUsernames: Map<string, string>;
-    private authentifiedUsers: Map<string, string>;
 
-    public static getInstance(): UsernameManager {
-        if (!UsernameManager.instance) {
-            UsernameManager.instance = new UsernameManager();
+    public static getInstance(): UserManager {
+        if (!UserManager.instance) {
+            UserManager.instance = new UserManager();
         }
 
-        return UsernameManager.instance;
+        return UserManager.instance;
+    }
+
+    public validateUsername(username: string): boolean {
+        return !this.idUsernames.has(username) && UsernameValidator.validateUsername(username);
+    }
+
+    public hasUsername(username: string): boolean {
+        return this.idUsernames.has(username);
     }
 
     public addUsername(socketId: string, username: string): void {
         console.log("adding userName: " + username);
-        this.idUsernames.set(socketId, username);
-    }
-
-    public getUsername(socketId: string): string {
-        return (this.idUsernames.get(socketId)) as string;
-    }
-
-    public removeUsername(socketId: string): void {
-        this.idUsernames.delete(socketId);
+        this.idUsernames.set(username, socketId);
+        this.idUsernames.forEach((val: string, key) => {
+            console.log(key + " : " + val);
+        });
     }
 
     public getSocketId(username: string): string {
-        const id: string | undefined =  Object.keys(this.idUsernames).find((key: string) => this.idUsernames[key] === username);
+        return (this.idUsernames.get(username)) as string;
+    }
+
+    public removeUsername(socketId: string): string {
+        console.log("removingUsername");
+        const removedUsername: string = this.getUsername(socketId);
+        this.idUsernames.delete(removedUsername);
+        console.log("removedUsername: " + removedUsername);
+
+        return removedUsername;
+    }
+
+    public getUsername(socketId: string): string {
+        console.log(this.idUsernames);
+        let id: string | undefined;
+        this.idUsernames.forEach((val: string, key) => {
+            console.log(key + " : " + val);
+            if (val === socketId) {
+                id = key;
+            }
+        });
+        
         if (id === undefined) {
-                throw new NotFoundException(_e(R.ERROR_INVALIDID, [username]));
+                throw new NotFoundException(_e(R.ERROR_INVALIDID, [socketId]));
         }
 
         return id;
