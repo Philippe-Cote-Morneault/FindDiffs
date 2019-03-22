f4868b2ffb270f7c0bcc4a64f91eba144e58c8ca
Laurent Barberis <laurent.barberis15@gmail.com>
2019-03-21 21:12:59 -0400

    (D) Timer now listen to socket (LB)

--- client/src/app/services/timer/timer.service.ts
+++ client/src/app/services/timer/timer.service.ts
@@ -1,24 +1,53 @@
 import { Injectable } from "@angular/core";
 import Timer from "easytimer.js";
+import { Event } from "../../../../../common/communication/webSocket/socketMessage";
+import { SocketHandlerService } from "../socket/socketHandler.service";
+import { SocketSubscriber } from "../socket/socketSubscriber";
 
 @Injectable({
     providedIn: "root",
 })
-export class TimerService {
+export class TimerService implements SocketSubscriber {
     private static readonly MINUTES_POSITION: number = 3;
-    public timer: Timer;
+    private timer: Timer;
+    private chronometer: HTMLElement;
 
-    public constructor() {
+    public constructor(chronometer: HTMLElement) {
         this.timer = new Timer();
+        this.chronometer = chronometer;
     }
-    public startTimer(chronometer: HTMLElement): void {
+
+    public subscribeToSocket(): void {
+        SocketHandlerService.getInstance().subscribe(Event.GameEnded, this);
+        SocketHandlerService.getInstance().subscribe(Event.GameStarted, this);
+    }
+
+    public notify(event: Event): void {
+        switch (event) {
+            case Event.GameStarted: {
+                return this.startTimer();
+            }
+            case Event.GameEnded: {
+                return this.stopTimer();
+            }
+            default: {
+                break;
+            }
+        }
+    }
+
+    public startTimer(): void {
         this.timer.reset();
         this.timer.start();
         this.timer.addEventListener("secondsUpdated", () =>
-            chronometer.innerText = this.timer.getTimeValues().toString().slice(TimerService.MINUTES_POSITION));
+            this.chronometer.innerText = this.getTimeValues());
     }
 
     public stopTimer(): void {
         this.timer.stop();
+    }
+
+    public getTimeValues(): string {
+        return this.timer.getTimeValues().toString().slice(TimerService.MINUTES_POSITION);
     }
 }
