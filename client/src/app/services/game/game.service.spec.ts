e55f4268bb03a9c2c11fa305c18d6db909f0a6cf
Laurent Barberis <laurent.barberis15@gmail.com>
2019-03-23 13:41:41 -0400

    (t) added tests for gameservice (LB)

--- client/src/app/services/game/game.service.spec.ts
+++ client/src/app/services/game/game.service.spec.ts
@@ -1,16 +1,35 @@
-// import { TestBed } from "@angular/core/testing";
-// import { expect } from "chai";
-// import { GameService } from "./game.service";
+import { TestBed } from "@angular/core/testing";
+import { expect } from "chai";
+import { Event } from "../../../../../common/communication/webSocket/socketMessage";
+import { GameService } from "./game.service";
 
-describe("TimerService", () => {
-    // let service: GameService;
-    // const time: number = 2000;
+describe("GameService", () => {
+    let service: GameService;
+    const time: number = 2000;
 
     beforeEach(() => {
-        // service = TestBed.get(GameService);
+        service = TestBed.get(GameService);
     });
 
-    it("Should return the correct time", () => {
-        // Test
+    it("Should return the correct time after game start and end after 2 sec", () => {
+
+        service.notify(Event.GameStarted);
+        expect(service.getGameStarted()).to.equal(true);
+        expect(service.getTimeValues()).to.equal("00:00");
+        setTimeout(() => {
+            service.notify(Event.GameEnded);
+            expect(service.getTimeValues()).to.equal("00:02");
+                }, time);
+    });
+
+    it("Should return 00:00 if the event is not supported", () => {
+
+        service.notify(Event.InvalidClick);
+        expect(service.getGameStarted()).to.equal(false);
+        expect(service.getTimeValues()).to.equal("00:00");
+        setTimeout(() => {
+            service.notify(Event.GameEnded);
+            expect(service.getTimeValues()).to.equal("00:00");
+                }, time);
     });
 });
