6f5248b7d52af567243c11ec8435bc9d555aabf1
Felix <felix-atnoine.bourbonnais@polymtl.ca>
2019-03-23 01:32:38 -0400

    (d) Made thematicScene mock

---
+++ server/app/tests/sceneThematic/thematicSceneModifications.ts
@@ -0,0 +1,11 @@
+import { Pair } from "../../../../common/model/pair";
+import { ICommonThematicModifications } from "../../../../common/model/scene/modifications/thematicModifications";
+import { ObjectType } from "../../../../common/model/scene/scene";
+
+export const sceneThematicModifications: ICommonThematicModifications = {
+    id: "dsgskfds32",
+    texturesChangedObjects: texturesChangedObjects,
+    type: ObjectType.Geometric,
+    addedObjects: [],
+    deletedObjects: deletedObjects,
+};
