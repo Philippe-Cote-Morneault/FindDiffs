b137db2039f353342ef9e71489efcd1f021ba5be
Basil <bas98c@hotmail.com>
2019-03-23 00:39:06 -0400

    (d) added interface (BC)

--- client/src/app/services/3DObjects/GeometricObjects/IThreeObject.ts
+++ client/src/app/services/3DObjects/GeometricObjects/IThreeObject.ts
@@ -2,3 +2,8 @@ export interface IThreeObject {
     original: THREE.Object3D;
     modified: THREE.Object3D;
   }
+
+export interface IThreeScene {
+  original: THREE.Scene;
+  modified: THREE.Scene;
+}
