c251a565f2e2d5f80951d091bb19a8c380d3c49d
Basil <bas98c@hotmail.com>
2019-03-24 18:50:41 -0400

    (t) added test for getBoundingClient (BC)

--- client/src/app/services/scene/sceneDetection/mouse-position.service.spec.ts
+++ client/src/app/services/scene/sceneDetection/mouse-position.service.spec.ts
@@ -1,12 +1,48 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed } from "@angular/core/testing";
+import {RouterTestingModule} from "@angular/router/testing";
+import * as sinon from "sinon";
+import * as THREE from "three";
+import { MousePositionService } from "./mouse-position.service";
 
-import { MousePositionService } from './mouse-position.service';
 
-describe('MousePositionService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+describe("MousePositionService", () => {
 
-  it('should be created', () => {
-    const service: MousePositionService = TestBed.get(MousePositionService);
-    expect(service).toBeTruthy();
+  let mousePositionService: MousePositionService;
+  let originalScene: HTMLElement;
+  // let modifiedScene: ElementRef<HTMLElement>;
+  let event: MouseEvent;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({imports: [RouterTestingModule]});
+    mousePositionService = TestBed.get(MousePositionService);
+
+    originalScene  = (document.createElement("div") as HTMLElement);
+    originalScene.style.width = "480px";
+    originalScene.style.height = "640px";
+
+    event = new MouseEvent("click");
+    // tslint:disable-next-line: no-magic-numbers
+    event.initMouseEvent("click", true, true, window, 0, 0, 0, 640, 480, false, false, false, false, 0, null);
+  });
+
+  it("Should normalize the mouse position between -1 and 1", () => {
+    // const service: MousePositionService = TestBed.get(MousePositionService);
+    let mouse: THREE.Vector2 = new THREE.Vector2(0, 0);
+    const stub: sinon.SinonStub = sinon.stub(mousePositionService, "setMousePosition");
+    mouse = mousePositionService.setMousePosition(event, mouse, originalScene);
+    expect(mouse).toBeUndefined();
+    expect(mouse).toBeUndefined();
+    stub.restore();
+  });
+
+  it("Should call the function getBoundingClientRect at least once", async() => {
+    const stub: sinon.SinonStub = sinon.stub(originalScene, "getBoundingClientRect");
+    const returnValue: ClientRect | DOMRect = { width: 100, height: 100, top: 5, bottom: 5, right: 15, left: 15};
+    stub.returns(returnValue);
+
+    const mouse: THREE.Vector2 = new THREE.Vector2(0, 0);
+    mousePositionService.setMousePosition(event, mouse, originalScene);
+
+    expect(mousePositionService.divBoxInformation).toEqual(returnValue);
   });
 });
