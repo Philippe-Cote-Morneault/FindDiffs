90f563a681a0915c04f1a0407dec0b51d8681452
Laurent Barberis <laurent.barberis15@gmail.com>
2019-03-24 00:31:02 -0400

    (r) removed unused event (LB)

--- common/communication/webSocket/socketMessage.ts
+++ common/communication/webSocket/socketMessage.ts
@@ -3,7 +3,7 @@ export interface ICommonSocketMessage {
     timestamp: Date,
 }
 
-export enum Event {Authenticate = "Authenticate", AuthenticateError = "AuthenticateError", UserConnected = "UserConnected",
+export enum Event {Authenticate = "Authenticate", UserConnected = "UserConnected",
                    NewUser = "NewUser", UserDisconnected = "UserDisconnected", GameStarted = "GameStarted",
                    PlaySoloGame = "PlaySoloGame", ReadyToPlay = "ReadyToPlay", GameClick = "GameClick",
                    DifferenceFound = "DifferenceFound", InvalidClick = "InvalidClick", GameEnded = "GameEnded"};
