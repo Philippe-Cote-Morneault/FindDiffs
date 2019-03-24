eeedf4140a5ac7bce4ba8e5dabd6f8fbfc73587c
Basil <bas98c@hotmail.com>
2019-03-24 00:50:02 -0400

    (d) added an interface for a 3D click (BC)(FAB)

--- common/model/positions.ts
+++ common/model/positions.ts
@@ -1,3 +1,5 @@
+import { ObjectType } from "./scene/scene";
+
 export interface ICommon3DPosition {
     x: number;
     y: number;
@@ -7,4 +9,11 @@ export interface ICommon3DPosition {
 export interface ICommon2DPosition{
     x: number;
     y: number;
+}
+
+export interface ICommon3DObject {
+    scenePairId: string;
+    originalObjectId: string;
+    modifiedObjectId: string;
+    gameType: ObjectType;
 }
