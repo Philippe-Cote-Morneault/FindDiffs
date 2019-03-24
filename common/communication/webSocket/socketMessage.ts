0a74b751ec90fc6daf19769f1e1709efb0b05f74
Laurent Barberis <laurent.barberis15@gmail.com>
2019-03-23 21:12:38 -0400

    (d) added AuthenticateError (LB)

--- common/communication/webSocket/socketMessage.ts
+++ common/communication/webSocket/socketMessage.ts
@@ -3,6 +3,7 @@ export interface ICommonSocketMessage {
     timestamp: Date,
 }
 
-export enum Event {Authenticate = "Authenticate", UserConnected = "UserConnected", NewUser = "NewUser", UserDisconnected = "UserDisconnected", GameStarted = "GameStarted",
+export enum Event {Authenticate = "Authenticate", AuthenticateError = "AuthenticateError", UserConnected = "UserConnected",
+                   NewUser = "NewUser", UserDisconnected = "UserDisconnected", GameStarted = "GameStarted",
                    PlaySoloGame = "PlaySoloGame", ReadyToPlay = "ReadyToPlay", GameClick = "GameClick",
                    DifferenceFound = "DifferenceFound", InvalidClick = "InvalidClick", GameEnded = "GameEnded"};
