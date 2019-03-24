809e98b7f58da80575d0722bb083013676cba931
Phil <philbond1997@hotmail.com>
2019-03-24 03:55:31 -0400

    (r) Removed logs. (PC)

--- server/app/services/socket/usernameManager.ts
+++ server/app/services/socket/usernameManager.ts
@@ -26,12 +26,7 @@ export class UsernameManager  {
         if (oldUsername) {
             this.idUsernames.delete(oldUsername);
         }
-        console.log("adding userName: " + username);
         this.idUsernames.set(username, socketId);
-        console.log("printing usernames and sockets");
-        this.idUsernames.forEach((val: string, key) => {
-            console.log(key + " : " + val);
-        });
     }
 
     public getSocketId(username: string): string {
@@ -39,22 +34,17 @@ export class UsernameManager  {
     }
 
     public removeUsername(socketId: string): string | undefined {
-        console.log("removingUsername");
         const removedUsername: string | undefined = this.getUsername(socketId);
         if (removedUsername) {
             this.idUsernames.delete(removedUsername);
         }
-        console.log("removedUsername: " + removedUsername);
 
         return removedUsername;
     }
 
     public getUsername(socketId: string): string | undefined {
-        console.log(this.idUsernames);
         let id: string | undefined;
-        this.idUsernames.forEach((val: string, key) => {
-            console.log("printing usernames and sockets");
-            console.log(key + " : " + val);
+        this.idUsernames.forEach((val: string, key: string) => {
             if (val === socketId) {
                 id = key;
             }
