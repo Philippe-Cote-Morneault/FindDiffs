c9a606bd26835fc9d2b88cf436f00b04557d7d0b
Phil <philbond1997@hotmail.com>
2019-03-24 12:53:12 -0400

    (d) FInished playerClick(). (PC)

--- server/app/services/game/freePOVGameManager.ts
+++ server/app/services/game/freePOVGameManager.ts
@@ -1,4 +1,5 @@
 import { ICommon3DObject } from "../../../../common/model/positions";
+import { ICommonReveal3D } from "../../../../common/model/reveal";
 import { Game } from "../../model/game/game";
 import { ScenePositionService } from "../difference/scenePositionService";
 import { GameManager } from "./gameManager";
@@ -10,8 +11,24 @@ export class FreePOVGameManager extends GameManager {
         super(game, endGameCallback);
         this.scenePositionService = ScenePositionService.getInstance();
     }
-    public playerClick(position: ICommon3DObject, successCallack: (data: Object | null) => void, failureCallback: () => void): void {
+
+    public playerClick(position: ICommon3DObject, successCallback: (data: Object | null) => void, failureCallback: () => void): void {
         this.scenePositionService.post3DClick(this.game.ressource_id, position.originalObjectId,
-                                              position.modifiedObjectId, position.gameType);
+                                              position.modifiedObjectId, position.gameType)
+            .then((value: ICommonReveal3D | null) => {
+                if (value && !this.differencesFound.get(value.difference_id)) {
+                    this.differenceFound((value as ICommonReveal3D).difference_id);
+                    successCallback(value);
+                } else {
+                    failureCallback();
+                }
+            });
+    }
+
+    private differenceFound(differenceId: string): void {
+        this.differencesFound.set(differenceId, true);
+        if (++this.game.differences_found === GameManager.SOLO_WINNING_DIFFERENCES_COUNT) {
+            this.endGameCallback(this.game, this.game.players[0]);
+        }
     }
 }
