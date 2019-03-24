56eecfb680cf1123e5089254e4a3d172324f4951
Phil <philbond1997@hotmail.com>
2019-03-24 12:37:45 -0400

    (d) Added click handling on FreePOV games. (PC)

--- server/app/services/game/freePOVGameManager.ts
+++ server/app/services/game/freePOVGameManager.ts
@@ -1,7 +1,17 @@
+import { ICommon3DObject } from "../../../../common/model/positions";
+import { Game } from "../../model/game/game";
+import { ScenePositionService } from "../difference/scenePositionService";
 import { GameManager } from "./gameManager";
 
-export class FreePOVGameManager extends GameManager{
-    public playerClick(position: Object, successCallack: (data: Object | null) => void, failureCallback: () => void): void {
+export class FreePOVGameManager extends GameManager {
+    private scenePositionService: ScenePositionService;
 
+    public constructor(game: Game, endGameCallback: (game: Game, winner: string) => void) {
+        super(game, endGameCallback);
+        this.scenePositionService = ScenePositionService.getInstance();
+    }
+    public playerClick(position: ICommon3DObject, successCallack: (data: Object | null) => void, failureCallback: () => void): void {
+        this.scenePositionService.post3DClick(this.game.ressource_id, position.originalObjectId,
+                                              position.modifiedObjectId, position.gameType);
     }
 }
