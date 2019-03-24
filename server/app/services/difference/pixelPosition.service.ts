1efffb5de63d37200974cdb2a5f1b7d4dd6beaac
Phil <philbond1997@hotmail.com>
2019-03-23 23:45:14 -0400

    (r) Made PixelPosition return null if error. (PC)

--- server/app/services/difference/pixelPosition.service.ts
+++ server/app/services/difference/pixelPosition.service.ts
@@ -1,5 +1,4 @@
 import * as axios from "axios";
-import { Message } from "../../../../common/communication/message";
 import { ICommonReveal } from "../../../../common/model/reveal";
 
 export class PixelPositionService {
@@ -16,17 +15,12 @@ export class PixelPositionService {
     public constructor() {
     }
 
-    public async postPixelPosition(imagePairId: string, x: number, y: number): Promise<ICommonReveal | Message> {
+    public async postPixelPosition(imagePairId: string, x: number, y: number): Promise<ICommonReveal | null> {
         const requestBody: Object = { "image_pair_id": imagePairId, "x": x, "y": y};
         try {
             return (await axios.default.post("http://localhost:3000/difference/simple", requestBody)).data as ICommonReveal;
         } catch (error) {
-            console.log(error);
-
-            return {
-                title: "Error",
-                body: error.error.body,
-            } as Message;
+            return null;
         }
     }
 }
