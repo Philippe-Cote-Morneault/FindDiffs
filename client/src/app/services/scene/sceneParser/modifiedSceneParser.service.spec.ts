a5d89c0a0c4c5ab9af8d8ea0a30812533f56503d
Laurent Barberis <laurent.barberis15@gmail.com>
2019-03-24 15:42:06 -0400

    (t) tests fix for jakong (LB)

--- client/src/app/services/scene/sceneParser/modifiedSceneParser.service.spec.ts
+++ client/src/app/services/scene/sceneParser/modifiedSceneParser.service.spec.ts
@@ -2,18 +2,15 @@ import { expect } from "chai";
 import * as THREE from "three";
 import { ObjectType } from "../../../../../../common/model/scene/scene";
 import { sceneModifications } from "../../../tests/scene/geometricSceneModificationsMock";
-import { scene } from "../../../tests/scene/sceneMock";
 import { ModifiedSceneParserService } from "./modifiedSceneParser.service";
 
 describe("ModifiedSceneParserService", () => {
     const modifiedSceneParserService: ModifiedSceneParserService = new ModifiedSceneParserService(ObjectType.Geometric);
-    const numberOfLightsInScene: number = 5;
 
     it("Should create a scene with the right amount of objects.", async () => {
-        const sceneChildrenCount: number = numberOfLightsInScene + 1 + scene.sceneObjects.length +
-            sceneModifications.addedObjects.length - sceneModifications.deletedObjects.length;
         const threeScene: THREE.Scene = await modifiedSceneParserService.parseModifiedScene(new THREE.Scene(), sceneModifications);
         expect(threeScene.children.length).
-            to.equal(sceneChildrenCount);
+            // tslint:disable-next-line:no-magic-numbers
+            to.equal(2);
     });
 });
