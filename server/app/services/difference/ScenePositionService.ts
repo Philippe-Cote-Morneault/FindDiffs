9a22350c808f8fc7bd9e95d3a80394bb284f7f47
Phil <philbond1997@hotmail.com>
2019-03-24 12:32:01 -0400

    (d) Finished ScenePositionService. (PC)

--- server/app/services/difference/ScenePositionService.ts
+++ server/app/services/difference/ScenePositionService.ts
@@ -1,5 +1,7 @@
 import * as axios from "axios";
-import { ICommonReveal } from "../../../../common/model/reveal";
+import { ICommon3DObject } from "../../../../common/model/positions";
+import { ICommonReveal3D } from "../../../../common/model/reveal";
+import { ObjectType } from "../../../../common/model/scene/scene";
 
 export class ScenePositionService {
     private static instance: ScenePositionService;
@@ -12,10 +14,16 @@ export class ScenePositionService {
         return ScenePositionService.instance;
     }
 
-    public async post3DClick(imagePairId: string, x: number, y: number): Promise<ICommonReveal | null> {
-        const requestBody: Object = { "image_pair_id": imagePairId, "x": x, "y": y};
+    public async post3DClick(scenePairId: string, originalObjectId: string, modifiedObjectId: string, 
+                             gameType: ObjectType): Promise<ICommonReveal3D | null> {
+        const requestBody: ICommon3DObject = {
+            scenePairId: scenePairId,
+            originalObjectId: originalObjectId,
+            modifiedObjectId: modifiedObjectId,
+            gameType: gameType,
+        }
         try {
-            return (await axios.default.post("http://localhost:3000/difference/free", requestBody)).data as ICommonReveal;
+            return (await axios.default.post("http://localhost:3000/difference/free", requestBody)).data as ICommonReveal3D;
         } catch (error) {
             return null;
         }
