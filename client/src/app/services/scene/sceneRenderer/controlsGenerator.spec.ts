37979e7870872b2b5b7a2a782a98e0f87e945ca0
Jacob Meilleur <jacob.meilleur@polymtl.ca>
2019-03-28 16:33:17 -0400

    (t) fully covered (JM)

--- client/src/app/services/scene/sceneRenderer/controlsGenerator.spec.ts
+++ client/src/app/services/scene/sceneRenderer/controlsGenerator.spec.ts
@@ -20,7 +20,15 @@ describe("ControlsGenerator", () => {
             stub.restore();
         });
 
-        it("should should move the camera by 1 unit in the -z direction", () => {
+        it("should do nothing if nothing is pressed", () => {
+            const newPosition: THREE.Vector3 = mockCamera.position;
+            const mockEvent: KeyboardEvent = new KeyboardEvent("keydown", {key: ""});
+            ControlsGenerator.generateGameControls(mockCamera, mockCanvas);
+            document.dispatchEvent(mockEvent);
+            expect(mockCamera.position).to.equal(newPosition);
+        });
+
+        it("should move the camera by 1 unit in the -z direction when w is pressed", () => {
             const newPosition: THREE.Vector3 = new THREE.Vector3(0, 0, mockCamera.position.z - 1);
             const mockEvent: KeyboardEvent = new KeyboardEvent("keydown", {key: "w"});
             ControlsGenerator.generateGameControls(mockCamera, mockCanvas);
@@ -28,12 +36,28 @@ describe("ControlsGenerator", () => {
             expect(mockCamera.position.z).to.equal(newPosition.z);
         });
 
-        it("should should move the camera by 1 unit in the z direction", () => {
+        it("should move the camera by 1 unit in the z direction when s is pressed", () => {
             const newPosition: THREE.Vector3 = new THREE.Vector3(0, 0, mockCamera.position.z + 1);
             const mockEvent: KeyboardEvent = new KeyboardEvent("keydown", {key: "s"});
             ControlsGenerator.generateGameControls(mockCamera, mockCanvas);
             document.dispatchEvent(mockEvent);
             expect(mockCamera.position.z).to.equal(newPosition.z);
+        });
+
+        it("should move the camera by 1 unit in the -x direction when a is pressed", () => {
+            const newPosition: THREE.Vector3 = new THREE.Vector3(mockCamera.position.x - 1, 0, 0);
+            const mockEvent: KeyboardEvent = new KeyboardEvent("keydown", {key: "a"});
+            ControlsGenerator.generateGameControls(mockCamera, mockCanvas);
+            document.dispatchEvent(mockEvent);
+            expect(mockCamera.position.x).to.equal(newPosition.x);
+        });
+
+        it("should move the camera by 1 unit in the x direction when d is pressed", () => {
+            const newPosition: THREE.Vector3 = new THREE.Vector3(mockCamera.position.x + 1, 0, 0);
+            const mockEvent: KeyboardEvent = new KeyboardEvent("keydown", {key: "d"});
+            ControlsGenerator.generateGameControls(mockCamera, mockCanvas);
+            document.dispatchEvent(mockEvent);
+            expect(mockCamera.position.x).to.equal(newPosition.x);
         });
     });
 });
