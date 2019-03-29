aaf9c848a61669050e75c4f134b1e6a493e04e66
Basil <bas98c@hotmail.com>
2019-03-29 18:34:25 -0400

    (t) added tests for sockets (BC)

--- client/src/app/services/socket/socketHandler.service.spec.ts
+++ client/src/app/services/socket/socketHandler.service.spec.ts
@@ -2,23 +2,78 @@ import { TestBed } from "@angular/core/testing";
 import { RouterTestingModule } from "@angular/router/testing";
 import { Event, ICommonSocketMessage } from "../../../../../common/communication/webSocket/socketMessage";
 import { SocketHandlerService } from "./socketHandler.service";
+import { SERVER_URL } from "../../../../../common/url";
+import * as io from "socket.io-client";
+// import * as sinon from "sinon";
 
-describe("ChatFormater", () => {
+describe("SocketHandlerService", () => {
     let service: SocketHandlerService;
-
+    let socketClient: SocketIOClient.Socket;
+    let returnValue: string;
     beforeEach(async() => {
         TestBed.configureTestingModule({
             imports: [RouterTestingModule],
         });
         service = TestBed.get(SocketHandlerService);
+        socketClient = io.connect(SERVER_URL);
+
+        spyOn(sessionStorage, "getItem").and.callFake(() => {
+            returnValue = "hello";
+            return returnValue!;
+        });
     });
 
-    it("Should emit the right message to the server when a new user is connected", () => {
-        const msg: ICommonSocketMessage = {data: "", timestamp: new Date()};
+    describe("emitMessage()", () => {
+        it("Should emit the right message to the server when a new user is connected", () => {
+            const msg: ICommonSocketMessage = {data: "", timestamp: new Date()};
 
-        const event: Event = Event.NewUser;
-        spyOn(service.socket, "emit").and.callFake((event: Event, msg: ICommonSocketMessage) => { });
-        service.emitMessage(event, msg);
-        expect(service.socket.emit).toHaveBeenCalledWith(event, msg);
+            const event: Event = Event.NewUser;
+            spyOn(service.socket, "emit").and.callFake((event: Event, msg: ICommonSocketMessage) => { });
+            service.emitMessage(event, msg);
+            expect(service.socket.emit).toHaveBeenCalledWith(event, msg);
+        });
+    });
+
+    describe("onAutenticate()", () => {
+        it("Should authenticate the user", () => {
+            const message: ICommonSocketMessage = {data: "", timestamp: new Date()};
+            const msg: ICommonSocketMessage = message;
+
+            const eventAuthenticate: Event = Event.Authenticate;
+
+            service.onAuthenticate();
+            spyOn(service.socket, "on").and.callFake((event: Event, fct: Function) => {
+                fct(msg);
+                expect(event).toEqual(eventAuthenticate);
+            });
+        });
+    });
+
+    describe("manageAuthenticateEvent()", () => {
+        it("Should not set the session storage", () => {
+            const message: ICommonSocketMessage = {data: "hello", timestamp: new Date()};
+
+            service.manageAuthenticateEvent(message);
+
+            let returnValue2: string = sessionStorage.getItem("token")!;
+            expect(returnValue2).not.toEqual("");
+        });
+
+        it("Should set the session storage", () => {
+            const message: ICommonSocketMessage = {data: "hello", timestamp: new Date()};
+
+            service.manageAuthenticateEvent(message);
+            
+            let returnValue2: string = sessionStorage.getItem("token")!;
+            expect(returnValue2).toEqual(returnValue);
+        });
+    });
+
+    describe("setEventListeners()", () => {
+        it("Should setEventListeners on an event", () => {
+            spyOn(service, "onAuthenticate");
+            service.setEventListeners(socketClient);
+            expect(service.onAuthenticate).toHaveBeenCalled();
+        });
     });
 });
