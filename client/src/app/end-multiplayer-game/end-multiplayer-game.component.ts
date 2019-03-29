d64dc1ddb30f2f51f4d7541fa21b64bdc96137ce
Felix <felix-atnoine.bourbonnais@polymtl.ca>
2019-03-28 21:04:25 -0400

    (d) New end-multiplayer-game component (FAB)

---
+++ client/src/app/end-multiplayer-game/end-multiplayer-game.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "app-end-multiplayer-game",
+  templateUrl: "./end-multiplayer-game.component.html",
+  styleUrls: ["./end-multiplayer-game.component.css"],
+})
+export class EndMultiplayerGameComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
