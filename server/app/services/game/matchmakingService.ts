85c17165c506d001d9e368175391ef215ae73838
Laurent Barberis <laurent.barberis15@gmail.com>
2019-03-30 14:44:26 -0400

    (D) added endMatchmaking to mms (LB)

--- server/app/services/game/matchmakingService.ts
+++ server/app/services/game/matchmakingService.ts
@@ -1,5 +1,6 @@
 import { ICommonGame } from "../../../../common/communication/webSocket/game";
-import { ICommonSocketMessage } from "../../../../common/communication/webSocket/socketMessage";
+import { Event, ICommonSocketMessage } from "../../../../common/communication/webSocket/socketMessage";
+import { SocketHandler } from "../socket/socketHandler";
 import { GameService } from "./gameService";
 
 export class MatchmakingService {
@@ -7,6 +8,7 @@ export class MatchmakingService {
     // private socketHandler: SocketHandler;
     private pendingGame: Map<string, string>;
     private gameService: GameService;
+    private socketHandler: SocketHandler;
 
     public static getInstance(): MatchmakingService {
         if (!MatchmakingService.instance) {
@@ -19,21 +21,37 @@ export class MatchmakingService {
     private constructor() {
         this.pendingGame = new Map();
         this.gameService = GameService.getInstance();
+        this.socketHandler = SocketHandler.getInstance();
+        this.subscribeToSocket();
     }
 
+    private subscribeToSocket(): void {
+        this.socketHandler.subscribe(Event.PlayMultiplayerGame, (message: ICommonSocketMessage, player: string) => {
+            this.matchPlayers(message, player);
+        });
+    }
     public matchPlayers(message: ICommonSocketMessage, player: string): void {
         const data: ICommonGame = message.data as ICommonGame;
         (this.pendingGame.has(data.ressource_id)) ?
         this.createMultiplayersGame(data, player) :
         this.pendingGame.set(data.ressource_id, player);
-
     }
 
-    private createMultiplayersGame(data: ICommonGame, player: string): void {
+    private createMultiplayersGame(data: ICommonGame, secondPlayer: string): void {
         const firstPlayer: string | undefined = this.pendingGame.get(data.ressource_id);
         if (firstPlayer) {
-            this.gameService.createMultiplayerGame(data, firstPlayer, player);
+            this.gameService.createMultiplayerGame(data, firstPlayer, secondPlayer);
+            this.EndMatchmaking(secondPlayer);
+            this.EndMatchmaking(firstPlayer);
             this.pendingGame.delete(data.ressource_id);
         }
+    }
+
+    private EndMatchmaking(player: string): void {
+        const socketMessage: ICommonSocketMessage = {
+            data: "",
+            timestamp: new Date(),
+        };
+        this.socketHandler.sendMessage(Event.EndMatchmaking, socketMessage, player);
     }
 }
