4722b4589bf67b224c9c88a2ac92daa1a825e852
Jacob Meilleur <jacob.meilleur@polymtl.ca>
2019-03-31 19:52:10 -0400

    (d) new component (JM)

---
+++ client/src/app/waiting-view/waiting-view.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "app-waiting-view",
+  templateUrl: "./waiting-view.component.html",
+  styleUrls: ["./waiting-view.component.css"]
+})
+export class WaitingViewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
