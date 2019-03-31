84c5fdd635a8ab8c5f0ae81234e1642945fec23d
Felix <felix-atnoine.bourbonnais@polymtl.ca>
2019-03-31 00:54:28 -0400

    (d) Basic component of end-multiplayer (FAB)

--- client/src/app/end-multiplayer-game/end-multiplayer-game.component.ts
+++ client/src/app/end-multiplayer-game/end-multiplayer-game.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, ElementRef, Input, OnInit, ViewChild } from "@angular/core";
+import { ActivatedRoute, Router } from "@angular/router";
+import { ICommonGameCard, ICommonScoreEntry } from "../../../../common/model/gameCard";
+import { GamesCardService } from "../services/gameCard/gamesCard.service";
+import { StringFormater } from "../util/stringFormater";
 
 @Component({
   selector: "app-end-multiplayer-game",
@@ -7,9 +11,30 @@ import { Component, OnInit } from "@angular/core";
 })
 export class EndMultiplayerGameComponent implements OnInit {
 
-  constructor() { }
+  @ViewChild("tryAgain") private tryAgain: ElementRef;
 
-  ngOnInit() {
+  @Input() public gameCard: ICommonGameCard;
+  @Input() public playerTimeChild: string;
+  @Input() public winner: string;
+
+  public endingMessage: string;
+  public gameCardId: string;
+  private waitingScreenPath: string;
+
+  public constructor(
+    private route: ActivatedRoute,
+    public gamesCardService: GamesCardService,
+    private router: Router) {
+    this.waitingScreenPath = "/gamesList";
   }
+
+  public ngOnInit(): void {
+    this.route.params.subscribe((params) => {
+      this.gameCardId = params["id"];
+    });
+    this.getGameCardById();
+    this.verifyWinCondition();
+  }
+
 
 }
