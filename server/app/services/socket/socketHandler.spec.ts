fdeb67a92a4959141dcad2ea6dcafef9c48c8b27
Phil <philbond1997@hotmail.com>
2019-03-31 14:58:09 -0400

    (t) Added more tests for SocketHandler. (PC)

--- server/app/services/socket/socketHandler.spec.ts
+++ server/app/services/socket/socketHandler.spec.ts
@@ -1,10 +1,15 @@
 import { expect } from "chai";
 import * as http from "http";
+import * as sinon from "sinon";
 import { Event, ICommonSocketMessage } from "../../../../common/communication/webSocket/socketMessage";
 import { SocketCallback } from "./socketCallback";
 import { SocketHandler } from "./socketHandler";
+import { UsernameManager } from "./usernameManager";
 
 describe("SocketHandler", () => {
+    const socketHander: SocketHandler = SocketHandler.getInstance();
+    const usernameManager: UsernameManager = UsernameManager.getInstance();
+
     describe("getInstance()", () => {
         it("Should return an instance of SocketHandler when getting an instance", () => {
             const instance: SocketHandler = SocketHandler.getInstance();
@@ -33,6 +38,57 @@ describe("SocketHandler", () => {
             const callback: SocketCallback = (message: ICommonSocketMessage, sender: string) => {};
             socketHandler.subscribe(Event.UserConnected, callback);
             expect((socketHandler["subscribers"].get(Event.UserConnected) as SocketCallback[]).indexOf(callback)).to.not.equal(-1);
+        });
+    });
+
+    describe("sendMessage()", () => {
+        it("Should send a message with the event and message passed in parameters", () => {
+            const spy = sinon.spy(socketHander["io"], "to");
+            const message: ICommonSocketMessage = {
+                data: "test",
+                timestamp: new Date(),
+            };
+            usernameManager.addUsername("1234asdf", "phil");
+            socketHander.sendMessage(Event.Authenticate, message, "phil");
+            expect(spy.calledOnce).to.be.true;
+            spy.restore();
+        });
+
+        it("Should send a message to the socket associated with the username passed as an argument", () => {
+            const spy = sinon.spy(socketHander["io"], "to");
+            const message: ICommonSocketMessage = {
+                data: "test",
+                timestamp: new Date(),
+            };
+            usernameManager.addUsername("1234asdf", "phil");
+            socketHander.sendMessage(Event.Authenticate, message, "phil");
+            expect(spy.getCalls()[0].args[0]).to.equal("1234asdf");
+            spy.restore();
+        });
+    });
+
+    describe("broadcastMessage()", () => {
+        it("Should broadcast a message to all sockets with the event passed as an argument", () => {
+            const spy = sinon.spy(socketHander["io"].sockets, "emit");
+            const message: ICommonSocketMessage = {
+                data: "test123",
+                timestamp: new Date(),
+            }
+
+            socketHander.broadcastMessage(Event.Authenticate, message);
+            expect(spy.getCalls()[0].args[0]).to.equal(Event.Authenticate);
+            spy.restore();
+        });
+        it("Should broadcast a message to all sockets with the message passed as an argument", () => {
+            const spy = sinon.spy(socketHander["io"].sockets, "emit");
+            const message: ICommonSocketMessage = {
+                data: "test123",
+                timestamp: new Date(),
+            };
+
+            socketHander.broadcastMessage(Event.Authenticate, message);
+            expect(spy.getCalls()[0].args[1]).to.equal(message);
+            spy.restore();
         });
     });
 });
