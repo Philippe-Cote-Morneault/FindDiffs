629e45d380bb2d1ae522efb883bdd2e7c608dd9f
Felix <felix-atnoine.bourbonnais@polymtl.ca>
2019-04-01 07:39:05 -0400

    (r) Changed component name accordingly (FAB)

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -11,7 +11,7 @@ import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { CreateGameFreeViewComponent } from "./create-game-free-view/create-game-free-view.component";
 import { CreateGameSimpleViewComponent } from "./create-game-simple-view/create-game-simple-view.component";
-import { EndMultiplayerGameComponent } from "./end-multiplayer-game/end-multiplayer-game.component";
+import { EndGameComponent } from "./end-game/end-game.component";
 import { EndSoloGameComponent } from "./end-solo-game/end-solo-game.component";
 import { GameViewFreeComponent } from "./game-view-free/game-view-free.component";
 import { GameViewSimpleComponent } from "./game-view-simple/game-view-simple.component";
@@ -32,7 +32,7 @@ import { GameCardLoaderService } from "./services/gameCard/gameCardLoader.servic
         CreateGameFreeViewComponent,
         GameViewFreeComponent,
         EndSoloGameComponent,
-        EndMultiplayerGameComponent,
+        EndGameComponent,
     ],
     imports: [
         BrowserModule,
