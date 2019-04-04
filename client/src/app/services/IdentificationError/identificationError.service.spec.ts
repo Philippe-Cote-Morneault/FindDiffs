1e2ca5949759a225cbedb065f7593a9c0b93b8be
Martin Pouliot <martinp507@gmail.com>
2019-04-04 16:00:53 -0400

    (t) Added sinon stub for audio play (MP)

--- client/src/app/services/IdentificationError/identificationError.service.spec.ts
+++ client/src/app/services/IdentificationError/identificationError.service.spec.ts
@@ -1,6 +1,7 @@
 import { TestBed } from "@angular/core/testing";
 import { RouterTestingModule } from "@angular/router/testing";
 import { expect } from "chai";
+import * as Sinon from "sinon";
 import { IdentificationError } from "./identificationError.service";
 
 describe("IdentificationError", () => {
@@ -11,6 +12,15 @@ describe("IdentificationError", () => {
             imports: [RouterTestingModule],
         });
         service = TestBed.get(IdentificationError);
+
+    });
+
+    beforeEach(() => {
+        Sinon.stub(Audio.prototype, "play");
+    });
+
+    afterEach(() => {
+        (Audio.prototype.play as Sinon.SinonStub).restore();
     });
 
     it("Should return the correct values after the error is shown", async () => {
