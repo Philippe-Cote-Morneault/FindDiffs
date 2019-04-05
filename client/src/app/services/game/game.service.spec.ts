30a5188197d4e068d40f587bda17b74ce1e2e93c
Laurent Barberis <laurent.barberis15@gmail.com>
2019-04-05 18:36:55 -0400

    (t) fixed tests (LB)

--- client/src/app/services/game/game.service.spec.ts
+++ client/src/app/services/game/game.service.spec.ts
@@ -95,17 +95,4 @@ describe("GameService", () => {
             expect(service.getGameStarted()).to.equal(false);
         });
     });
-    describe("getIsSoloGame, setItSoloGame", () => {
-        it("Should return true if the game is solo", async () => {
-            service.setIsSoloGame(true);
-            expect(service.getIsSoloGame()).to.equal(true);
-        });
-        it("Should return true by default", async () => {
-            expect(service.getIsSoloGame()).to.equal(true);
-        });
-        it("Should return false if the game is multiplayer", async () => {
-            service.setIsSoloGame(false);
-            expect(service.getIsSoloGame()).to.equal(false);
-        });
-    });
 });
