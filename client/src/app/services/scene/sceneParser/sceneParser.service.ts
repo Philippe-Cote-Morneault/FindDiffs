f37268a22e5b5b789ee6b4b5e7550bd6f5f6d068
Basil <bas98c@hotmail.com>
2019-04-06 18:23:59 -0400

    (d) added box 3 to the array for all elements in the array (BC)

--- client/src/app/services/scene/sceneParser/sceneParser.service.ts
+++ client/src/app/services/scene/sceneParser/sceneParser.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from "@angular/core";
 import * as THREE from "three";
 import { ICommonSceneObject } from "../../../../../../common/model/scene/objects/sceneObject";
 import { ICommonScene } from "../../../../../../common/model/scene/scene";
+import { ISceneBoundingBox } from "./ISceneBoundingBox";
 import { AbstractSceneParser } from "./abstractSceneParserService";
 
 @Injectable({
@@ -12,17 +13,23 @@ export class SceneParserService extends AbstractSceneParser {
         super(scene);
     }
 
-    public async parseScene(): Promise<THREE.Scene> {
+    public async parseScene(): Promise<ISceneBoundingBox> {
         const scene: THREE.Scene = await this.createScene();
 
-        await this.parseObjects(scene, this.sceneModel.sceneObjects);
+        const bbox: THREE.Box3[] = await this.parseObjects(scene, this.sceneModel.sceneObjects);
+        // tslint:disable-next-line:no-unnecessary-local-variable
+        const sceneBoundingBox: ISceneBoundingBox = {
+            scene: scene,
+            bbox: bbox,
+        };
 
-        return scene;
+        return sceneBoundingBox;
     }
 
     // tslint:disable-next-line:max-func-body-length
-    private async parseObjects(scene: THREE.Scene, sceneObjects: ICommonSceneObject[]): Promise<void> {
+    private async parseObjects(scene: THREE.Scene, sceneObjects: ICommonSceneObject[]): Promise<THREE.Box3[]> {
 
+        const boundingBoxes: THREE.Box3[] = new Array<THREE.Box3>();
         const promises: Promise<THREE.Object3D>[] = sceneObjects.map(
             async (object: ICommonSceneObject) => this.sceneObjectParser.parse(object));
         // tslint:disable-next-line:max-func-body-length
@@ -57,7 +64,6 @@ export class SceneParserService extends AbstractSceneParser {
                     // box3dTop.set(vect3dMin2, vect3dMax2);
 
                     const box3dTop: THREE.Box3 = new THREE.Box3();
-
                     // tslint:disable-next-line:max-line-length
                     const geometry2: THREE.BoxGeometry = new THREE.BoxGeometry( 6 * element.scale.x, 1.3 * element.scale.y , 0.72 * element.scale.z);
                     const material2: THREE.MeshBasicMaterial = new THREE.MeshBasicMaterial( {color: 0x00ff00} );
@@ -69,23 +75,38 @@ export class SceneParserService extends AbstractSceneParser {
 
                     box3dTop.setFromObject(cube2);
 
-                    const color: THREE.Color = new THREE.Color(0xffff00);
-                    const helper1: THREE.BoxHelper = new THREE.BoxHelper(cube1, color);
-                    const helper2: THREE.BoxHelper = new THREE.BoxHelper(cube2, color);
-                    scene.add(helper1);
-                    scene.add(helper2);
+                    // const color: THREE.Color = new THREE.Color(0xffff00);
+                    // const helper1: THREE.BoxHelper = new THREE.BoxHelper(cube1, color);
+                    // const helper2: THREE.BoxHelper = new THREE.BoxHelper(cube2, color);
+
+                    // const mat4: THREE.Matrix4 = new THREE.Matrix4();
+                    // mat4.extractRotation( element.matrixWorld );
+                    // helper1.applyMatrix( mat4 );
+
+                    // helper2.update();
+                    // const mat4_2: THREE.Matrix4 = new THREE.Matrix4();
+                    // mat4_2.extractRotation( cube2.matrixWorld );
+                    // helper2.applyMatrix( mat4_2 );
+
+                    // scene.add(helper1);
+                    // scene.add(helper2);
+                    boundingBoxes.push(box3dBottom);
+                    boundingBoxes.push(box3dTop);
                     // console.log(element.position);
                 } else {
-                    // const box3d: THREE.Box3 = new THREE.Box3();
-                    // box3d.setFromObject(element);
+                    const box3d: THREE.Box3 = new THREE.Box3();
+                    box3d.setFromObject(element);
 
                     // const color: THREE.Color = new THREE.Color(0xffff00);
                     // const helper: THREE.BoxHelper = new THREE.BoxHelper(element, color);
                     // scene.add(helper);
+                    boundingBoxes.push(box3d);
                 }
             });
             scene.add(...v);
         });
+
+        return boundingBoxes;
     }
 
 }
