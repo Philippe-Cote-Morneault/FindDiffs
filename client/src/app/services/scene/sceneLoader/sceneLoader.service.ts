e62e1350322441a7c33d0c50400239a015930239
Basil <bas98c@hotmail.com>
2019-04-06 18:22:21 -0400

    (d) added array for all box3 elements and added box3 for the camera (BC)

--- client/src/app/services/scene/sceneLoader/sceneLoader.service.ts
+++ client/src/app/services/scene/sceneLoader/sceneLoader.service.ts
@@ -3,6 +3,7 @@ import * as THREE from "three";
 import { ICommonSceneModifications } from "../../../../../../common/model/scene/modifications/sceneModifications";
 import { ICommonScene } from "../../../../../../common/model/scene/scene";
 import { CameraGenerator } from "../../../services/scene/sceneRenderer/cameraGenerator";
+import { ISceneBoundingBox } from "../sceneParser/ISceneBoundingBox";
 import { ModifiedSceneParserService } from "../sceneParser/modifiedSceneParser.service";
 import { SceneParserService } from "../sceneParser/sceneParser.service";
 import { ControlsGenerator } from "../sceneRenderer/controlsGenerator";
@@ -13,12 +14,17 @@ import { RendererGenerator } from "../sceneRenderer/rendererGenerator";
 })
 
 export class SceneLoaderService {
+    public static bbox: THREE.Box3[] = new Array<THREE.Box3>();
     public camera: THREE.PerspectiveCamera;
     private renderer: THREE.WebGLRenderer;
     public scene: THREE.Scene;
 
     public async loadOriginalScene(container: HTMLElement | null, scene: ICommonScene): Promise<void> {
-        this.scene = await new SceneParserService(scene).parseScene();
+        const sceneBoundingBox: ISceneBoundingBox = await new SceneParserService(scene).parseScene();
+        this.scene = sceneBoundingBox.scene;
+        sceneBoundingBox.bbox.forEach((element: THREE.Box3) => {
+            SceneLoaderService.bbox.push(element);
+        });
         this.renderScene(container);
     }
 
@@ -27,24 +33,40 @@ export class SceneLoaderService {
             scene: THREE.Scene,
             sceneModifications: ICommonSceneModifications,
         ): Promise<void> {
-        this.scene = await new ModifiedSceneParserService(sceneModifications.type).parseModifiedScene(scene, sceneModifications);
+        // tslint:disable-next-line:max-line-length
+        const sceneBoundingBox: ISceneBoundingBox = await new ModifiedSceneParserService(sceneModifications.type).parseModifiedScene(scene, sceneModifications);
+        this.scene = sceneBoundingBox.scene;
 
+        sceneBoundingBox.bbox.forEach((element: THREE.Box3) => {
+            SceneLoaderService.bbox.push(element);
+        });
         this.renderScene(container);
     }
 
     public async loadOnCanvas(canvas: HTMLCanvasElement, scene: ICommonScene): Promise<void> {
-        this.scene = await new SceneParserService(scene).parseScene();
-
+        const sceneBoundingBox: ISceneBoundingBox = await new SceneParserService(scene).parseScene();
+        this.scene = sceneBoundingBox.scene;
         this.renderOnCanvas(canvas);
     }
 
+    // tslint:disable-next-line:max-func-body-length
     private renderScene(container: HTMLElement | null): void {
         if (container) {
             this.renderer = RendererGenerator.generateRenderer(container.clientWidth,
                                                                container.clientHeight);
             container.appendChild(this.renderer.domElement);
             this.camera = CameraGenerator.createCamera(container.clientWidth, container.clientHeight);
-            ControlsGenerator.generateGameControls(this.camera, container);
+
+            const geometry2: THREE.BoxGeometry = new THREE.BoxGeometry( );
+            const material2: THREE.MeshBasicMaterial = new THREE.MeshBasicMaterial( {color: 0x00ff00} );
+            const cube2: THREE.Mesh = new THREE.Mesh( geometry2, material2 );
+            cube2.position.set(this.camera.position.x, (this.camera.position.y ), this.camera.position.z);
+
+            const bboxCam: THREE.Box3 = new THREE.Box3().setFromObject(cube2);
+            const helper: THREE.BoxHelper = new THREE.BoxHelper(cube2);
+            this.scene.add(cube2);
+            this.scene.add(helper);
+            ControlsGenerator.generateGameControls(this.camera, helper, cube2, SceneLoaderService.bbox, bboxCam, container);
             this.animate();
         }
     }
