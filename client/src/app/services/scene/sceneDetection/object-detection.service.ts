1bcce3d75961bb674dfb5822b370f38bbae9a4e8
Basil <bas98c@hotmail.com>
2019-04-06 18:22:43 -0400

    (r) removed console.log (BC)

--- client/src/app/services/scene/sceneDetection/object-detection.service.ts
+++ client/src/app/services/scene/sceneDetection/object-detection.service.ts
@@ -22,12 +22,12 @@ export class ObjectDetectionService {
     const intersectsOriginal: THREE.Intersection[] = this.raycaster.intersectObjects(meshesOriginal, true);
     const intersectsModified: THREE.Intersection[] = this.raycaster2.intersectObjects(meshesModified, true);
 
-    console.log(intersectsOriginal);
-    console.log(intersectsModified);
     const originalObject: THREE.Object3D = intersectsOriginal[0] ?
             this.getParent(intersectsOriginal[0].object, sceneOriginal) : new THREE.Object3D;
     const modifiedObject: THREE.Object3D = intersectsModified[0] ?
             this.getParent(intersectsModified[0].object, sceneModified) : new THREE.Object3D;
+
+    console.log(originalObject);
 
     return {
       original: originalObject,
