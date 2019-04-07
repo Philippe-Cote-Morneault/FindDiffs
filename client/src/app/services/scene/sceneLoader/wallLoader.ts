adc27f50140b90adbfe5adfbed2dcd71fab8778a
Felix <felix-atnoine.bourbonnais@polymtl.ca>
2019-04-07 17:59:27 -0400

    (d) Added wallLoader method for geometric scene (FAB)

--- client/src/app/services/scene/sceneLoader/wallLoader.ts
+++ client/src/app/services/scene/sceneLoader/wallLoader.ts
@@ -1,29 +1,47 @@
 import * as THREE from "three";
 
 export class WallLoader {
-    private static readonly SCENE_DEPTH: number = 140;
-    private static readonly SCENE_HEIGHT: number = 30;
-    private static readonly SCENE_WIDTH: number = 100;
+    private static readonly SCENE_DEPTH_T: number = 140;
+    private static readonly SCENE_HEIGHT_T: number = 30;
+    private static readonly SCENE_WIDTH_T: number = 100;
+    private static readonly SCENE_DEPTH_WIDTH_G: number = 250;
+    private static readonly SCENE_HEIGHT_G: number = 125;
+
+    private static readonly WALL_POS_X_T: number = 45;
+    private static readonly WALL_POS_Y_T: number = 15;
+    private static readonly WALL_TOP_POS_Y_T: number = 30;
+    private static readonly WALL_POS_Z_T: number = 65;
+    private static readonly WALL_POS_Y_G: number = 63;
+    private static readonly WALL_POS_XZ_G: number = 125;
 
     private static readonly WALL_DEPTH: number = 1;
 
-    private static readonly WALL_POS_X: number = 45;
-    private static readonly WALL_POS_Y: number = 15;
-    private static readonly WALL_TOP_POS_Y: number = 30;
-    private static readonly WALL_POS_Z: number = 65;
-
-    public static async load(boundingBoxes: THREE.Box3[], scene: THREE.Scene): Promise<void> {
-        const geometryD: THREE.BoxGeometry = new THREE.BoxGeometry(this.SCENE_WIDTH, this.SCENE_HEIGHT, this.WALL_DEPTH);
-        const geometryW: THREE.BoxGeometry = new THREE.BoxGeometry(this.WALL_DEPTH, this.SCENE_HEIGHT, this.SCENE_DEPTH);
-        const geometryH: THREE.BoxGeometry = new THREE.BoxGeometry(this.SCENE_WIDTH, this.WALL_DEPTH, this.SCENE_DEPTH);
+    public static async loadThematic(boundingBoxes: THREE.Box3[], scene: THREE.Scene): Promise<void> {
+        const geometryD: THREE.BoxGeometry = new THREE.BoxGeometry(this.SCENE_WIDTH_T, this.SCENE_HEIGHT_T, this.WALL_DEPTH);
+        const geometryW: THREE.BoxGeometry = new THREE.BoxGeometry(this.WALL_DEPTH, this.SCENE_HEIGHT_T, this.SCENE_DEPTH_T);
+        const geometryH: THREE.BoxGeometry = new THREE.BoxGeometry(this.SCENE_WIDTH_T, this.WALL_DEPTH, this.SCENE_DEPTH_T);
         const material: THREE.MeshBasicMaterial = new THREE.MeshBasicMaterial({ color: 0x00FF00 });
 
-        this.addWall(geometryD, material, boundingBoxes, scene, 0, this.WALL_POS_Y, -this.WALL_POS_Z);
-        this.addWall(geometryD, material, boundingBoxes, scene, 0, this.WALL_POS_Y, this.WALL_POS_Z);
-        this.addWall(geometryW, material, boundingBoxes, scene, this.WALL_POS_X, this.WALL_POS_Y, 0);
-        this.addWall(geometryW, material, boundingBoxes, scene, -this.WALL_POS_X, this.WALL_POS_Y, 0);
-        this.addWall(geometryH, material, boundingBoxes, scene, 0, this.WALL_TOP_POS_Y, 0);
+        this.addWall(geometryD, material, boundingBoxes, scene, 0, this.WALL_POS_Y_T, -this.WALL_POS_Z_T);
+        this.addWall(geometryD, material, boundingBoxes, scene, 0, this.WALL_POS_Y_T, this.WALL_POS_Z_T);
+        this.addWall(geometryW, material, boundingBoxes, scene, this.WALL_POS_X_T, this.WALL_POS_Y_T, 0);
+        this.addWall(geometryW, material, boundingBoxes, scene, -this.WALL_POS_X_T, this.WALL_POS_Y_T, 0);
+        this.addWall(geometryH, material, boundingBoxes, scene, 0, this.WALL_TOP_POS_Y_T, 0);
         this.addWall(geometryH, material, boundingBoxes, scene, 0, 0, 0);
+    }
+
+    public static async loadGeometric(boundingBoxes: THREE.Box3[], scene: THREE.Scene): Promise<void> {
+        const geometryD: THREE.BoxGeometry = new THREE.BoxGeometry(this.SCENE_DEPTH_WIDTH_G, this.SCENE_HEIGHT_G, this.WALL_DEPTH);
+        const geometryW: THREE.BoxGeometry = new THREE.BoxGeometry(this.WALL_DEPTH, this.SCENE_HEIGHT_G, this.SCENE_DEPTH_WIDTH_G);
+        const geometryH: THREE.BoxGeometry = new THREE.BoxGeometry(this.SCENE_DEPTH_WIDTH_G, this.WALL_DEPTH, this.SCENE_DEPTH_WIDTH_G);
+        const material: THREE.MeshBasicMaterial = new THREE.MeshBasicMaterial({ color: 0x00FF00 });
+
+        this.addWall(geometryD, material, boundingBoxes, scene, 0, 0, -this.WALL_POS_XZ_G);
+        this.addWall(geometryD, material, boundingBoxes, scene, 0, 0, this.WALL_POS_XZ_G);
+        this.addWall(geometryW, material, boundingBoxes, scene, this.WALL_POS_XZ_G, 0, 0);
+        this.addWall(geometryW, material, boundingBoxes, scene, -this.WALL_POS_XZ_G, 0, 0);
+        this.addWall(geometryH, material, boundingBoxes, scene, 0, this.WALL_POS_Y_G, 0);
+        this.addWall(geometryH, material, boundingBoxes, scene, 0, -this.WALL_POS_Y_G, 0);
     }
 
     private static addWall(geometry: THREE.BoxGeometry, material: THREE.MeshBasicMaterial,
@@ -32,7 +50,8 @@ export class WallLoader {
         const wall: THREE.Mesh = new THREE.Mesh(geometry, material);
         wall.position.set(posX, posY, posZ);
 
-        //Test pour voir les murs
+        // TODO: remove
+        // Test pour voir les murs
         // const color: THREE.Color = new THREE.Color(0x00FF00);
         // const helper: THREE.BoxHelper = new THREE.BoxHelper(wall, color);
         // scene.add(helper);
