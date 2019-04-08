8c08e5a3145fe4cf8700815a4106786ade228a7d
Jacob Meilleur <jacob.meilleur@polymtl.ca>
2019-04-07 21:20:34 -0400

    (d) added file with different text for button (JM)

---
+++ common/buttonName.ts
@@ -0,0 +1,6 @@
+export const CREATE_BUTTON: string = "Create";
+export const JOIN_BUTTON: string = "Join";
+export const SIMPLE_BUTTON: string = "Simple";
+export const PLAY_BUTTON: string = "Play";
+export const DELETE_BUTTON: string = "Delete";
+export const RESET_BUTTON: string = "Reset";
