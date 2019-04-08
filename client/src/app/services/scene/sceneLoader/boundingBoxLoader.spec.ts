4355e17762325229726e78e4b77469418eb8daff
Felix <felix-atnoine.bourbonnais@polymtl.ca>
2019-04-07 22:39:43 -0400

    (t) Added test for BoundingBoxLoader (FAB)

---
+++ client/src/app/services/scene/sceneLoader/boundingBoxLoader.spec.ts
@@ -0,0 +1,19 @@
+import { expect } from "chai";
+import { scene } from "src/app/tests/scene/sceneMock";
+import * as THREE from "three";
+import { BoundingBoxLoader } from "./boundingBoxLoader";
+
+describe("BoundingBoxLoader", () => {
+    describe("loadLamp()", () => {
+
+        it("Should add a lamp to boundingBoxes array, which is 2 objects", async () => {
+            const boundingBoxes: THREE.Box3[] = new Array<THREE.Box3>();
+            const lampObj: THREE.Object3D = new THREE.Object3D();
+            // tslint:disable:no-magic-numbers
+            await BoundingBoxLoader.loadLamp(boundingBoxes, scene.sceneObjects[4], lampObj);
+
+            expect(boundingBoxes.length).to.equal(2);
+        });
+    });
+
+});
