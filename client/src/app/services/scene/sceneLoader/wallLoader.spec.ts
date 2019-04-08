88c2417958574f40c3374e7e899a5e455e0eb635
Felix <felix-atnoine.bourbonnais@polymtl.ca>
2019-04-07 22:58:01 -0400

    (t) New test file (FAB)

---
+++ client/src/app/services/scene/sceneLoader/wallLoader.spec.ts
@@ -0,0 +1,8 @@
+import { expect } from "chai";
+import * as THREE from "three";
+import { WallLoader } from "./WallLoader";
+
+describe("WallLoader", () => {
+    // tslint:disable:no-magic-numbers
+
+});
