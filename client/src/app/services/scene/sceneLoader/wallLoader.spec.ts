84182a72c879075b4e31defb39d93487d6a6b12f
Felix <felix-atnoine.bourbonnais@polymtl.ca>
2019-04-07 22:58:34 -0400

    (t) Added a test for wallLoader (FAB)

--- client/src/app/services/scene/sceneLoader/wallLoader.spec.ts
+++ client/src/app/services/scene/sceneLoader/wallLoader.spec.ts
@@ -4,5 +4,13 @@ import { WallLoader } from "./WallLoader";
 
 describe("WallLoader", () => {
     // tslint:disable:no-magic-numbers
+    describe("loadGeometric()", () => {
+        it("Should add 6 walls to the boundingBoxes array, for a geometric scene", async () => {
+            const boundingBoxes: THREE.Box3[] = new Array<THREE.Box3>();
+            await WallLoader.loadGeometric(boundingBoxes);
+
+            expect(boundingBoxes.length).to.equal(6);
+        });
+    });
 
 });
