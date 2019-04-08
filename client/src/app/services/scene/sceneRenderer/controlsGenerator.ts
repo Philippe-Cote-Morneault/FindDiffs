0ade5972b3f7ee3f93db8e9035ec6b9e0f4387f8
Felix <felix-atnoine.bourbonnais@polymtl.ca>
2019-04-08 12:37:03 -0400

    (d) A and D collisions now working (FAB)

--- client/src/app/services/scene/sceneRenderer/controlsGenerator.ts
+++ client/src/app/services/scene/sceneRenderer/controlsGenerator.ts
@@ -16,102 +16,40 @@ export class ControlsGenerator {
             const vector: THREE.Vector3 = new THREE.Vector3(); // create once and reuse it!
             camera.getWorldDirection(vector);
 
-            const norm: number = Math.sqrt(vector.x * vector.x + vector.y * vector.y + vector.z * vector.z);
-            const xTheta: number = Math.acos(vector.x / norm);
-            const yTheta: number = Math.acos(vector.y / norm);
-            const zTheta: number = Math.acos(vector.z / norm);
+            const norm2D: number = Math.sqrt(vector.x * vector.x + vector.z * vector.z);
+            const xTheta: number = Math.acos(vector.x / norm2D);
+            const zTheta: number = Math.acos(vector.z / norm2D);
 
             if (!ControlsGenerator.isLocked) {
                 switch (event.key) {
                     case "w":
                         // tslint:disable:max-line-length
-                        ControlsGenerator.verifyCollision(bbox, bboxCam, vector, true);
+                        ControlsGenerator.verifyCollisionWS(bbox, bboxCam, vector, true);
                         if (!ControlsGenerator.isCollision) {
-                            ControlsGenerator.applyTranslation(camera, helper, cube, vector);
+                            ControlsGenerator.applyTranslationWS(camera, helper, cube, vector);
                         }
                         ControlsGenerator.isCollision = false;
                         break;
                     case "a":
-                        const vector2: THREE.Vector3 = new THREE.Vector3(vector.z, 0, vector.x);
-                        bboxCam.translate(new THREE.Vector3(ControlsGenerator.DISTANCE_TO_MOVE * vector2.x + vector.y * Math.cos(zTheta),
-                                                            0,
-                                                            -ControlsGenerator.DISTANCE_TO_MOVE * vector2.z - vector.y * Math.cos(xTheta)));
-
-                        if (bbox) {
-                            for (let i: number = 0; i < bbox.length && !ControlsGenerator.isCollision; ++i) {
-                                if (bboxCam.intersectsBox(bbox[i])) {
-                                    ControlsGenerator.isCollision = true;
-                                    bboxCam.translate(new THREE.Vector3(-ControlsGenerator.DISTANCE_TO_MOVE * vector2.x,
-                                                                        0,
-                                                                        ControlsGenerator.DISTANCE_TO_MOVE * vector2.z));
-                                }
-                            }
-
-                            if (!ControlsGenerator.isCollision) {
-                                const vectTemp: THREE.Vector3 = new THREE.Vector3();
-                                cube.getWorldDirection(vectTemp);
-
-                                cube.translateZ(-Math.cos(xTheta) - Math.cos(yTheta));
-                                helper.translateZ(-Math.cos(xTheta) + Math.cos(yTheta));
-
-                                cube.translateX((Math.cos(zTheta)));
-                                helper.translateX((Math.cos(zTheta)));
-                                console.log((Math.cos(xTheta)));
-                                console.log(-Math.cos(xTheta) - Math.cos(yTheta));
-                                // console.log(-Math.cos(xTheta));
-                                // console.log(Math.sin(xTheta));
-                                // console.log(-(1 - Math.cos(xTheta)));
-                                // console.log(-Math.cos(xTheta));
-                                // console.log(vectTemp.z);
-                                // console.log(vector.z);
-                                // console.log(vectTemp.z - vector.z);
-                                // console.log(vectTemp.x - vector.x);
-                                // cube.translateX(-ControlsGenerator.DISTANCE_TO_MOVE * Math.abs((1 - Math.cos(xTheta))));
-                                // cube.translateZ(-ControlsGenerator.DISTANCE_TO_MOVE * (Math.cos(xTheta)));
-
-                                // helper.translateX(ControlsGenerator.DISTANCE_TO_MOVE * Math.abs((1 - Math.cos(xTheta))));
-                                // helper.translateZ(-ControlsGenerator.DISTANCE_TO_MOVE * (Math.cos(xTheta)));
-
-                                camera.translateX(-ControlsGenerator.DISTANCE_TO_MOVE);
-                            }
-                            ControlsGenerator.isCollision = false;
+                        ControlsGenerator.verifyCollisionAD(bbox, bboxCam, xTheta, zTheta, true);
+                        if (!ControlsGenerator.isCollision) {
+                            ControlsGenerator.applyTranslationAD(camera, helper, cube, xTheta, zTheta);
                         }
+                        ControlsGenerator.isCollision = false;
                         break;
                     case "s":
-                        ControlsGenerator.verifyCollision(bbox, bboxCam, vector, false);
+                        ControlsGenerator.verifyCollisionWS(bbox, bboxCam, vector, false);
                         if (!ControlsGenerator.isCollision) {
-                            ControlsGenerator.applyTranslation(camera, helper, cube, vector);
+                            ControlsGenerator.applyTranslationWS(camera, helper, cube, vector);
                         }
                         ControlsGenerator.isCollision = false;
                         break;
                     case "d":
-                        const vector3: THREE.Vector3 = new THREE.Vector3(vector.z, vector.y, vector.x);
-                        bboxCam.translate(new THREE.Vector3(-ControlsGenerator.DISTANCE_TO_MOVE * vector3.x,
-                                                            ControlsGenerator.DISTANCE_TO_MOVE * vector3.y,
-                                                            ControlsGenerator.DISTANCE_TO_MOVE * vector3.z));
-
-                        if (bbox) {
-                            for (let i: number = 0; i < bbox.length && !ControlsGenerator.isCollision; ++i) {
-                                if (bboxCam.intersectsBox(bbox[i])) {
-                                    ControlsGenerator.isCollision = true;
-                                    bboxCam.translate(new THREE.Vector3(ControlsGenerator.DISTANCE_TO_MOVE * vector3.x,
-                                                                        ControlsGenerator.DISTANCE_TO_MOVE * vector3.y,
-                                                                        -ControlsGenerator.DISTANCE_TO_MOVE * vector3.z));
-                                }
-                            }
-                            if (!ControlsGenerator.isCollision) {
-                                cube.translateX(-ControlsGenerator.DISTANCE_TO_MOVE * vector3.x);
-                                cube.translateY(ControlsGenerator.DISTANCE_TO_MOVE * vector3.y);
-                                cube.translateZ(ControlsGenerator.DISTANCE_TO_MOVE * vector3.z);
-
-                                helper.translateX(-ControlsGenerator.DISTANCE_TO_MOVE * vector3.x);
-                                helper.translateY(ControlsGenerator.DISTANCE_TO_MOVE * vector3.y);
-                                helper.translateZ(ControlsGenerator.DISTANCE_TO_MOVE * vector3.z);
-
-                                camera.translateX(ControlsGenerator.DISTANCE_TO_MOVE);
-                            }
-                            ControlsGenerator.isCollision = false;
+                        ControlsGenerator.verifyCollisionAD(bbox, bboxCam, xTheta, zTheta, false);
+                        if (!ControlsGenerator.isCollision) {
+                            ControlsGenerator.applyTranslationAD(camera, helper, cube, xTheta, zTheta);
                         }
+                        ControlsGenerator.isCollision = false;
                         break;
                     default:
                         break;
@@ -119,9 +57,9 @@ export class ControlsGenerator {
             }
         });
     }
-    private static verifyCollision(bbox: THREE.Box3[], bboxCam: THREE.Box3, vector: THREE.Vector3,
-                                   isForward: boolean): void {
-        this.distance = (!isForward) ? -ControlsGenerator.DISTANCE_TO_MOVE : ControlsGenerator.DISTANCE_TO_MOVE;
+    private static verifyCollisionWS(bbox: THREE.Box3[], bboxCam: THREE.Box3, vector: THREE.Vector3,
+                                     isForward: boolean): void {
+        this.distance = (isForward) ? ControlsGenerator.DISTANCE_TO_MOVE : -ControlsGenerator.DISTANCE_TO_MOVE;
         bboxCam.translate(new THREE.Vector3(this.distance * vector.x,
                                             this.distance * vector.y,
                                             this.distance * vector.z));
@@ -137,8 +75,27 @@ export class ControlsGenerator {
         }
     }
 
-    private static applyTranslation(camera: THREE.PerspectiveCamera, helper: THREE.BoxHelper, cube: THREE.Mesh,
-                                    vector: THREE.Vector3): void {
+    private static verifyCollisionAD(bbox: THREE.Box3[], bboxCam: THREE.Box3, xTheta: number, zTheta: number,
+                                     isLeft: boolean): void {
+        this.distance = (isLeft) ? ControlsGenerator.DISTANCE_TO_MOVE : -ControlsGenerator.DISTANCE_TO_MOVE;
+        bboxCam.translate(new THREE.Vector3(this.distance * Math.cos(zTheta),
+                                            0,
+                                            -this.distance * Math.cos(xTheta)));
+
+        if (bbox) {
+            for (let i: number = 0; i < bbox.length && !ControlsGenerator.isCollision; ++i) {
+                if (bboxCam.intersectsBox(bbox[i])) {
+                    ControlsGenerator.isCollision = true;
+                    bboxCam.translate(new THREE.Vector3(-this.distance * Math.cos(zTheta),
+                                                        0,
+                                                        this.distance * Math.cos(xTheta)));
+                }
+            }
+        }
+    }
+
+    private static applyTranslationWS(camera: THREE.PerspectiveCamera, helper: THREE.BoxHelper, cube: THREE.Mesh,
+                                      vector: THREE.Vector3): void {
         cube.translateX(this.distance * vector.x);
         cube.translateY(this.distance * vector.y);
         cube.translateZ(this.distance * vector.z);
@@ -148,5 +105,16 @@ export class ControlsGenerator {
         helper.translateZ(this.distance * vector.z);
 
         camera.translateZ(-this.distance);
+    }
+
+    private static applyTranslationAD(camera: THREE.PerspectiveCamera, helper: THREE.BoxHelper, cube: THREE.Mesh,
+                                      xTheta: number, zTheta: number): void {
+        cube.translateZ(-Math.cos(xTheta));
+        helper.translateZ(-Math.cos(xTheta));
+
+        cube.translateX(Math.cos(zTheta));
+        helper.translateX(Math.cos(zTheta));
+
+        camera.translateX(-ControlsGenerator.DISTANCE_TO_MOVE);
     }
 }
