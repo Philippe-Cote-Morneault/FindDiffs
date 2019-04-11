9058f19fa775f40111957d1c4580074bf4914d22
Felix <felix-atnoine.bourbonnais@polymtl.ca>
2019-04-10 20:10:19 -0400

    (r) Removed everything with bounding boxes, not usefull anymore (FAB)

--- client/src/app/services/scene/sceneLoader/sceneLoader.service.ts
+++ client/src/app/services/scene/sceneLoader/sceneLoader.service.ts
@@ -3,7 +3,6 @@ import * as THREE from "three";
 import { ICommonSceneModifications } from "../../../../../../common/model/scene/modifications/sceneModifications";
 import { ICommonScene } from "../../../../../../common/model/scene/scene";
 import { CameraGenerator } from "../../../services/scene/sceneRenderer/cameraGenerator";
-import { ISceneBoundingBox } from "../sceneParser/ISceneBoundingBox";
 import { ModifiedSceneParserService } from "../sceneParser/modifiedSceneParser.service";
 import { SceneParserService } from "../sceneParser/sceneParser.service";
 import { ControlsGenerator } from "../sceneRenderer/controlsGenerator";
@@ -14,17 +13,12 @@ import { RendererGenerator } from "../sceneRenderer/rendererGenerator";
 })
 
 export class SceneLoaderService {
-    public static bbox: THREE.Box3[] = new Array<THREE.Box3>();
     public camera: THREE.PerspectiveCamera;
     private renderer: THREE.WebGLRenderer;
     public scene: THREE.Scene;
 
     public async loadOriginalScene(container: HTMLElement | null, scene: ICommonScene): Promise<void> {
-        const sceneBoundingBox: ISceneBoundingBox = await new SceneParserService(scene).parseScene();
-        this.scene = sceneBoundingBox.scene;
-        sceneBoundingBox.bbox.forEach((element: THREE.Box3) => {
-            SceneLoaderService.bbox.push(element);
-        });
+        this.scene = await new SceneParserService(scene).parseScene();
         this.renderScene(container);
     }
 
@@ -34,22 +28,15 @@ export class SceneLoaderService {
             sceneModifications: ICommonSceneModifications,
         ): Promise<void> {
         // tslint:disable-next-line:max-line-length
-        const sceneBoundingBox: ISceneBoundingBox = await new ModifiedSceneParserService(sceneModifications.type).parseModifiedScene(scene, sceneModifications);
-        this.scene = sceneBoundingBox.scene;
-
-        sceneBoundingBox.bbox.forEach((element: THREE.Box3) => {
-            SceneLoaderService.bbox.push(element);
-        });
+        this.scene = await new ModifiedSceneParserService(sceneModifications.type).parseModifiedScene(scene, sceneModifications);
         this.renderScene(container);
     }
 
     public async loadOnCanvas(canvas: HTMLCanvasElement, scene: ICommonScene): Promise<void> {
-        const sceneBoundingBox: ISceneBoundingBox = await new SceneParserService(scene).parseScene();
-        this.scene = sceneBoundingBox.scene;
+        this.scene = await new SceneParserService(scene).parseScene();
         this.renderOnCanvas(canvas);
     }
 
-    // tslint:disable-next-line:max-func-body-length
     private renderScene(container: HTMLElement | null): void {
         if (container) {
             this.renderer = RendererGenerator.generateRenderer(container.clientWidth,
@@ -57,19 +44,7 @@ export class SceneLoaderService {
             container.appendChild(this.renderer.domElement);
             this.camera = CameraGenerator.createCamera(container.clientWidth, container.clientHeight);
 
-            const geometry2: THREE.BoxGeometry = new THREE.BoxGeometry( );
-            const material2: THREE.MeshBasicMaterial = new THREE.MeshBasicMaterial( {color: 0x00FF00} );
-            const cube2: THREE.Mesh = new THREE.Mesh( geometry2, material2 );
-            cube2.position.set(this.camera.position.x, (this.camera.position.y ), this.camera.position.z);
-
-            const bboxCam: THREE.Box3 = new THREE.Box3().setFromObject(cube2);
-            const helper: THREE.BoxHelper = new THREE.BoxHelper(cube2);
-            this.scene.add(cube2);
-            this.scene.add(helper);
-
-            // TODO: eventually use this.
-            // const bboxCam: any = BoundingBoxLoader.loadCamera(this.scene, this.camera);
-            ControlsGenerator.generateGameControls(this.camera, helper, cube2, SceneLoaderService.bbox, bboxCam, container);
+            ControlsGenerator.generateGameControls(this.camera, this.scene);
             this.animate();
         }
     }
