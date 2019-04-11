8bb367a533d2b6ddc80623ed2b898ec4a8726b2a
Phil <philbond1997@hotmail.com>
2019-04-11 15:37:02 -0400

    (t) Added tests for ScenePositionService. (PC)

--- server/app/services/difference/scenePositionService.spec.ts
+++ server/app/services/difference/scenePositionService.spec.ts
@@ -1,8 +1,17 @@
-/*import { expect } from "chai";
+import { expect } from "chai";
 import { ScenePositionService } from "./scenePositionService";
 
 describe("ScenePositionService", () => {
-    describe("post3DClick()", () => {
+    describe("getInstance()", () => {
+        it("Should return a ScenePositionService instance", () => {
+            const instance: ScenePositionService = ScenePositionService.getInstance();
+            expect(instance).to.be.an.instanceOf(ScenePositionService);
+        });
 
+        it("Should return the same GameService instance", () => {
+            const instance1: ScenePositionService = ScenePositionService.getInstance();
+            const instance2: ScenePositionService = ScenePositionService.getInstance();
+            expect(instance1).to.equal(instance2);
+        });
     });
-});*/
+});
