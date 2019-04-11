4566808998e082fd67557982503309cf6eb60150
Phil <philbond1997@hotmail.com>
2019-04-10 22:12:24 -0400

    (t) Added test file for ScenePositionService. (PC)

---
+++ server/app/services/difference/scenePositionService.spec.ts
@@ -0,0 +1,8 @@
+import { expect } from "chai";
+import { ScenePositionService } from "./scenePositionService";
+
+describe("ScenePositionService", () => {
+    describe("post3DClick()", () => {
+
+    });
+});
