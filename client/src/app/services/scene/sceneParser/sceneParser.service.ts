5ff2e0db8cf08e47ea4f7570c3d5aa7b9c36af82
Felix <felix-atnoine.bourbonnais@polymtl.ca>
2019-04-10 20:11:06 -0400

    (r) Removed everything with bounding boxes, not usefull anymore (FAB)

--- client/src/app/services/scene/sceneParser/sceneParser.service.ts
+++ client/src/app/services/scene/sceneParser/sceneParser.service.ts
@@ -2,9 +2,7 @@ import { Injectable } from "@angular/core";
 import * as THREE from "three";
 import { ICommonSceneObject } from "../../../../../../common/model/scene/objects/sceneObject";
 import { ICommonScene, ObjectType } from "../../../../../../common/model/scene/scene";
-import { BoundingBoxLoader } from "../sceneLoader/boundingBoxLoader";
 import { WallLoader } from "../sceneLoader/wallLoader";
-import { ISceneBoundingBox } from "./ISceneBoundingBox";
 import { AbstractSceneParser } from "./abstractSceneParserService";
 
 @Injectable({
@@ -15,33 +13,22 @@ export class SceneParserService extends AbstractSceneParser {
         super(scene);
     }
 
-    public async parseScene(): Promise<ISceneBoundingBox> {
+    public async parseScene(): Promise<THREE.Scene> {
         const scene: THREE.Scene = await this.createScene();
+        await this.parseObjects(scene, this.sceneModel.sceneObjects);
 
-        const bbox: THREE.Box3[] = await this.parseObjects(scene, this.sceneModel.sceneObjects);
-
-        return {scene: scene, bbox: bbox};
+        return scene;
     }
 
-    private async parseObjects(scene: THREE.Scene, sceneObjects: ICommonSceneObject[]): Promise<THREE.Box3[]> {
-
-        const boundingBoxes: THREE.Box3[] = new Array<THREE.Box3>();
+    private async parseObjects(scene: THREE.Scene, sceneObjects: ICommonSceneObject[]): Promise<void> {
         const promises: Promise<THREE.Object3D>[] = sceneObjects.map(
             async (object: ICommonSceneObject) => this.sceneObjectParser.parse(object));
-
         (this.sceneType === ObjectType.Geometric) ?
-            await WallLoader.loadGeometric(boundingBoxes) :
-            await WallLoader.loadThematic(boundingBoxes);
+            await WallLoader.loadGeometric(scene) :
+            await WallLoader.loadThematic(scene);
 
         await Promise.all(promises).then((v: THREE.Object3D[]) => {
-            v.forEach((element, index: number) => {
-                (element.name === "lamp.gltf") ?
-                    BoundingBoxLoader.loadLamp(boundingBoxes, sceneObjects[index], element) :
-                    BoundingBoxLoader.loadObject(boundingBoxes, element);
-            });
             scene.add(...v);
         });
-
-        return boundingBoxes;
     }
 }
