6a6163280f7a370e78e88ca11a48848312e01a99
Felix <felix-atnoine.bourbonnais@polymtl.ca>
2019-04-12 20:27:39 -0400

    (t) Changed test (FAB)

--- client/src/app/services/scene/sceneDetection/collisionDetection.service.spec.ts
+++ client/src/app/services/scene/sceneDetection/collisionDetection.service.spec.ts
@@ -6,20 +6,20 @@ describe("CollisionDetectionService", () => {
     // tslint:disable:no-magic-numbers
     describe("verifyCollisions()", () => {
         it("Should detect a collision if there is an object within the minimum distance and in the correct direction", async () => {
-            const scene: THREE.Scene = new THREE.Scene;
+            const sceneObjects: THREE.Object3D[] = new Array<THREE.Object3D>();
             const geometry: THREE.BoxGeometry = new THREE.BoxGeometry(1, 1, 1);
             const material: THREE.MeshBasicMaterial = new THREE.MeshBasicMaterial({ transparent: true, opacity: 0});
             const cube: THREE.Mesh = new THREE.Mesh(geometry, material);
             cube.position.set(0, 0, 0);
-            scene.add(cube);
-            scene.getObjectById(cube.id).position.set(0, 0, -2);
+            cube.translateZ(-2);
+            sceneObjects.push(cube);
 
             const camera: THREE.PerspectiveCamera = new THREE.PerspectiveCamera;
             camera.position.set(0, 0, 0);
             const vector: THREE.Vector3 = new THREE.Vector3();
             camera.getWorldDirection(vector);
 
-            const isCollision: boolean = CollisionDetectionService.verifyCollisions(camera, scene, vector);
+            const isCollision: boolean = CollisionDetectionService.verifyCollisions(camera, sceneObjects, vector);
             expect(isCollision).to.equal(true);
         });
     });
