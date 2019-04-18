36e4b1aa597b4f957f90f76a3cb2c891d8f512eb
Basil <bas98c@hotmail.com>
2019-04-18 14:48:37 -0400

    (r) removed console.log (BC)

--- client/src/app/services/scene/sceneDetection/mouse-position.service.ts
+++ client/src/app/services/scene/sceneDetection/mouse-position.service.ts
@@ -8,14 +8,10 @@ export class MousePositionService {
     public setMousePosition(event: MouseEvent, mouse: THREE.Vector2,
                             divBoxInformation: ClientRect | DOMRect,
                             clientWidth: number, clientHeight: number): void {
-        console.log(event.clientX);
-        console.log(event.clientY);
-
         const differenceX: number = event.clientX - divBoxInformation.left;
         const differenceY: number = event.clientY - divBoxInformation.top;
         // tslint:disable:no-magic-numbers
         mouse.x = (differenceX / clientWidth) * 2 - 1;
         mouse.y = -(differenceY / clientHeight) * 2 + 1;
-        console.log(mouse);
     }
 }
