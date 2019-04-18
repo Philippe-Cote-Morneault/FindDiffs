72d3d62624233eedd06bcfdf88b2f02bfc931ffd
Felix <felix-atnoine.bourbonnais@polymtl.ca>
2019-04-18 19:11:38 -0400

    (t) Added tests collision (FAB)

--- client/src/app/services/scene/sceneDetection/collisionDetection.service.spec.ts
+++ client/src/app/services/scene/sceneDetection/collisionDetection.service.spec.ts
@@ -1,4 +1,3 @@
-/*
 import { expect } from "chai";
 import * as THREE from "three";
 import { CollisionDetectionService } from "./collisionDetection.service";
@@ -8,11 +7,28 @@ describe("CollisionDetectionService", () => {
     describe("verifyCollisions()", () => {
         it("Should detect a collision if there is an object within the minimum distance and in the correct direction", async () => {
             const sceneObjects: THREE.Object3D[] = new Array<THREE.Object3D>();
+            const geometry: THREE.BoxGeometry = new THREE.BoxGeometry(100, 100, 2);
+            const material: THREE.MeshBasicMaterial = new THREE.MeshBasicMaterial({ transparent: true, opacity: 0});
+            const cube: THREE.Mesh = new THREE.Mesh(geometry, material);
+            cube.position.set(0, 0, -2);
+            sceneObjects.push(cube);
+
+            const camera: THREE.PerspectiveCamera = new THREE.PerspectiveCamera(45, (4 / 3), 0.1, 1000);
+            camera.position.set(0, 0, 0);
+            const vector: THREE.Vector3 = new THREE.Vector3();
+            camera.getWorldDirection(vector);
+
+            const isCollision: boolean = CollisionDetectionService.verifyCollisions(camera, sceneObjects, vector);
+            expect(isCollision).to.equal(true);
+        });
+
+        it("Should not detect a collision if there is an object within the minimum distance, but in the wrong direction", async () => {
+            const sceneObjects: THREE.Object3D[] = new Array<THREE.Object3D>();
             const geometry: THREE.BoxGeometry = new THREE.BoxGeometry(1, 1, 1);
             const material: THREE.MeshBasicMaterial = new THREE.MeshBasicMaterial({ transparent: true, opacity: 0});
             const cube: THREE.Mesh = new THREE.Mesh(geometry, material);
             cube.position.set(0, 0, 0);
-            cube.translateZ(-2);
+            cube.translateX(1);
             sceneObjects.push(cube);
 
             const camera: THREE.PerspectiveCamera = new THREE.PerspectiveCamera;
@@ -21,8 +37,25 @@ describe("CollisionDetectionService", () => {
             camera.getWorldDirection(vector);
 
             const isCollision: boolean = CollisionDetectionService.verifyCollisions(camera, sceneObjects, vector);
-            expect(isCollision).to.equal(true);
+            expect(isCollision).to.equal(false);
+        });
+
+        it("Should not detect a collision if there is an object out of the minimum distance and in the right direction", async () => {
+            const sceneObjects: THREE.Object3D[] = new Array<THREE.Object3D>();
+            const geometry: THREE.BoxGeometry = new THREE.BoxGeometry(1, 1, 1);
+            const material: THREE.MeshBasicMaterial = new THREE.MeshBasicMaterial({ transparent: true, opacity: 0});
+            const cube: THREE.Mesh = new THREE.Mesh(geometry, material);
+            cube.position.set(0, 0, 0);
+            cube.translateZ(-5);
+            sceneObjects.push(cube);
+
+            const camera: THREE.PerspectiveCamera = new THREE.PerspectiveCamera;
+            camera.position.set(0, 0, 0);
+            const vector: THREE.Vector3 = new THREE.Vector3();
+            camera.getWorldDirection(vector);
+
+            const isCollision: boolean = CollisionDetectionService.verifyCollisions(camera, sceneObjects, vector);
+            expect(isCollision).to.equal(false);
         });
     });
 });
-*/
