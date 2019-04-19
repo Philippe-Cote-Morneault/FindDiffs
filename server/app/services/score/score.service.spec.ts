8de5ca4628d189592db03197ddf2ff125d5e6e25
Phil <philbond1997@hotmail.com>
2019-04-19 16:13:58 -0400

    (t) Added more tests for ScoreService. (PC)

--- server/app/services/score/score.service.spec.ts
+++ server/app/services/score/score.service.spec.ts
@@ -1,11 +1,11 @@
 import { expect } from "chai";
 import * as sinon from "sinon";
 import { mockReq } from "sinon-express-mock";
-import { POVType, GameType } from "../../../../common/model/gameCard";
+import { GameType, POVType } from "../../../../common/model/gameCard";
+import { INewScore, INewScoreDetails } from "../../../../common/model/score";
 import { GameCard, IGameCard } from "../../model/schemas/gameCard";
 import { _e, R } from "../../strings";
 import { ScoreService } from "./score.service";
-import { INewScoreDetails } from "../../../../common/model/score";
 
 // tslint:disable-next-line:max-func-body-length
 describe("ScoreService", () => {
@@ -43,31 +43,83 @@ describe("ScoreService", () => {
         });
     });
     describe("update()", () => {
-        it("Should", async () => {
-            const req = {};
-            const mockRequest = mockReq(req);
-            const gameCardInterface: IGameCard = new GameCard({
-                pov: POVType.Free,
-                title: "hello",
-                resource_id: "1234abcd",
-                best_time_solo: [],
-                best_time_online: [],
-                creation_date: new Date(),
-            });
-            const saveStub = sinon.stub(GameCard.prototype, "save");
-            (saveStub as sinon.SinonStub).resolves();
-            const findByIdStub = sinon.stub(GameCard, "findById");
-            (findByIdStub as sinon.SinonStub).resolves(gameCardInterface);
-            const verifyScoreStub = sinon.stub(scoreService, "verifyScore");
-            (verifyScoreStub as sinon.SinonStub).returns({is_top_score: false});
-            const validateUpdateStub = sinon.stub(scoreService, "validateUpdate" as any);
+        const gameCardInterface: IGameCard = new GameCard({
+            pov: POVType.Free,
+            title: "hello",
+            resource_id: "1234abcd",
+            best_time_solo: [
+                {name: "thomas", score: 10},
+                {name: "phil", score: 20},
+                {name: "joe", score: 30},
+            ],
+            best_time_online: [
+                {name: "thomas", score: 10},
+                {name: "phil", score: 20},
+                {name: "joe", score: 30},
+            ],
+            creation_date: new Date(),
+        });
+        let validateUpdateStub: sinon.SinonStub;
+        let verifyScoreStub: sinon.SinonStub;
+        let findByIdStub: sinon.SinonStub;
+        let saveStub: sinon.SinonStub;
 
-            const result = await scoreService.update(mockRequest);
-            expect(JSON.parse(result).is_top_score).to.equal(false);
+        const score1: INewScore = {
+            is_top_score: true,
+            details: {
+                place: 3,
+                game_name: "hello",
+                username: "player1",
+                game_type: GameType.Online,
+            },
+        };
+        beforeEach((done: MochaDone) => {
+            saveStub = sinon.stub(GameCard.prototype, "save");
+            findByIdStub = sinon.stub(GameCard, "findById");
+            verifyScoreStub = sinon.stub(scoreService, "verifyScore");
+            validateUpdateStub = sinon.stub(scoreService, "validateUpdate" as any);
+            done();
+        });
+        afterEach((done: MochaDone) => {
             validateUpdateStub.restore();
             verifyScoreStub.restore();
             findByIdStub.restore();
             saveStub.restore();
+            done();
+        });
+        it("Should return is_top_score = false if this is not a new top score", async () => {
+            const req = {};
+            const mockRequest = mockReq(req);
+
+            saveStub.resolves();
+            findByIdStub.resolves(gameCardInterface);
+            verifyScoreStub.returns({is_top_score: false});
+
+            const result = await scoreService.update(mockRequest);
+            expect(JSON.parse(result).is_top_score).to.equal(false);
+        });
+        it("Should return is_top_score = true if this is a new top score", async () => {
+            const req = {};
+            const mockRequest = mockReq(req);
+
+            saveStub.resolves();
+            findByIdStub.resolves(gameCardInterface);
+            verifyScoreStub.returns(score1);
+            const result = await scoreService.update(mockRequest);
+            expect(JSON.parse(result).is_top_score).to.equal(true);
+        });
+        it("Should throw an UNKOWN_ID error if the id is not found", async () => {
+            const req = {};
+            const mockRequest = mockReq(req);
+
+            saveStub.resolves();
+            findByIdStub.resolves();
+            verifyScoreStub.returns(score1);
+            try {
+            await scoreService.update(mockRequest);
+            } catch (err) {
+                expect(err.message).to.equal(R.ERROR_UNKNOWN_ID);
+            }
         });
     });
     describe("verifyScore", () => {
