fa31f85a45ed5feea809ccdb4ec2b4347a82d246
Basil <bas98c@hotmail.com>
2019-04-19 04:17:34 -0400

    (t) added test for clickAreAllowed (BC)

--- client/src/app/services/scene/sceneDetection/objects-handler.service.spec.ts
+++ client/src/app/services/scene/sceneDetection/objects-handler.service.spec.ts
@@ -1,5 +1,8 @@
+import { ElementRef } from "@angular/core";
 import { TestBed } from "@angular/core/testing";
 import { RouterTestingModule } from "@angular/router/testing";
+import * as THREE from "three";
+import { ObjectType } from "../../../../../../common/model/scene/scene";
 import { ObjectHandler } from "./objects-handler.service";
 
 describe("ObjectHandler", () => {
@@ -9,8 +12,27 @@ describe("ObjectHandler", () => {
     });
   });
 
-  it("should be created", () => {
-    const service: ObjectHandler = TestBed.get(ObjectHandler);
-    expect(service).toBeTruthy();
+  // tslint:disable:no-any
+  // tslint:disable:no-magic-numbers
+  describe("clickAreAllowed()", () => {
+    it("should return true", async () => {
+      const service: ObjectHandler = TestBed.get(ObjectHandler);
+      spyOn<any>(service["identificationError"], "getTimeout").and.returnValue(false);
+      spyOn<any>(service["game"], "getGameStarted").and.returnValue(true);
+
+      const returnValue: boolean = service["clickAreAllowed"]();
+
+      await expect(returnValue).toEqual(true);
+    });
+
+    it("should return false", async () => {
+      const service: ObjectHandler = TestBed.get(ObjectHandler);
+      spyOn<any>(service["identificationError"], "getTimeout").and.returnValue(true);
+      spyOn<any>(service["game"], "getGameStarted").and.returnValue(true);
+
+      const returnValue: boolean = service["clickAreAllowed"]();
+
+      await expect(returnValue).toEqual(false);
+    });
   });
 });
