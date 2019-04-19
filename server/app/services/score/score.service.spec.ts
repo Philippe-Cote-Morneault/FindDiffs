00731281f636c724e23378c388098670623f7ce0
Phil <philbond1997@hotmail.com>
2019-04-19 19:36:56 -0400

    (r) All lint errors fixed on server. (PC)

--- server/app/services/score/score.service.spec.ts
+++ server/app/services/score/score.service.spec.ts
@@ -7,23 +7,24 @@ import { GameCard, IGameCard } from "../../model/schemas/gameCard";
 import { _e, R } from "../../strings";
 import { ScoreService } from "./score.service";
 
-// tslint:disable-next-line:max-func-body-length
 describe("ScoreService", () => {
     const scoreService: ScoreService = new ScoreService();
     describe("post()", () => {
         it("Should throw a UNKOWN_ID error if the id of the gameCard is invalid", async () => {
-            const req = {};
-            const mockRequest = mockReq(req);
+            const req: Object = {};
+            // tslint:disable-next-line:no-any
+            const mockRequest: any = mockReq(req);
             mockRequest.params.id = "notAValidId";
             try {
                 await scoreService.post(mockRequest);
-            } catch(err) {
+            } catch (err) {
                 expect(err.message).to.equal(R.ERROR_UNKNOWN_ID);
             }
         });
         it("Should return success if the score update worked", async () => {
-            const req = {};
-            const mockRequest = mockReq(req);
+            const req: Object = {};
+            // tslint:disable-next-line:no-any
+            const mockRequest: any = mockReq(req);
             mockRequest.params.id = "someRandomId";
             sinon.stub(GameCard, "findById");
             (GameCard.findById as sinon.SinonStub).resolves({
@@ -34,9 +35,10 @@ describe("ScoreService", () => {
                 best_time_online: [],
                 creation_date: new Date(),
             });
-            const generateScoreStub = sinon.stub(scoreService, "generateScore" as any);
+            // tslint:disable-next-line:no-any
+            const generateScoreStub: sinon.SinonStub = sinon.stub(scoreService, "generateScore" as any);
 
-            const result = await scoreService.post(mockRequest);
+            const result: string = await scoreService.post(mockRequest);
             expect(JSON.parse(result).title).to.equal(R.SUCCESS);
             generateScoreStub.restore();
             (GameCard.findById as sinon.SinonStub).restore();
@@ -73,14 +75,15 @@ describe("ScoreService", () => {
                 game_type: GameType.Online,
             },
         };
-        beforeEach((done: MochaDone) => {
+        beforeEach((done: Mocha.Done) => {
             saveStub = sinon.stub(GameCard.prototype, "save");
             findByIdStub = sinon.stub(GameCard, "findById");
             verifyScoreStub = sinon.stub(scoreService, "verifyScore");
+            // tslint:disable-next-line:no-any
             validateUpdateStub = sinon.stub(scoreService, "validateUpdate" as any);
             done();
         });
-        afterEach((done: MochaDone) => {
+        afterEach((done: Mocha.Done) => {
             validateUpdateStub.restore();
             verifyScoreStub.restore();
             findByIdStub.restore();
@@ -88,29 +91,32 @@ describe("ScoreService", () => {
             done();
         });
         it("Should return is_top_score = false if this is not a new top score", async () => {
-            const req = {};
-            const mockRequest = mockReq(req);
+            const req: Object = {};
+            // tslint:disable-next-line:no-any
+            const mockRequest: any = mockReq(req);
 
             saveStub.resolves();
             findByIdStub.resolves(gameCardInterface);
             verifyScoreStub.returns({is_top_score: false});
 
-            const result = await scoreService.update(mockRequest);
+            const result: string = await scoreService.update(mockRequest);
             expect(JSON.parse(result).is_top_score).to.equal(false);
         });
         it("Should return is_top_score = true if this is a new top score", async () => {
-            const req = {};
-            const mockRequest = mockReq(req);
+            const req: Object = {};
+            // tslint:disable-next-line:no-any
+            const mockRequest: any = mockReq(req);
 
             saveStub.resolves();
             findByIdStub.resolves(gameCardInterface);
             verifyScoreStub.returns(score1);
-            const result = await scoreService.update(mockRequest);
+            const result: string = await scoreService.update(mockRequest);
             expect(JSON.parse(result).is_top_score).to.equal(true);
         });
         it("Should throw an UNKOWN_ID error if the id is not found", async () => {
-            const req = {};
-            const mockRequest = mockReq(req);
+            const req: Object = {};
+            // tslint:disable-next-line:no-any
+            const mockRequest: any = mockReq(req);
 
             saveStub.resolves();
             findByIdStub.resolves();
@@ -139,64 +145,70 @@ describe("ScoreService", () => {
             ],
             creation_date: new Date(),
         });
-        it("Should return is_top_score: true when the score is a new top score", async () => {
-            const req = {
+        it("Should return is_top_score: true when the score is a new top score", () => {
+            const req: Object = {
                 body: {
                     time: 25000,
                     type: GameType.Online,
                     username: "player1",
                 },
             };
-            const mockRequest = mockReq(req);
-            const result = await scoreService.verifyScore(mockRequest, gameCard);
+            // tslint:disable-next-line:no-any
+            const mockRequest: any = mockReq(req);
+            const result: INewScore = scoreService.verifyScore(mockRequest, gameCard);
             expect(result.is_top_score).to.equal(true);
         });
         it("Should return the right player username when he gets a new top score", async () => {
-            const req = {
+            const req: Object = {
                 body: {
                     time: 25000,
                     type: GameType.Online,
                     username: "player1",
                 },
             };
-            const mockRequest = mockReq(req);
-            const result = await scoreService.verifyScore(mockRequest, gameCard);
+            // tslint:disable-next-line:no-any
+            const mockRequest: any = mockReq(req);
+            const result: INewScore = scoreService.verifyScore(mockRequest, gameCard);
             expect((result.details as INewScoreDetails).username).to.equal("player1");
         });
         it("Should return the right GameType when the player gets a new top score", async () => {
-            const req = {
+            const req: Object = {
                 body: {
                     time: 25000,
                     type: GameType.Online,
                     username: "player1",
                 },
             };
-            const mockRequest = mockReq(req);
-            const result = await scoreService.verifyScore(mockRequest, gameCard);
+            // tslint:disable-next-line:no-any
+            const mockRequest: any = mockReq(req);
+            const result: INewScore = scoreService.verifyScore(mockRequest, gameCard);
             expect((result.details as INewScoreDetails).game_type).to.equal(GameType.Online);
         });
         it("Should return the gameCard title when the players gets a new top score", async () => {
-            const req = {
+            const req: Object = {
                 body: {
                     time: 25000,
                     type: GameType.Online,
                     username: "player1",
                 },
             };
-            const mockRequest = mockReq(req);
-            const result = await scoreService.verifyScore(mockRequest, gameCard);
+            // tslint:disable-next-line:no-any
+            const mockRequest: any = mockReq(req);
+            const result: INewScore = scoreService.verifyScore(mockRequest, gameCard);
             expect((result.details as INewScoreDetails).game_name).to.equal(gameCard.title);
         });
         it("Should return the place 3 when the players is the 3rd best player", async () => {
-            const req = {
+            const req: Object = {
                 body: {
                     time: 25000,
                     type: GameType.Online,
                     username: "player1",
                 },
             };
-            const mockRequest = mockReq(req);
-            const result = await scoreService.verifyScore(mockRequest, gameCard);
+            // tslint:disable-next-line:no-any
+            const mockRequest: any = mockReq(req);
+            const result: INewScore = scoreService.verifyScore(mockRequest, gameCard);
+            // tslint:disable-next-line:no-magic-numbers
             expect((result.details as INewScoreDetails).place).to.equal(3);
         });
     });
@@ -220,19 +232,20 @@ describe("ScoreService", () => {
         let findByIdStub: sinon.SinonStub;
         let saveStub: sinon.SinonStub;
 
-        beforeEach((done: MochaDone) => {
+        beforeEach((done: Mocha.Done) => {
             saveStub = sinon.stub(GameCard.prototype, "save");
             findByIdStub = sinon.stub(GameCard, "findById");
             done();
         });
-        afterEach((done: MochaDone) => {
+        afterEach((done: Mocha.Done) => {
             findByIdStub.restore();
             saveStub.restore();
             done();
         });
         it("Should throw a UsernameMissing error when the request doesn't have a username", async () => {
-            const req = {};
-            const mockRequest = mockReq(req);
+            const req: Object = {};
+            // tslint:disable-next-line:no-any
+            const mockRequest: any = mockReq(req);
             mockRequest.params.id = "justARandomId";
 
             saveStub.resolves();
@@ -244,12 +257,13 @@ describe("ScoreService", () => {
             }
         });
         it("Should throw a TimeMissing error when the request doesn't have a time", async () => {
-            const req = {
+            const req: Object = {
                 body: {
                     username: "player1",
                 },
             };
-            const mockRequest = mockReq(req);
+            // tslint:disable-next-line:no-any
+            const mockRequest: any = mockReq(req);
             mockRequest.params.id = "justARandomId";
 
             saveStub.resolves();
@@ -261,13 +275,14 @@ describe("ScoreService", () => {
             }
         });
         it("Should throw a NAN error if the time in the request is not a number", async () => {
-            const req = {
+            const req: Object = {
                 body: {
                     username: "player1",
                     time: "hello",
                 },
             };
-            const mockRequest = mockReq(req);
+            // tslint:disable-next-line:no-any
+            const mockRequest: any = mockReq(req);
             mockRequest.params.id = "justARandomId";
 
             saveStub.resolves();
@@ -279,13 +294,14 @@ describe("ScoreService", () => {
             }
         });
         it("Should throw a LessThanZero error if the time in the request is less than 0", async () => {
-            const req = {
+            const req: Object = {
                 body: {
                     username: "player1",
                     time: -5,
                 },
             };
-            const mockRequest = mockReq(req);
+            // tslint:disable-next-line:no-any
+            const mockRequest: any = mockReq(req);
             mockRequest.params.id = "justARandomId";
 
             saveStub.resolves();
@@ -297,14 +313,15 @@ describe("ScoreService", () => {
             }
         });
         it("Should throw a WrongType error if the type enum passed is not valid", async () => {
-            const req = {
+            const req: Object = {
                 body: {
                     username: "player1",
                     time: 5,
                     type: 14,
                 },
             };
-            const mockRequest = mockReq(req);
+            // tslint:disable-next-line:no-any
+            const mockRequest: any = mockReq(req);
             mockRequest.params.id = "justARandomId";
 
             saveStub.resolves();
@@ -316,4 +333,5 @@ describe("ScoreService", () => {
             }
         });
     });
+// tslint:disable-next-line:max-file-line-count
 });
